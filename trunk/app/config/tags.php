a8ec5a1a265c047ee8827215ccd802f653cf4b91
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2009-07-16 12:20:01 +0000

    Override стандартных тэгов, добавлен span внутрь a тэга

---
+++ trunk/app/config/tags.php
@@ -0,0 +1,13 @@
+<?php 
+/* --------------------------------------------------------------
+   VaM Shop - open source ecommerce solution
+   http://vamshop.com
+   Copyright (c) 2007-2009 VaM Shop
+   --------------------------------------------------------------
+   Released under the GNU General Public License 
+   --------------------------------------------------------------*/
+
+$tags = array(
+	'link' => '<a href="%s" %s><span>%s</span></a>'
+);
+?> 
