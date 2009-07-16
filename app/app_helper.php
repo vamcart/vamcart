ca3bedf6a1d156b8f851e3c005fced74dfaefa7b
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2009-07-16 12:20:01 +0000

    Override стандартных тэгов, добавлен span внутрь a тэга

---
+++ app/app_helper.php
@@ -0,0 +1,18 @@
+<?php 
+/* --------------------------------------------------------------
+   VaM Shop - open source ecommerce solution
+   http://vamshop.com
+   Copyright (c) 2007-2009 VaM Shop
+   --------------------------------------------------------------
+   Released under the GNU General Public License 
+   --------------------------------------------------------------*/
+
+class AppHelper extends Helper 
+	{
+		function __construct() 
+		{
+			parent::__construct();
+			$this->loadConfig(); 
+		}
+	}
+?>
