f6b0b43e00116d9a19b453f715e5504ffdef6f99
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2009-08-23 12:58:14 +0000

    Исправлена ошибка с кэшированием цены на странице карточки товара, теперь цена правильно обновляется при переключении цены магазина в боксе Валюты

--- trunk/app/vendors/smarty/vam_plugins/function.content.php
+++ trunk/app/vendors/smarty/vam_plugins/function.content.php
@@ -16,7 +16,7 @@ function smarty_function_content($params, &$smarty)
 	global $content;
 
 	 	// Cache the output... Don't cache core pages.
-	 	$cache_name = 'vam_page_content_' . $content['Content']['id'] . '_' . $_SESSION['Customer']['language_id'];
+	 	$cache_name = 'vam_page_content_' . $content['Content']['id'] . '_' . $_SESSION['Customer']['language_id']. '_' . $_SESSION['Customer']['currency_id'];
 	 	$output = Cache::read($cache_name);
 	 	if(($output === false)||($content['Content']['parent_id'] == '-1'))
 	 	{
