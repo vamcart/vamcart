41d86d9f3990609641e60ab734b32b10a97c7b1f
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2009-08-27 06:11:38 +0000

    Изменена структура папки controllers в модуля доставки

---
+++ trunk/app/plugins/shipping/controllers/admin_controller.php
@@ -0,0 +1,33 @@
+<?php 
+/* -----------------------------------------------------------------------------------------
+   VaM Shop
+   http://vamshop.com
+   http://vamshop.ru
+   Copyright 2009 VaM Shop
+   -----------------------------------------------------------------------------------------
+   Portions Copyright:
+   Copyright 2007 by Kevin Grandon (kevingrandon@hotmail.com)
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------------------------*/
+
+class AdminController extends ShippingAppController {
+	var $uses = null;
+	
+	function edit($code)
+	{
+		App::import('Model', 'ShippingMethod');
+		$this->ShippingMethod =& new ShippingMethod();
+		
+		$data = $this->ShippingMethod->findByCode($code);
+	
+		$keys = array();
+		if(!empty($data['ShippingMethodValue']))
+			$keys = array_combine(Set::extract($data, 'ShippingMethodValue.{n}.key'),
+							  Set::extract($data, 'ShippingMethodValue.{n}.value'));		
+				
+		$this->set('data',$keys);
+	}
+	
+}
+?>
