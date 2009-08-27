7282aabf0141a08ff199ce3dc639dfc360df1f52
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2009-08-27 06:11:38 +0000

    Изменена структура папки controllers в модуля доставки

---
+++ app/plugins/shipping/controllers/flat_rate.php
@@ -0,0 +1,37 @@
+<?php 
+/* -----------------------------------------------------------------------------------------
+   VaM Shop
+   http://vamshop.com
+   http://vamshop.ru
+   Copyright 2009 VaM Shop
+   -----------------------------------------------------------------------------------------
+   Portions Copyright:
+   Copyright 2007 by Kevin Grandon (kevingrandon@hotmail.com)
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------------------------*/
+
+class FlatRateComponent extends Object 
+{
+
+	function beforeFilter ()
+	{
+	}
+
+    function startup(&$controller)
+	{
+    }
+
+
+	function calculate ()
+	{
+		App::import('Model', 'ShippingMethod');
+		$this->ShippingMethod =& new ShippingMethod();
+		
+		$method = $this->ShippingMethod->find(array('code' => 'flat_rate'));
+
+		return $method['ShippingMethodValue'][0]['value'];
+	}
+	
+}
+?>
---
+++ app/plugins/shipping/controllers/table_based.php
@@ -0,0 +1,87 @@
+<?php 
+/* -----------------------------------------------------------------------------------------
+   VaM Shop
+   http://vamshop.com
+   http://vamshop.ru
+   Copyright 2009 VaM Shop
+   -----------------------------------------------------------------------------------------
+   Portions Copyright:
+   Copyright 2007 by Kevin Grandon (kevingrandon@hotmail.com)
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------------------------*/
+
+class TableBasedComponent extends Object 
+{
+
+	function beforeFilter ()
+	{
+	}
+
+    function startup(&$controller)
+	{
+    }
+
+
+	function calculate ()
+	{
+		App::import('Model', 'ShippingMethod');
+		$this->ShippingMethod =& new ShippingMethod();
+		$key_values = $this->ShippingMethod->findByCode('table_based');
+	
+		$data = array();
+		if(!empty($key_values['ShippingMethodValue']))
+			$data = array_combine(Set::extract($key_values, 'ShippingMethodValue.{n}.key'),
+							  Set::extract($key_values, 'ShippingMethodValue.{n}.value'));	
+		
+		// Calculate the unit of measure depending on what's set in the database
+		global $order;
+		$units = 0;		
+		switch($data['table_based_type'])
+		{
+			case 'products':
+				$units = count($order['OrderProduct']);
+			break;
+			case 'total':
+				$units = $order['Order']['total'];
+			break;		
+			case 'weight':
+				foreach($order['OrderProduct'] AS $product)				
+				{
+					$units += ($product['weight'] * $product['quantity']);
+				}
+			break;				
+		}
+		
+		// Loop through the rates value
+$newline = 
+'
+';	
+		$rates = str_replace($newline,'',$data['table_based_rates']);
+		$rates = explode(',',$rates);
+		
+		$keyed_rates = array();
+		foreach($rates AS $key => $value)
+		{
+			$temp_rates = explode(':',$value);
+			$temp_rate_key = $temp_rates['0'];
+			$keyed_rates[$temp_rate_key] = $temp_rates['1'];
+		}
+		
+		$keyed_rates = array_reverse($keyed_rates,true);
+
+		$shipping_price = 0;
+		foreach($keyed_rates AS $key => $value)
+		{
+			if(($key < $units)&&($shipping_price == 0))
+			{
+				$shipping_price = $value;
+			}
+		}
+		
+		return $shipping_price;
+	}
+	
+	
+}
+?>
