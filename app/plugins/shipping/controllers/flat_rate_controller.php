7636e6bf06d3c52042493d1ce066177047447a32
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2009-08-27 06:26:54 +0000

    Добавлены новые контроллеры для модулей доставки

---
+++ app/plugins/shipping/controllers/flat_rate_controller.php
@@ -0,0 +1,58 @@
+<?php 
+/* -----------------------------------------------------------------------------------------
+   VaM Shop
+   http://vamshop.com
+   http://vamshop.ru
+   Copyright 2009 VaM Shop
+   -----------------------------------------------------------------------------------------
+   Portions Copyright:
+   Copyright 2007 by Kevin Grandon (kevingrandon@hotmail.com)
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------------------------*/
+
+class FlatRateController extends ShippingAppController {
+	var $uses = array('ShippingMethod');
+	var $module_name = 'flat_rate';
+
+	function settings ()
+	{
+	}
+
+	function install()
+	{
+
+		$new_module = array();
+		$new_module['ShippingMethod']['active'] = '1';
+		$new_module['ShippingMethod']['name'] = Inflector::humanize($this->module_name);
+		$new_module['ShippingMethod']['code'] = $this->module_name;
+		$this->ShippingMethod->save($new_module);
+
+		$this->Session->setFlash(__('Module Installed', true));
+		$this->redirect('/shipping_methods/admin/');
+	}
+
+	function uninstall()
+	{
+
+		$module_id = $this->ShippingMethod->findByCode($this->module_name);
+
+		$this->ShippingMethod->del($module_id['ShippingMethod']['id'], true);
+			
+		$this->Session->setFlash(__('Module Uninstalled', true));
+		$this->redirect('/shipping_methods/admin/');
+	}
+
+	function calculate ()
+	{
+		App::import('Model', 'ShippingMethod');
+		$this->ShippingMethod =& new ShippingMethod();
+		
+		$method = $this->ShippingMethod->findByCode($this->module_name);
+
+		return $method['ShippingMethodValue'][0]['value'];
+	}
+	
+}
+
+?>
---
+++ app/plugins/shipping/controllers/free_shipping_controller.php
@@ -0,0 +1,53 @@
+<?php 
+/* -----------------------------------------------------------------------------------------
+   VaM Shop
+   http://vamshop.com
+   http://vamshop.ru
+   Copyright 2009 VaM Shop
+   -----------------------------------------------------------------------------------------
+   Portions Copyright:
+   Copyright 2007 by Kevin Grandon (kevingrandon@hotmail.com)
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------------------------*/
+
+class FreeShippingController extends ShippingAppController {
+	var $uses = array('ShippingMethod');
+	var $module_name = 'free_shipping';
+
+	function settings ()
+	{
+	}
+
+	function install()
+	{
+
+		$new_module = array();
+		$new_module['ShippingMethod']['active'] = '1';
+		$new_module['ShippingMethod']['name'] = Inflector::humanize($this->module_name);
+		$new_module['ShippingMethod']['code'] = $this->module_name;
+		$this->ShippingMethod->save($new_module);
+
+		$this->Session->setFlash(__('Module Installed', true));
+		$this->redirect('/shipping_methods/admin/');
+	}
+
+	function uninstall()
+	{
+
+		$module_id = $this->ShippingMethod->findByCode($this->module_name);
+
+		$this->ShippingMethod->del($module_id['ShippingMethod']['id'], true);
+			
+		$this->Session->setFlash(__('Module Uninstalled', true));
+		$this->redirect('/shipping_methods/admin/');
+	}
+
+	function calculate ()
+	{
+		return 0;
+	}
+	
+}
+
+?>
