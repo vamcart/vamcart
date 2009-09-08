aecdc889060126f7dec0df0c083059a7574feb09
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2009-09-08 11:18:19 +0000

    Добавлена запись в сессию значений language_id и currency_id, на случай, если не установлена валюты и язык магазина.

--- trunk/app/app_controller.php
+++ trunk/app/app_controller.php
@@ -266,6 +266,12 @@ class AppController extends Controller {
 		if(!defined('BASE'))
 			define('BASE', $this->base);
 
+		if(!$this->Session->check('Customer.language_id'))
+			$this->Session->write('Customer.language_id',1);;
+			
+		if(!$this->Session->check('Customer.currency_id'))
+			$this->Session->write('Customer.currency_id',1);;
+
 		if(strstr($_SERVER['REQUEST_URI'],'/install'))
 		{
 			$install = 1;
