97abf9a14333e276940e50d067f9eec47cdeff96
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2009-09-14 10:49:21 +0000

    Добавлена текущая версия Smarty 2.6.26

---
+++ trunk/app/vendors/smarty/vam_plugins/function.admin_area_link.php
@@ -0,0 +1,50 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   VaM Shop
+   http://vamshop.ru
+   http://vamshop.com
+   Copyright 2009 VaM Shop
+   -----------------------------------------------------------------------------------------
+   Portions Copyright:
+   Copyright 2007 by Kevin Grandon (kevingrandon@hotmail.com)
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------------------------*/
+
+function smarty_function_admin_area_link($params, &$smarty)
+{
+	if(!empty($_SESSION['User']))
+	{
+		echo '
+		<div class="admin_link admin_area">
+			<a href="' . BASE . '/admin/">'.__('Administration', true).'</a>
+		</div>';
+	}
+
+}
+
+function smarty_help_function_admin_area_link() {
+	?>
+	<h3><?php echo __('What does this tag do?') ?></h3>
+	<p><?php echo __('Creates a link to the admin area inside of the front end template.') ?></p>
+	<p><?php echo __('The link will only be displayed if the user has an active admin session.') ?></p>
+	<h3><?php echo __('How do I use it?') ?></h3>
+	<p><?php echo __('Just insert the tag into your template like:') ?> <code>{admin_login_link}</code></p>
+	<h3><?php echo __('What parameters does it take?') ?></h3>
+	<ul>
+		<li><em>(<?php echo __('None') ?>)</em></li>		
+	</ul>
+	<?php
+}
+
+function smarty_about_function_admin_area_link() {
+	?>
+	<p><?php echo __('Author: Kevin Grandon &lt;kevingrandon@hotmail.com&gt;') ?></p>
+	<p><?php echo __('Version:') ?> 0.1</p>
+	<p>
+	<?php echo __('Change History:') ?><br/>
+	<?php echo __('None') ?>
+	</p>
+	<?php
+}
+?>
---
+++ trunk/app/vendors/smarty/vam_plugins/function.headdata.php
@@ -0,0 +1,46 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   VaM Shop
+   http://vamshop.ru
+   http://vamshop.com
+   Copyright 2009 VaM Shop
+   -----------------------------------------------------------------------------------------
+   Portions Copyright:
+   Copyright 2007 by Kevin Grandon (kevingrandon@hotmail.com)
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------------------------*/
+
+function smarty_function_headdata($params, &$smarty)
+{
+	global $content;
+
+	$result = $content['Content']['head_data'];
+
+	return $result;
+}
+
+function smarty_help_function_headdata() {
+	?>
+	<h3><?php echo __('What does this tag do?') ?></h3>
+	<p><?php echo __('Displays the head data for this content. Edit the head data for each content item under the options tab.') ?></p>
+	<h3><?php echo __('How do I use it?') ?></h3>
+	<p><?php echo __('Just insert the tag into your template like:') ?> <code>{headdata}</code></p>
+	<h3><?php echo __('What parameters does it take?') ?></h3>
+	<ul>
+		<li><em>(<?php echo __('None') ?>)</em></li>
+	</ul>
+	<?php
+}
+
+function smarty_about_function_headdata() {
+	?>
+	<p><?php echo __('Author: Kevin Grandon &lt;kevingrandon@hotmail.com&gt;') ?></p>
+	<p><?php echo __('Version:') ?> 0.1</p>
+	<p>
+	<?php echo __('Change History:') ?><br/>
+	<?php echo __('None') ?>
+	</p>
+	<?php
+}
+?>
