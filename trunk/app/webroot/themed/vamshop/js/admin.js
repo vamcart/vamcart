be60d1e86d68cd077d90f9224a095f9b836d78bc
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2009-10-10 14:49:22 +0000

    Добавлена новая JS функция checlAll для выделения checkbox элементов в админке

--- trunk/app/webroot/themed/vamshop/js/admin.js
+++ trunk/app/webroot/themed/vamshop/js/admin.js
@@ -11,3 +11,12 @@
 	                if (checkboxes[i].type == "checkbox") checkboxes[i].checked=true;
 	        }
 		}
+
+	function checkAll(theElement) {
+     var theForm = theElement.form, z = 0;
+	 for(z=0; z<theForm.length;z++){
+      if(theForm[z].type == 'checkbox' && theForm[z].name != 'checkall'){
+	  theForm[z].checked = theElement.checked;
+	  }
+     }
+    }
