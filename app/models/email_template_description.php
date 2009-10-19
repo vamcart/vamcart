2c00e3840a7ac54688ae475b3fb7de43b2b10d1c
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2009-10-19 07:27:02 +0000

    Добавлены две новые модели для шаблонов писем: email_template и email_template_description

---
+++ app/models/email_template_description.php
@@ -0,0 +1,18 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   VaM Shop
+   http://vamshop.com
+   http://vamshop.ru
+   Copyright 2009 VaM Shop
+   -----------------------------------------------------------------------------------------
+   Portions Copyright:
+   Copyright 2007 by Kevin Grandon (kevingrandon@hotmail.com)
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------------------------*/
+
+class EmailTemplateDescription extends AppModel {
+	var $name = 'EmailTemplateDescription';
+	var $belongsTo = array('EmailTemplate','Language');
+}
+?>
