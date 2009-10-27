0cc0e1b8b6143129a9478bee468e21fd24888a3d
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2009-10-27 08:52:42 +0000

    Добавлены новые настройки для отправки письма при оформлении заказа

--- app/install_schemas/database.sql
+++ app/install_schemas/database.sql
@@ -16,8 +16,11 @@ INSERT INTO `configurations` (`id`, `key`, `value`) VALUES
 (4, 'GD_LIBRARY', '0'),
 (5, 'THUMBNAIL_SIZE', '125'),
 (6, 'CACHE_TIME', '3600'),
-(7, 'NEW_ORDER_STATUS_FROM_EMAIL', 'vam@test.com'),
-(8, 'NEW_ORDER_STATUS_FROM_NAME', 'VaM Shop');
+(7, 'SEND_EXTRA_EMAIL', 'vam@test.com'),
+(8, 'NEW_ORDER_FROM_EMAIL', 'vam@test.com'),
+(9, 'NEW_ORDER_FROM_NAME', 'VaM Shop'),
+(10, 'NEW_ORDER_STATUS_FROM_EMAIL', 'vam@test.com'),
+(11, 'NEW_ORDER_STATUS_FROM_NAME', 'VaM Shop');
 
 DROP TABLE IF EXISTS contents;
 CREATE TABLE `contents` (
