d6484e03d264bb180e5b7fd0704bdb98ab88ea3f
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2009-11-01 16:56:22 +0000

    Добавлена файлы

---
+++ trunk/app/webroot/js/admin.js
@@ -0,0 +1,22 @@
+
+	$(function(){
+		$('#tabs').tabs();
+	});
+
+		function selectall()
+		{
+	        checkboxes = document.getElementsByTagName("input");
+	        for (i=0; i<checkboxes.length ; i++)
+	        {
+	                if (checkboxes[i].type == "checkbox") checkboxes[i].checked=true;
+	        }
+		}
+
+	function checkAll(theElement) {
+     var theForm = theElement.form, z = 0;
+	 for(z=0; z<theForm.length;z++){
+      if(theForm[z].type == 'checkbox' && theForm[z].name != 'checkall'){
+	  theForm[z].checked = theElement.checked;
+	  }
+     }
+    }
