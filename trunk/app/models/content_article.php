8961333c47c284937b447574eaede4abee69a8bc
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2009-11-10 17:24:24 +0000

    Добавлены модели для новостей и статей

---
+++ trunk/app/models/content_article.php
@@ -0,0 +1,17 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   VaM Shop
+   http://vamshop.com
+   http://vamshop.ru
+   Copyright 2009 VaM Shop
+   -----------------------------------------------------------------------------------------
+   Portions Copyright:
+   Copyright 2007 by Kevin Grandon (kevingrandon@hotmail.com)
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------------------------*/
+
+class ContentArticle extends AppModel {
+	var $name = 'ContentArticle';
+}
+?>
