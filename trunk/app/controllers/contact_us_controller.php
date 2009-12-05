d872487a9b02b86603522a7fedeaf647874df61a
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2009-12-05 14:05:11 +0000

    Добавлен новый контроллер contact_us для отправки почты

---
+++ trunk/app/controllers/contact_us_controller.php
@@ -0,0 +1,52 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   VaM Cart
+   http://vamcart.com
+   http://vamcart.ru
+   Copyright 2009 VaM Cart
+   -----------------------------------------------------------------------------------------
+   Portions Copyright:
+   Copyright 2007 by Kevin Grandon (kevingrandon@hotmail.com)
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------------------------*/
+
+class ContactUsController extends AppController {
+	var $name = 'ContactUs';
+	var $uses = null;
+	var $components = array('Email', 'ConfigurationBase');
+
+	function send_email ()
+	{
+		// Clean up the post
+		uses('sanitize');
+		$clean = new Sanitize();
+		$clean->paranoid($_POST);
+		
+		$config = $this->ConfigurationBase->load_configuration();
+		
+		// Send to admin
+		if($config['SEND_CONTACT_US_EMAIL'] != '')
+		{
+		
+		// Set up mail
+		$this->Email->init();
+		$this->Email->From = $_POST['name'];
+		$this->Email->FromName = $_POST['email'];
+		$this->Email->AddAddress($config['SEND_CONTACT_US_EMAIL']);
+		$this->Email->Subject = $config['SITE_NAME'] . ' - ' . __('Contact Us' ,true);
+
+		// Email Body
+		$this->Email->Body = $_POST['message'];
+		
+		// Sending mail
+		$this->Email->send();
+		
+		}
+
+		$this->redirect('/');
+	}
+	
+
+}
+?>
