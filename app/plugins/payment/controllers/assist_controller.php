41ace92c3a34ae350ae2d0b56490f4a7d5878710
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2009-12-15 17:25:19 +0000

    Добавлен новый модуль оплаты Assist

---
+++ app/plugins/payment/controllers/assist_controller.php
@@ -0,0 +1,102 @@
+<?php 
+/* -----------------------------------------------------------------------------------------
+   VaM Cart
+   http://vamcart.com
+   http://vamcart.ru
+   Copyright 2009 VaM Cart
+   -----------------------------------------------------------------------------------------
+   Portions Copyright:
+   Copyright 2007 by Kevin Grandon (kevingrandon@hotmail.com)
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------------------------*/
+
+class AssistController extends PaymentAppController {
+	var $uses = array('PaymentMethod', 'Order');
+	var $module_name = 'assist';
+
+	function settings ()
+	{
+		$this->set('data', $this->PaymentMethod->findByAlias($this->module_name));
+	}
+
+	function install()
+	{
+		$new_module = array();
+		$new_module['PaymentMethod']['active'] = '1';
+		$new_module['PaymentMethod']['default'] = '0';
+		$new_module['PaymentMethod']['name'] = Inflector::humanize($this->module_name);
+		$new_module['PaymentMethod']['alias'] = $this->module_name;
+
+		$new_module['PaymentMethodValue'][0]['payment_method_id'] = $this->PaymentMethod->id;
+		$new_module['PaymentMethodValue'][0]['key'] = 'assist_shop_id';
+		$new_module['PaymentMethodValue'][0]['value'] = '';
+
+		$this->PaymentMethod->saveAll($new_module);
+
+		$this->Session->setFlash(__('Module Installed', true));
+		$this->redirect('/payment_methods/admin/');
+	}
+
+	function uninstall()
+	{
+
+		$module_id = $this->PaymentMethod->findByAlias($this->module_name);
+
+		$this->PaymentMethod->del($module_id['PaymentMethod']['id'], true);
+			
+		$this->Session->setFlash(__('Module Uninstalled', true));
+		$this->redirect('/payment_methods/admin/');
+	}
+
+	function before_process () 
+	{
+			
+		$order = $this->Order->read(null,$_SESSION['Customer']['order_id']);
+		
+		$payment_method = $this->PaymentMethod->find(array('alias' => $this->module_name));
+
+		$assist_settings = $this->PaymentMethod->PaymentMethodValue->find(array('key' => 'assist_shop_id'));
+		$assist_shop_id = $assist_settings['PaymentMethodValue']['value'];
+		$return_url = 'http://'.$_SERVER['HTTP_HOST'] .  BASE . '/orders/place_order/';
+		
+		$content = '<form action="https://secure.assist.ru/shops/purchase.cfm" method="post">
+			<input type="hidden" name="Shop_IDP" value="'.$assist_shop_id.'">
+			<input type="hidden" name="Order_IDP" value="' . $_SESSION['Customer']['order_id'] . '">
+			<input type="hidden" name="Subtotal_P" value="' . $order['Order']['total'] . '">
+			<input type="hidden" name="Currency" value="' . $_SESSION['Customer']['currency_code'] . '">
+			<input type="hidden" name="URL_RETURN" value="' . $return_url . '">
+			<input type="hidden" name="Comment" value="' . $_SESSION['Customer']['order_id'] . ' ' . $order['Order']['email'] . '">';
+						
+		$content .= '
+			<span class="button"><button type="submit" value="{lang}Process to Payment{/lang}">{lang}Process to Payment{/lang}</button></span>
+			</form>';
+	
+	// Save the order
+	
+		foreach($_POST AS $key => $value)
+			$order['Order'][$key] = $value;
+		
+		// Get the default order status
+		$default_status = $this->Order->OrderStatus->find(array('default' => '1'));
+		$order['Order']['order_status_id'] = $default_status['OrderStatus']['id'];
+
+		// Save the order
+		$this->Order->save($order);
+
+		return $content;
+	}
+
+	function after_process()
+	{
+	}
+	
+	
+	function result()
+	{
+		$this->layout = 'empty';
+	}
+	
+}
+
+?>
---
+++ app/plugins/payment/views/assist/settings.ctp
@@ -0,0 +1,22 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   VaM Cart
+   http://vamcart.com
+   http://vamcart.ru
+   Copyright 2009 VaM Cart
+   -----------------------------------------------------------------------------------------
+   Portions Copyright:
+   Copyright 2007 by Kevin Grandon (kevingrandon@hotmail.com)
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------------------------*/
+
+echo $form->inputs(array(
+	'legend' => null,
+	'assist.assist_shop_id' => array(
+	'label' => __('Assist Shop ID', true),
+	'value' => $data['PaymentMethodValue'][0]['value']
+	)
+	
+));
+?>
