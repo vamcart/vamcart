31616108ba482f68d01d15413dfe4622eaa3c86c
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2010-01-16 18:35:16 +0000

    Copyright date change

--- app/controllers/install_controller.php
+++ app/controllers/install_controller.php
@@ -3,7 +3,7 @@
    VaM Cart
    http://vamcart.com
    http://vamcart.ru
-   Copyright 2010 VaM Cart
+   Copyright 2009-2010 VaM Cart
    -----------------------------------------------------------------------------------------
    Portions Copyright:
    Copyright 2007 by Kevin Grandon (kevingrandon@hotmail.com)
@@ -135,7 +135,7 @@ class InstallController extends AppController {
    VaM Cart
    http://vamcart.com
    http://vamcart.ru
-   Copyright 2010 VaM Cart
+   Copyright 2009-2010 VaM Cart
    -----------------------------------------------------------------------------------------
    Portions Copyright:
    Copyright 2007 by Kevin Grandon (kevingrandon@hotmail.com)
