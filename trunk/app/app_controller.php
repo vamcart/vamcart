95cbe7141fe29e6fe780c8dd10ba555ad10e2998
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2010-02-12 17:01:09 +0000

    Перевод seo url адресов в транслит реализован через компонент translit

--- trunk/app/app_controller.php
+++ trunk/app/app_controller.php
@@ -13,7 +13,7 @@
 
 class AppController extends Controller {
 	var $helpers = array('Html','Javascript','Ajax','Form','Admin', 'Asset');
-	var $components = array('RequestHandler','ConfigurationBase','CurrencyBase','OrderBase');
+	var $components = array('RequestHandler','ConfigurationBase','CurrencyBase','OrderBase', 'Translit');
 	var $layout = 'admin';
 
 	/**
@@ -128,90 +128,11 @@ class AppController extends Controller {
 		if($alias == "")
 			$alias = rand(1000,9999);
 			
-		//Replace cyrillic symbols to translit
-		$trdic = array(
-		"ё"=>"jo",
-		"ж"=>"zh",
-		"ф"=>"ph",
-		"х"=>"kh",
-		"ц"=>"ts",
-		"ч"=>"ch",
-		"ш"=>"sh",
-		"щ"=>"sch",
-		"э"=>"je",
-		"ю"=>"ju",
-		"я"=>"ja",
-		
-		"а"=>"a",
-		"б"=>"b",
-		"в"=>"v",
-		"г"=>"g",
-		"д"=>"d",
-		"е"=>"e",
-		"з"=>"z",
-		"и"=>"i",
-		"й"=>"j",
-		"к"=>"k",
-		"л"=>"l",
-		"м"=>"m",
-		"н"=>"n",
-		"о"=>"o",
-		"п"=>"p",
-		"р"=>"r",
-		"с"=>"s",
-		"т"=>"t",
-		"у"=>"u",
-		"х"=>"h",
-		"ц"=>"c",
-		"ы"=>"y",
-		
-		"Ё"=>"E",
-		"Ж"=>"ZH",
-		"Ф"=>"PH",
-		"Х"=>"KH",
-		"Ц"=>"TS",
-		"Ч"=>"CH",
-		"Ш"=>"SH",
-		"Щ"=>"SCH",
-		"Э"=>"JE",
-		"Ю"=>"JU",
-		"Я"=>"JA",
-		
-		"А"=>"A",
-		"Б"=>"B",
-		"В"=>"V",
-		"Г"=>"G",
-		"Д"=>"D",
-		"Е"=>"E",
-		"З"=>"Z",
-		"И"=>"I",
-		"Й"=>"J",
-		"К"=>"K",
-		"Л"=>"L",
-		"М"=>"M",
-		"Н"=>"N",
-		
-		"О"=>"O",
-		"П"=>"P",
-		"Р"=>"R",
-		"С"=>"S",
-		"Т"=>"T",
-		"У"=>"U",
-		"Х"=>"H",
-		"Ц"=>"C",
-		"Ы"=>"Y",
-		
-		// -----------------------
-		"Ъ" => "",
-		"Ь" => "",
-		"ъ" => "",
-		"ь" => ""
-		);			
-			
 		$alias = trim($alias);
 		$alias = strtolower($alias);
 		$alias = str_replace(' ','-',$alias);
-		$alias = strtr(stripslashes($alias), $trdic);
+		//Replace cyrillic symbols to translit
+		$alias = $this->Translit->convert($alias);
 		$alias = preg_replace("/[^a-zA-Z0-9-s]/", "", $alias);
 		
 		return $alias;
