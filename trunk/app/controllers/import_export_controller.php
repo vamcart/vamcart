3962107cf93f6c1d52313cf158a1cb5a6da34578
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2010-02-16 18:17:27 +0000

    Добавлен контроллер import_export для будущего модуля импорта/экспорта

---
+++ trunk/app/controllers/import_export_controller.php
@@ -0,0 +1,27 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   VaM Cart
+   http://vamcart.com
+   http://vamcart.ru
+   Copyright 2009-2010 VaM Cart
+   -----------------------------------------------------------------------------------------
+   Portions Copyright:
+   Copyright 2007 by Kevin Grandon (kevingrandon@hotmail.com)
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------------------------*/
+
+class ImportExportController extends AppController {
+
+	var $name = 'ImportExport';
+	var $uses = null;
+
+	function admin ($ajax = false)
+	{
+		$this->set('current_crumb', __('Import/Export', true));
+		$this->pageTitle = __('Import/Export', true);
+	}
+	
+}
+
+?>
---
+++ trunk/app/views/import_export/admin.ctp
@@ -0,0 +1,20 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   VaM Cart
+   http://vamcart.com
+   http://vamcart.ru
+   Copyright 2009-2010 VaM Cart
+   -----------------------------------------------------------------------------------------
+   Portions Copyright:
+   Copyright 2007 by Kevin Grandon (kevingrandon@hotmail.com)
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------------------------*/
+
+echo $admin->ShowPageHeaderStart($current_crumb, 'import.png');
+
+echo '&nbsp;';
+
+echo $admin->ShowPageHeaderEnd();
+
+?>
