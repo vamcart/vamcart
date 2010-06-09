cfe04a45ddce91bd4c58ea6b6b7c2bd890a7b830
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2010-06-09 06:31:48 +0000

    Добавлен новый модуль доставки - EMS Почта России

---
+++ trunk/app/plugins/shipping/controllers/russian_post_ems_controller.php
@@ -0,0 +1,95 @@
+<?php 
+/* -----------------------------------------------------------------------------------------
+   VaM Cart
+   http://vamcart.com
+   http://vamcart.ru
+   Copyright 2009-2010 VaM Cart
+   -----------------------------------------------------------------------------------------
+   Portions Copyright:
+   Copyright 2007 by Kevin Grandon (kevingrandon@hotmail.com)
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------------------------*/
+
+class RussianPostEmsController extends ShippingAppController {
+	var $uses = array('ShippingMethod');
+	var $module_name = 'russian_post_ems';
+	var $icon = 'ems.png';
+
+	function settings ()
+	{
+		$this->set('data', $this->ShippingMethod->findByCode($this->module_name));
+	}
+
+	function install()
+	{
+
+		$new_module = array();
+		$new_module['ShippingMethod']['active'] = '1';
+		$new_module['ShippingMethod']['default'] = '1';
+		$new_module['ShippingMethod']['name'] = Inflector::humanize($this->module_name);
+		$new_module['ShippingMethod']['icon'] = $this->icon;
+		$new_module['ShippingMethod']['code'] = $this->module_name;
+
+		$new_module['ShippingMethodValue'][0]['shipping_method_id'] = $this->ShippingMethod->id;
+		$new_module['ShippingMethodValue'][0]['key'] = 'city';
+		$new_module['ShippingMethodValue'][0]['value'] = 'Москва';
+
+		$this->ShippingMethod->saveAll($new_module);
+
+		$this->Session->setFlash(__('Module Installed', true));
+		$this->redirect('/shipping_methods/admin/');
+	}
+
+	function uninstall()
+	{
+
+		$module_id = $this->ShippingMethod->findByCode($this->module_name);
+
+		$this->ShippingMethod->delete($module_id['ShippingMethod']['id'], true);
+			
+		$this->Session->setFlash(__('Module Uninstalled', true));
+		$this->redirect('/shipping_methods/admin/');
+	}
+
+	function calculate ()
+	{
+		App::import('Model', 'ShippingMethod');
+		$this->ShippingMethod =& new ShippingMethod();
+		
+		$method = $this->ShippingMethod->findByCode($this->module_name);
+
+        $from_city = strtolower('city--'.MODULE_SHIPPING_RUSSIANPOSTEMS_CITY);
+        $to_city = strtolower('city--'.vam_cleanName($this->_makeAlias($_POST['city'])));
+        
+        $url = 'http://emspost.ru/api/rest?method=ems.calculate&from='.$from_city.'&to='.$to_city.'&weight='.$shipping_weight;
+
+        // create curl resource
+        $ch = curl_init();
+
+        // set url
+        curl_setopt($ch, CURLOPT_URL, $url);
+
+        //return the transfer as a string
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        // $output contains the output string
+        $output = curl_exec($ch);
+
+        // close curl resource to free up system resources
+        curl_close($ch);  
+
+        $contents = $output;
+        $contents = utf8_encode($contents);
+        $results = json_decode($contents, true); 
+
+		return $results['rsp']['price'];
+	}
+
+	function before_process()
+	{
+	}
+	
+}
+
+?>
---
+++ trunk/app/plugins/shipping/views/russian_post_ems/settings.ctp
@@ -0,0 +1,23 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   VaM Cart
+   http://vamcart.com
+   http://vamcart.ru
+   Copyright 2009-2010 VaM Cart
+   -----------------------------------------------------------------------------------------
+   Portions Copyright:
+   Copyright 2007 by Kevin Grandon (kevingrandon@hotmail.com)
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------------------------*/
+
+echo $form->inputs(array(
+	'legend' => null,
+	'key_values.city' => array(
+		'label' => __('Store City',true), 
+		'type' => 'text',
+		'value' => $data['ShippingMethodValue'][0]['value']
+	)
+));
+
+?>
