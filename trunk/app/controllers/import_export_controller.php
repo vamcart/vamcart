6e993a96bdca1e5f8ee81ec17df5e98caa2f06b2
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2010-09-09 13:16:08 +0000

    Корректировка кода в модуле импорта/экспорта, добавлен код для будущего импорта YML файла

--- trunk/app/controllers/import_export_controller.php
+++ trunk/app/controllers/import_export_controller.php
@@ -21,6 +21,30 @@ class ImportExportController extends AppController {
 		$this->set('current_crumb', __('Import/Export', true));
 		$this->set('title_for_layout', __('Import/Export', true));
 	}
+
+	function import ($params)
+	{
+
+		// If they pressed cancel
+		if(isset($this->params['form']['cancel']))
+		{
+			$this->redirect('/import_export/admin/');
+			die();
+		}
+		
+  $val = $this->data['ImportExport']['submittedfile'];
+ if ( (!empty( $this->data['ImportExport']['submittedfile']['tmp_name']) && $this->data['ImportExport']['submittedfile']['tmp_name'] != 'none')) {
+ $this->Session->setFlash( __('File Uploaded', true));		
+ }
+ $this->Session->setFlash( __('File Not Uploaded', true));		
+
+	$xml = simplexml_load_file($this->data['ImportExport']['submittedfile']["tmp_name"]);
+
+		$this->set('xml', $xml);
+		
+		//$this->redirect('/import_export/admin/');
+	
+	}
 	
 }
 
