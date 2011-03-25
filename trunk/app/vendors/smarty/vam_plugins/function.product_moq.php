0c2a5a27180d7284b6423e795274bcf7652928a6
david.lednik@gmail.com <david.lednik@gmail.com@fde51352-d033-11de-a95b-c1c8cd607182>
2011-03-25 16:33:27 +0000

    Functions to show the minimum order quantity and packet quantities in templates

---
+++ trunk/app/vendors/smarty/vam_plugins/function.product_moq.php
@@ -0,0 +1,42 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   VaM Cart
+   Copyright 2011 David Lednik
+   -----------------------------------------------------------------------------------------
+   Portions Copyright:
+   Copyright 2011 by David Lednik (david.lednik@gmail.com)
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------------------------*/
+
+function smarty_function_product_moq($params, &$smarty)
+{
+    global $content;
+
+    echo $content['ContentProduct']['moq'];
+}
+
+function smarty_help_function_product_moq () {
+	?>
+	<h3><?php echo __('What does this tag do?') ?></h3>
+	<p><?php echo __('Displays the product minimum order quantity.') ?></p>
+	<h3><?php echo __('How do I use it?') ?></h3>
+	<p><?php echo __('Just insert the tag into your template like:') ?> <code>{product_moq}</code></p>
+	<h3><?php echo __('What parameters does it take?') ?></h3>
+	<ul>
+		<li><em>(<?php echo __('None') ?>)</em></li>
+	</ul>
+	<?php
+}
+
+function smarty_about_function_product_moq () {
+	?>
+	<p><?php echo __('Author: David Lednik &lt;david.lednik@gmail.com&gt;') ?></p>
+	<p><?php echo __('Version:') ?> 0.1</p>
+	<p>
+	<?php echo __('Change History:') ?><br/>
+	<?php echo __('None') ?>
+	</p>
+	<?php
+}
+?>
