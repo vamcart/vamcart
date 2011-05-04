86b502b8415f37ff132e1d3eac5583231ca56695
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2011-05-04 17:09:23 +0000

    Added model, view, controller for bookmarks

---
+++ app/models/bookmark.php
@@ -0,0 +1,17 @@
+<?php 
+/* -----------------------------------------------------------------------------------------
+   VaM Cart
+   http://vamcart.com
+   http://vamcart.ru
+   Copyright 2009-2010 VaM Cart
+   -----------------------------------------------------------------------------------------
+   Portions Copyright:
+   Copyright 2007 by Kevin Grandon (kevingrandon@hotmail.com)
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------------------------*/
+
+class Module extends AppModel {
+   var $name = 'Module';
+}
+?>
