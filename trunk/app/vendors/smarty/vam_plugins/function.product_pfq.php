77fb667e5625d41c44f24c4acf34cd7c4c7d66ab
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2011-05-27 12:31:40 +0000

    Добавлены новые smarty модули

---
+++ trunk/app/vendors/smarty/vam_plugins/function.product_pfq.php
@@ -0,0 +1,46 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   VaM Cart
+   Copyright 2011 David Lednik
+   -----------------------------------------------------------------------------------------
+   Portions Copyright:
+   Copyright 2011 by David Lednik (david.lednik@gmail.com)
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------------------------*/
+
+function smarty_function_product_pfq($params, &$smarty)
+{
+    global $content;
+    if ($content['ContentProduct']['stock'] == -1)
+    {
+        return '';
+    }
+
+    echo __('Packet quantity', true) . ': ' . $content['ContentProduct']['pf'];
+}
+
+function smarty_help_function_product_pfq () {
+	?>
+	<h3><?php echo __('What does this tag do?') ?></h3>
+	<p><?php echo __('Displays the product packet quantity.') ?></p>
+	<h3><?php echo __('How do I use it?') ?></h3>
+	<p><?php echo __('Just insert the tag into your template like:') ?> <code>{product_pfq}</code></p>
+	<h3><?php echo __('What parameters does it take?') ?></h3>
+	<ul>
+		<li><em>(<?php echo __('None') ?>)</em></li>
+	</ul>
+	<?php
+}
+
+function smarty_about_function_product_pfq () {
+	?>
+	<p><?php echo __('Author: David Lednik &lt;david.lednik@gmail.com&gt;') ?></p>
+	<p><?php echo __('Version:') ?> 0.1</p>
+	<p>
+	<?php echo __('Change History:') ?><br/>
+	<?php echo __('None') ?>
+	</p>
+	<?php
+}
+?>
