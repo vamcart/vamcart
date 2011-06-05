11bf4b9295ac4c39c656d5ca94b65c15f264cf70
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2011-06-05 16:32:30 +0000

    Корректировка кода по умолчанию

--- trunk/app/vendors/smarty/vam_plugins/function.checkout.php
+++ trunk/app/vendors/smarty/vam_plugins/function.checkout.php
@@ -17,7 +17,7 @@ $template = '
 <script type="text/javascript" src="{base_path}/js/jquery/jquery.min.js"></script>
 <script type="text/javascript" src="{base_path}/js/modified.js"></script>
 <script type="text/javascript" src="{base_path}/js/focus-first-input.js"></script>
-{literal}
+
 <script type="text/javascript">
 	$(document).ready(function() {
 		$("div#ship_information").hide();
@@ -27,7 +27,7 @@ $template = '
 			});
 	});
 </script>
-{/literal}
+
 <div id="checkout">
 <form action="{$checkout_form_action}" method="post" id="contentform">
 	<div id="shipping_method">
