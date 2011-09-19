56f4021db520b47a26b21d8a383007e1377b0ec4
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2011-09-19 09:52:28 +0000

    Добавлен код Яндекс.Метрики

--- app/vendors/smarty/vam_plugins/function.yandex_metrika.php
+++ app/vendors/smarty/vam_plugins/function.yandex_metrika.php
@@ -8,7 +8,7 @@
    Released under the GNU General Public License
    ---------------------------------------------------------------------------------------*/
 
-function smarty_function_google_analytics($params, $template)
+function smarty_function_yandex_metrika($params, $template)
 {
 	
 	global $order;		
@@ -17,7 +17,7 @@ function smarty_function_google_analytics($params, $template)
 	$params['checkout_success'] = (!isset($params['checkout_success'])) ? false : true;
 	$result = '';
 	
-	if ($config['GOOGLE_ANALYTICS'] != '') {
+	if ($config['YANDEX_METRIKA'] != '') {
 
 	$_SERVER['QUERY_STRING'] = str_replace('url=','',$_SERVER['QUERY_STRING']);
 	
@@ -26,37 +26,39 @@ function smarty_function_google_analytics($params, $template)
 
 // Prepare the Analytics "Transaction line" string
 
-	$transaction_string = '\'' . $order['Order']['id'] . '\','."\n".'\'' . $config['SITE_NAME'] . '\','."\n".'\'' . $order['Order']['total'] . '\','."\n".'\'' . $order['Order']['tax'] . '\','."\n".'\'' . $order['Order']['shipping'] . '\','."\n".'\'' . $order['Order']['bill_city'] . '\','."\n".'\'' . $order['Order']['bill_state'] . '\','."\n".'\'' . $order['Order']['bill_country'] . '\'';
+	$transaction_string = 'order_id: "' . $order['Order']['id'] . '",'."\n".'order_price: ' . number_format($order['Order']['total'],2,'.','') . ','."\n".'currency: "' . $_SESSION['Customer']['currency_code'] . '",'."\n".'exchange_rate: 1,';
 
 // Get products info for Analytics "Item lines"
 
 	$item_string = '';
     foreach($order['OrderProduct'] AS $items) {
-	  $item_string .=  '_gaq.push([\'_addItem\','."\n".'\'' . $order['Order']['id'] . '\','."\n".'\'' . $items['id'] . '\','."\n".'\'' . htmlspecialchars($items['name']) . '\','."\n".'\'' . htmlspecialchars($items['Content']['parent_id']) . '\','."\n".'\'' . $items['price'] . '\','."\n".'\'' . $items['quantity'] . '\''."\n".']);'."\n";
+	  $item_string .=  '{'."\n".'id: "' . htmlspecialchars($items['id']) . '",'."\n".'name: "' . htmlspecialchars($items['name']) . '",'."\n".'price: ' . number_format($items['price'],2,'.','') . ','."\n".'quantity: ' . number_format($items['quantity']) . ''."\n".'},'."\n";
     }
-
+    
 			$result = '
 			
 <script type="text/javascript">
-  var _gaq = _gaq || [];
-  _gaq.push([\'_setAccount\', \'' . $config['GOOGLE_ANALYTICS'] . '\']);
-  _gaq.push([\'_trackPageview\']);
-  _gaq.push([\'_trackPageLoadTime\']);
-
-   _gaq.push([\'_addTrans\',
-' . $transaction_string . '
-]);
-
-' . $item_string . '
-  _gaq.push([\'_trackTrans\']); //submits transaction to the Analytics servers
-
-  (function() {
-    var ga = document.createElement(\'script\'); ga.type = \'text/javascript\'; ga.async = true;
-    ga.src = (\'https:\' == document.location.protocol ? \'https://ssl\' : \'http://www\') + \'.google-analytics.com/ga.js\';
-    var s = document.getElementsByTagName(\'script\')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
+var yaParams = {
+	
+'.$transaction_string.'	
+  goods: 
+     [
+'.$item_string.'	
+      ]
+	};
 </script>
+<div style="display:none;"><script type="text/javascript">
+(function(w, c) {
+    (w[c] = w[c] || []).push(function() {
+        try {
+            w.yaCounter' . $config['YANDEX_METRIKA'] . ' = new Ya.Metrika({id:' . $config['YANDEX_METRIKA'] . ', enableAll: true,ut:"noindex",params:window.yaParams||{ }});
+        }
+        catch(e) { }
+    });
+})(window, \'yandex_metrika_callbacks\');
+</script></div>
+<script src="//mc.yandex.ru/metrika/watch.js" type="text/javascript" defer="defer"></script>
+<noscript><div><img src="//mc.yandex.ru/watch/' . $config['YANDEX_METRIKA'] . '" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 			
 			';
 			break;		
@@ -64,20 +66,25 @@ function smarty_function_google_analytics($params, $template)
 			if (($_SERVER['QUERY_STRING'] != 'page/confirmation' . $config['URL_EXTENSION'])) {
 			$result = '
 
+<!-- Yandex.Metrika counter -->
 <script type="text/javascript">
+var yaParams = {};
+</script>
 
-  var _gaq = _gaq || [];
-  _gaq.push([\'_setAccount\', \''.$config['GOOGLE_ANALYTICS'].'\']);
-  _gaq.push([\'_trackPageview\']);
-  _gaq.push([\'_trackPageLoadTime\']);
-
-  (function() {
-    var ga = document.createElement(\'script\'); ga.type = \'text/javascript\'; ga.async = true;
-    ga.src = (\'https:\' == document.location.protocol ? \'https://ssl\' : \'http://www\') + \'.google-analytics.com/ga.js\';
-    var s = document.getElementsByTagName(\'script\')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>			
+<div style="display:none;"><script type="text/javascript">
+(function(w, c) {
+    (w[c] = w[c] || []).push(function() {
+        try {
+            w.yaCounter'.$config['YANDEX_METRIKA'].' = new Ya.Metrika({id:'.$config['YANDEX_METRIKA'].', enableAll: true,ut:"noindex",params:window.yaParams||{ }});
+        }
+        catch(e) { }
+    });
+})(window, \'yandex_metrika_callbacks\');
+</script></div>
+<script src="//mc.yandex.ru/metrika/watch.js" type="text/javascript" defer="defer"></script>
+<noscript><div><img src="//mc.yandex.ru/watch/'.$config['YANDEX_METRIKA'].'" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+		
 			
 			';
 			}
@@ -87,20 +94,20 @@ function smarty_function_google_analytics($params, $template)
 	return $result;
 }
 
-function smarty_help_function_google_analytics() {
+function smarty_help_function_yandex_metrika() {
 	?>
 	<h3><?php echo __('What does this tag do?') ?></h3>
-	<p><?php echo __('Displays the google analytics code.') ?></p>
+	<p><?php echo __('Displays the yandex metrika code.') ?></p>
 	<h3><?php echo __('How do I use it?') ?></h3>
-	<p><?php echo __('Just insert the tag into your template like:') ?> <code>{google_analytics}</code></p>
+	<p><?php echo __('Just insert the tag into your template like:') ?> <code>{yandex_metrika}</code></p>
 	<h3><?php echo __('What parameters does it take?') ?></h3>
 	<ul>
-		<li><em><?php echo __('(checkout_success)') ?></em> - <?php echo __('(true or false) If set to true display the display google analytics ecommerce tracking code in checkout success page.') ?></li>		
+		<li><em><?php echo __('(checkout_success)') ?></em> - <?php echo __('(true or false) If set to true display the display yandex metrika ecommerce tracking code in checkout success page.') ?></li>		
 	</ul>
 	<?php
 }
 
-function smarty_about_function_google_analytics() {
+function smarty_about_function_yandex_metrika() {
 	?>
 	<p><?php echo __('Author: Alexandr Menovchicov &lt;vam@kypi.ru&gt;') ?></p>
 	<p><?php echo __('Version:') ?> 0.1</p>
