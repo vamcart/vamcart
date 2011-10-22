aa5c55c03372318232be5488f03725244cc89652
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2011-10-22 17:46:42 +0000

    Корректировка кода

--- trunk/app/controllers/contents_controller.php
+++ trunk/app/controllers/contents_controller.php
@@ -2,9 +2,6 @@
 /* -----------------------------------------------------------------------------------------
    VamCart - http://vamcart.com
    -----------------------------------------------------------------------------------------
-   Portions Copyright:
-   Copyright 2007 by Kevin Grandon (kevingrandon@hotmail.com)
-   -----------------------------------------------------------------------------------------
    Released under the GNU General Public License
    ---------------------------------------------------------------------------------------*/
 
@@ -401,6 +398,7 @@ class ContentsController extends AppController {
 					$this->set('menu_checked',' ');					
 			}
 		
+			$this->set('content_id',$content_id);
 			$this->set('data',$data);
 			
 			// Content type
@@ -574,6 +572,9 @@ class ContentsController extends AppController {
 		
 		$this->set('content_data', $content_data);
 		$this->set('content_count', $this->Content->find('count', array('conditions' => array('Content.parent_id' => $parent_id))));
+		$last_content_id = $this->Content->find('first', array('order' => array('Content.id DESC')));
+		$last_content_id = $last_content_id['Content']['id']+1;
+		$this->set('last_content_id', $last_content_id);
 	}
 
 }
