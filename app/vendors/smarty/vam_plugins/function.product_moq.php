e01e86666f046d6116af346ff4cdce1cd766b036
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2011-11-22 12:40:17 +0000

    Корректировка кода

--- app/vendors/smarty/vam_plugins/function.product_moq.php
+++ app/vendors/smarty/vam_plugins/function.product_moq.php
@@ -31,13 +31,5 @@ function smarty_help_function_product_moq () {
 }
 
 function smarty_about_function_product_moq () {
-	?>
-	<p><?php echo __('Author: David Lednik &lt;david.lednik@gmail.com&gt;') ?></p>
-	<p><?php echo __('Version:') ?> 0.1</p>
-	<p>
-	<?php echo __('Change History:') ?><br/>
-	<?php echo __('None') ?>
-	</p>
-	<?php
 }
 ?>
