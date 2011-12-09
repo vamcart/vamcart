28ac5547fa9e96b8c1b897208935ef1f2386640b
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2011-12-09 06:29:31 +0000

    Добавлена модель ConfigurationGroup

---
+++ trunk/app/models/configuration_group.php
@@ -0,0 +1,13 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   VamCart - http://vamcart.com
+   -----------------------------------------------------------------------------------------
+   Copyright (c) 2011 VamSoft Ltd.
+   License - http://vamcart.com/license.html
+   ---------------------------------------------------------------------------------------*/
+
+class ConfigurationGroup extends AppModel {
+	var $name = 'ConfigurationGroup';
+	var $hasMany = array('Configuration' => array('dependent' => true));	
+}
+?>
