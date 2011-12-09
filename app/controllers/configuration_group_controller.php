00baeb3d7982fc8d527132fe1537dea30af400f5
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2011-12-09 16:09:46 +0000

    Добавлен контроллер configuration_grup, для вывода настроек из базу данных

---
+++ app/controllers/configuration_group_controller.php
@@ -0,0 +1,20 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   VamCart - http://vamcart.com
+   -----------------------------------------------------------------------------------------
+   Copyright (c) 2011 VamSoft Ltd.
+   License - http://vamcart.com/license.html
+   ---------------------------------------------------------------------------------------*/
+
+class ConfigurationGroupController extends AppController {
+	var $name = 'ConfigurationGroup';
+	
+	function admin ()
+	{
+		$this->set('current_crumb', __('Store Configuration', true));
+		$this->set('title_for_layout', __('Store Configuration', true));
+		
+		$this->set('data',$this->ConfigurationGroup->find('all'));
+	}
+}
+?>
