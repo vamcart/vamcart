30e28dcb8e1ac8c2d5cfbd62cdf9150ab9baecb4
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2013-01-31 16:03:25 +0000

    Добавлены новые конфиги для CakePHP 2.3

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -1,43 +1,36 @@
 <?php
-/* SVN FILE: $Id: routes.php 7945 2008-12-19 02:16:01Z gwoo $ */
 /**
- * Short description for file.
+ * Routes configuration
  *
  * In this file, you set up routes to your controllers and their actions.
  * Routes are very important mechanism that allows you to freely connect
  * different urls to chosen controllers and their actions (functions).
  *
- * PHP versions 4 and 5
+ * PHP 5
  *
- * CakePHP(tm) :  Rapid Development Framework (http://www.cakephp.org)
- * Copyright 2005-2008, Cake Software Foundation, Inc. (http://www.cakefoundation.org)
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
  *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
- * @filesource
- * @copyright     Copyright 2005-2008, Cake Software Foundation, Inc. (http://www.cakefoundation.org)
- * @link          http://www.cakefoundation.org/projects/info/cakephp CakePHP(tm) Project
- * @package       cake
- * @subpackage    cake.app.config
+ * @copyright     Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @package       app.Config
  * @since         CakePHP(tm) v 0.2.9
- * @version       $Revision: 7945 $
- * @modifiedby    $LastChangedBy: gwoo $
- * @lastmodified  $Date: 2008-12-18 18:16:01 -0800 (Thu, 18 Dec 2008) $
- * @license       http://www.opensource.org/licenses/mit-license.php The MIT License
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 /**
  * Here, we are connecting '/' (base path) to controller called 'Pages',
  * its action called 'display', and we pass a param to select the view file
- * to use (in this case, /app/views/pages/home.ctp)...
+ * to use (in this case, /app/View/Pages/home.ctp)...
  */
-//	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
-/**
- * ...and connect the rest of 'Pages' controller's urls.
+ 
+ /**
+ * Load all plugin routes.  See the CakePlugin documentation on
+ * how to customize the loading of plugin routes.
  */
-//	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
-
-	Router::parseExtensions('rss','xml');
+ Router::parseExtensions('rss','xml');
 
 	Router::connect('/', array('controller' => 'pages', 'action' => 'index'));
 	Router::connect('/pages/*', array('controller' => 'pages'));
@@ -51,8 +44,19 @@
 	Router::connect('/news/:content_alias', array('controller' => 'pages', 'action' => 'index'));
 	Router::connect('/article/:content_alias', array('controller' => 'pages', 'action' => 'index'));
 	Router::connect('/customer/:content_alias', array('controller' => 'pages', 'action' => 'index'));
-	Router::connect('/google_sitemap', array('controller' => 'sitemaps', 'action' => 'google', 'url' => array('ext' => 'xml')));
-	Router::connect('/yandex_market', array('controller' => 'sitemaps', 'action' => 'yandex', 'url' => array('ext' => 'xml')));
+	Router::connect('/google_sitemap', array('controller' => 'sitemaps', 'action' => 'google', 'ext' => 'xml'));
+	Router::connect('/yandex_market', array('controller' => 'sitemaps', 'action' => 'yandex', 'ext' => 'xml'));
 
 	Router::connect('/tests', array('controller' => 'tests', 'action' => 'index'));
-?>
+	
+/**
+ * Load all plugin routes.  See the CakePlugin documentation on 
+ * how to customize the loading of plugin routes.
+ */
+	CakePlugin::routes();
+
+/**
+ * Load the CakePHP default routes. Only remove this if you do not want to use
+ * the built-in default routes.
+ */
+	require CAKE . 'Config' . DS . 'routes.php';
