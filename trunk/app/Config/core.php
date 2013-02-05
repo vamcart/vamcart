aff8bd829b52cc068db9f38f2f6bf7368f4e0b3b
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2013-02-05 15:50:42 +0000

    Корректировка настроек кэша

--- trunk/app/Config/core.php
+++ trunk/app/Config/core.php
@@ -239,40 +239,42 @@
  *       Please check the comments in boostrap.php for more info on the cache engines available
  *       and their setttings.
  */
-$engine = 'File';
-if (extension_loaded('apc') && function_exists('apc_dec') && (php_sapi_name() !== 'cli' || ini_get('apc.enable_cli'))) {
-	$engine = 'Apc';
-}
+//$engine = 'File';
+//if (extension_loaded('apc') && function_exists('apc_dec') && (php_sapi_name() !== 'cli' || ini_get('apc.enable_cli'))) {
+	//$engine = 'Apc';
+//}
 
 // In development mode, caches should expire quickly.
-$duration = '+999 days';
-if (Configure::read('debug') >= 1) {
-	$duration = '+10 seconds';
-}
+//$duration = '+999 days';
+//if (Configure::read('debug') >= 1) {
+	//$duration = '+10 seconds';
+//}
 
 // Prefix each application on the same server with a different string, to avoid Memcache and APC conflicts.
-$prefix = 'myapp_';
+//$prefix = 'myapp_';
 
 /**
  * Configure the cache used for general framework caching.  Path information,
  * object listings, and translation cache files are stored with this configuration.
  */
-Cache::config('_cake_core_', array(
-	'engine' => $engine,
-	'prefix' => $prefix . 'cake_core_',
-	'path' => CACHE . 'persistent' . DS,
-	'serialize' => ($engine === 'File'),
-	'duration' => $duration
-));
+//Cache::config('_cake_core_', array(
+	//'engine' => $engine,
+	//'prefix' => $prefix . 'cake_core_',
+	//'path' => CACHE . 'persistent' . DS,
+	//'serialize' => ($engine === 'File'),
+	//'duration' => $duration
+//));
 
 /**
  * Configure the cache for model and datasource caches.  This cache configuration
  * is used to store schema descriptions, and table listings in connections.
  */
-Cache::config('_cake_model_', array(
-	'engine' => $engine,
-	'prefix' => $prefix . 'cake_model_',
-	'path' => CACHE . 'models' . DS,
-	'serialize' => ($engine === 'File'),
-	'duration' => $duration
-));
+//Cache::config('_cake_model_', array(
+	//'engine' => $engine,
+	//'prefix' => $prefix . 'cake_model_',
+	//'path' => CACHE . 'models' . DS,
+	//'serialize' => ($engine === 'File'),
+	//'duration' => $duration
+//));
+
+	Cache::config('default', array('engine' => 'File'));
