8115947b3f4424a3bd62d6c9a069e01c9c60d2c9
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2013-04-28 18:45:49 +0000

    Подключён TinyMCE 4

--- app/View/Helper/TinyMceHelper.php
+++ app/View/Helper/TinyMceHelper.php
@@ -12,7 +12,7 @@ class TinyMceHelper extends Helper {
 	}
 	public function init($options=false){
 	$code = '';
-	$code .= $this->Html->script('tiny_mce/jquery.tinymce', false);
+	$code .= $this->Html->script('tinymce/tinymce.min.js', false);
 	if($options){
 	$code .= $this->Html->scriptBlock('
 		tinyMCE.init(
@@ -21,43 +21,26 @@ class TinyMceHelper extends Helper {
 	', array('allowCache'=> false,'safe'=> false,'inline'=> false));
 		} else {
 	$code .= $this->Html->scriptBlock('
-$().ready(function() {
-		$("textarea.pagesmalltextarea").tinymce({
-			// Location of TinyMCE script
-			script_url : "'.BASE.'/js/tiny_mce/tiny_mce.js",
-
-			// General options
-			editor_deselector : "notinymce",
-			theme : "advanced",
-			language : "'.$this->Session->read('Customer.language').'",
-			paste_create_paragraphs : false,
-			paste_create_linebreaks : false,
-			paste_use_dialog : true,
-			convert_urls : false,
-		
-			plugins : "pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,typograf,spellchecker",
-		
-			spellchecker_languages : "+Russian=ru,English=en",
-			spellchecker_rpc_url : "'.BASE.'/js/tiny_mce/plugins/spellchecker/rpc_proxy.php",
-		
-			// Theme options
-			theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect",
-			theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
-			theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
-			theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,spellchecker,typograf,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,blockquote,pagebreak,|,insertfile,insertimage",
-			theme_advanced_toolbar_location : "top",
-			theme_advanced_toolbar_align : "left",
-			theme_advanced_statusbar_location : "bottom",
-			theme_advanced_resizing : true
-
-
-		    });
-	});
+				tinymce.init({
+					selector: "textarea.pagesmalltextarea",
+					plugins: [
+						"advlist autolink lists link image charmap print preview anchor",
+						"searchreplace visualblocks code fullscreen",
+						"insertdatetime media table contextmenu paste"
+					],
+					toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
+					autosave_ask_before_unload: false,
+					max_height: 200,
+					min_height: 160,
+					height : 180,
+					'.('ru' == $this->Session->read('Customer.language') ? 'language : "ru"' : '').'
+				});
+ 	
 		function toggleHTMLEditor(id) {
 			if (!tinyMCE.get(id))
-				tinyMCE.execCommand("mceAddControl", false, id);
+				tinymce.execCommand("mceAddControl", false, id);
 			else
-				tinyMCE.execCommand("mceRemoveControl", false, id);
+				tinymce.execCommand("mceRemoveControl", false, id);
 		}    	
 	
 	', array('allowCache'=> false,'safe'=> false,'inline'=> false));
