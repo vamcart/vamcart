77f03c29f7fe30a71ff71a5fff4a21f9a7cbdf9f
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2013-05-22 08:46:51 +0000

    Добавлены модели для нового модуля шаблоны ответов

---
+++ trunk/app/Model/AnswerTemplateDescription.php
@@ -0,0 +1,13 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   VamCart - http://vamcart.com
+   -----------------------------------------------------------------------------------------
+   Copyright (c) 2011 VamSoft Ltd.
+   License - http://vamcart.com/license.html
+   ---------------------------------------------------------------------------------------*/
+App::uses('Model', 'AppModel');
+class AnswerTemplateDescription extends AppModel {
+	public $name = 'AnswerTemplateDescription';
+	public $belongsTo = array('AnswerTemplate','Language');
+}
+?>
