c04e7179e973531ef3281428d8d75d6cb7eec8a4
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2013-05-27 18:41:04 +0000

    Добавлены отчёты в админке, графики на главной страницы админки, issue 26

---
+++ app/Controller/ReportsController.php
@@ -0,0 +1,127 @@
+<?php
+
+class ReportsController extends AppController {
+    public $name = 'Reports';
+    public $uses = array('Report','Html','Admin','Form');
+    public $helpers = array('FlashChart');
+
+    public function admin($action = 'new')
+    {
+ 
+        $this->set('current_crumb', __('Sales statistics', true));
+        $this->set('title_for_layout', __('Reports', true));
+
+        $order_status = array();
+        $options = array();
+        
+        $l = $this->Session->read('Config.language');
+        if (NULL == $l) {
+            $l = $this->Session->read('Customer.language');
+        }
+        
+        if($action == 'new')
+        {
+            $this->loadModel('OrderStatusDescription');
+            /*вместо статического rus можно использовать $l(содержит текущий код языка)*/
+            $order_status = $this->OrderStatusDescription->find('all',  array('conditions' => array('Language.code' => 'rus')));
+            foreach ($order_status as $k => $status) 
+            {
+                $status['OrderStatus']['default'] = '1';
+                $order_status[$k] = $status;
+
+            }
+            $options = array('period' => 'day'
+                            ,'statuses' => $order_status
+                            ,'stamp_dat' => null
+                            ,'group_dat' => null);
+            $this->Session->write('Reports.options', $options);
+            $this->redirect('/reports/admin/show');
+        }
+        elseif($action == 'show')
+        { 
+            $listStatuses = array();
+            $options = $this->Session->read('Reports.options');
+            
+            foreach ($options['statuses'] as $k => $status) 
+            {
+                if($status['OrderStatus']['default'] == '1') $listStatuses[$k] = $status['OrderStatus']['id'];
+            }
+
+            switch ($options['period'])
+            {
+                case 'hour':
+                    $options['stamp_dat'] = date("Y-m-d H:i:s",time()-(3600));
+                    $options['group_dat'] = '%d %H:%i';
+                break;
+                case 'day':
+                    $options['stamp_dat'] = date("Y-m-d H:i:s",time()-(24*3600));
+                    $options['group_dat'] = '%d %H:00';
+                break;
+                case 'week':
+                    $options['stamp_dat'] = date("Y-m-d H:i:s",time()-(7*24*3600));
+                    $options['group_dat'] = '%m/%d';
+                break;
+                case 'month':
+                    $options['stamp_dat'] = date("Y-m-d H:i:s",time()-(30*24*3600));
+                    $options['group_dat'] = '%m/%d';
+                break;
+                case 'year':
+                    $options['stamp_dat'] = date("Y-m-d H:i:s",time()-(365*24*3600));
+                    $options['group_dat'] = '%Y/%m';
+                break;
+                default :
+                break;
+            }
+            
+            $this->loadModel('Order');
+            $order = $this->Order->find('all', array('fields' => array('DATE_FORMAT(Order.created, \''.$options['group_dat'].'\') as dat','TRUNCATE(SUM(Order.total),2) as summ','COUNT(Order.id) as cnt')
+                                                    ,'conditions' => array('Order.order_status_id' => $listStatuses
+                                                                          ,'Order.created >' => $options['stamp_dat'])
+                                                    ,'group' => array('dat')
+                                                    ,'order' => array('dat')));
+            
+            $result = array();
+            foreach ($order as $k => $ord) 
+            {
+                $result['dat'][$k] = $ord[0]['dat'];
+                $result['cnt'][$k] = $ord[0]['cnt'];
+                $result['summ'][$k] = $ord[0]['summ'];
+            } 
+
+           $this->set('result',$result);
+           $this->set('statuses',$options['statuses']);
+           $this->set('period',$options['period']);
+        }
+        else
+        {
+            $this->redirect('/reports/admin/');
+        }
+
+    }
+    
+    public function admin_change_active_status ($id) 
+    {
+       
+        $options = $this->Session->read('Reports.options');
+
+        if($options['statuses'][$id]['OrderStatus']['default'] == '0')
+        {
+            $options['statuses'][$id]['OrderStatus']['default'] = '1';
+        }
+        else $options['statuses'][$id]['OrderStatus']['default'] = '0';
+        
+        $this->Session->write('Reports.options', $options);
+        $this->redirect('/reports/admin/show');
+       
+    }
+    
+    public function admin_change_date ($date = 'day') 
+    {
+        $options = $this->Session->read('Reports.options');
+        $options['period'] = $date;
+        $this->Session->write('Reports.options', $options);
+        $this->redirect('/reports/admin/show');
+    }
+
+
+}
---
+++ app/Model/Report.php
@@ -0,0 +1,8 @@
+<?php
+
+App::uses('Model', 'AppModel');
+class Report extends AppModel {
+	public $name = 'Report';
+	public $useTable = false;
+}
+?>
