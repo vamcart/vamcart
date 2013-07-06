386af109ea657dc189331a4809000fd7747a3a20
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2013-07-06 17:03:55 +0000

    Добавлен перевод для смарти-метки flash_message

--- trunk/app/Vendor/smarty/vam_plugins/function.flash_message.php
+++ trunk/app/Vendor/smarty/vam_plugins/function.flash_message.php
@@ -14,14 +14,13 @@ function smarty_function_flash_message($params, $template)
 
 	$result = $Session->flash();
 
-
 	return $result;
 }
 
 function smarty_help_function_flash_message() {
 	?>
 	<h3><?php echo __('What does this tag do?') ?></h3>
-	<p><?php echo __('Displays $this->Sesion->flash message.') ?></p>
+	<p><?php echo __('Displays $this->Session->flash() message.') ?></p>
 	<h3><?php echo __('How do I use it?') ?></h3>
 	<p><?php echo __('Just insert the tag into your template like:') ?> <code>{flash_message}</code></p>
 	<h3><?php echo __('What parameters does it take?') ?></h3>
