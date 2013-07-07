f5e151c72edce457efd8f73c73b0312b5e835e4a
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2013-07-07 16:47:20 +0000

    Добавлен новый модуль - Импорт/Экспорт

--- trunk/app/Controller/ImportExportController.php
+++ trunk/app/Controller/ImportExportController.php
@@ -1,45 +1,157 @@
 <?php
-/* -----------------------------------------------------------------------------------------
-   VamCart - http://vamcart.com
-   -----------------------------------------------------------------------------------------
-   Copyright (c) 2011 VamSoft Ltd.
-   License - http://vamcart.com/license.html
-   ---------------------------------------------------------------------------------------*/
+
 class ImportExportController extends AppController {
 
 	public $name = 'ImportExport';
 	public $uses = null;
+        public $helpers = array('Html','Admin','PhpExcel');
 
 	public function admin ($ajax = false)
 	{
-		$this->set('current_crumb', __('Import/Export', true));
-		$this->set('title_for_layout', __('Import/Export', true));
+            //Все связанные с контентом таблицы
+            $tmp_table_name = array();
+            $this->loadModel('Content');
+            $this->Content->Behaviors->attach('Containable');
+            $contents = $this->Content->find('first',array('contain' => array('ContentType','ContentDescription','ContentImage','ContentProduct')));	
+            foreach ($contents AS $k_c => $content)
+            {
+                $myModelname = $k_c;
+                App::import('Model', $myModelname);
+                $this->myModel = new $myModelname();
+                $tmp_table_name[$k_c ] = $this->myModel->getColumnTypes();
+            }
+            $this->Session->write('import_export.table_name', $tmp_table_name);
+            $this->set('table_names', $tmp_table_name);
+            
+            $this->set('current_crumb', __('Import/Export', true));
+            $this->set('title_for_layout', __('Import/Export', true));
 	}
 
-	public function import ($params)
+	public function import ()
 	{
+            if (isset($this->data['form_Import']['submittedfile'])
+			&& $this->data['form_Import']['submittedfile']['error'] == 0
+			&& is_uploaded_file($this->data['form_Import']['submittedfile']['tmp_name'])) 
+            {
+                $file_name = $this->data['form_Import']['submittedfile']['name'];
+                @unlink('./files/' . $file_name); 
+                move_uploaded_file($this->data['form_Import']['submittedfile']['tmp_name'], './files/' . $file_name);
+                $f_inf = pathinfo($file_name);
+                if($f_inf['extension'] == 'zip')
+                {
+                    $zip = new ZipArchive();
+                    $zip->open('./files/' . $file_name);
+                    $res = $zip->extractTo('./files/','vc_content.xls');
+                    $zip->close();
+                    @unlink('./files/' . $file_name); 
+                }
+                else 
+                {
+                    $this->Session->setFlash(__('Error extracting.',true));
+                    $this->redirect('/import_export/admin');  
+                }
+                App::import('Vendor', 'PHPExcel/Classes/PHPExcel');
+                try {
+                $xls = PHPExcel_IOFactory::load('./files/' . 'vc_content.xls');
+                } catch (Exception $e) {
+                die("Error loading file: ".$e->getMessage() . "<br />\n");
+                }
+                $worksheetIterator = $xls->getWorksheetIterator();
+                foreach($worksheetIterator as $worksheet_name => $worksheet)
+                {
+                    $table_name = $worksheet->getTitle();
+                    $rowIterator = $worksheet->getRowIterator();
+                    $rowIterator->resetStart();
+                    $cellIterator = $rowIterator->current()->getCellIterator();
+                    foreach($cellIterator as $k_cell => $cell)
+                    {
+                        $tmp_head[$k_cell] = $cell->getValue();
+                    }
+                    $rowIterator->resetStart(2);
+                    foreach($rowIterator as $k_row => $row)
+                    {
+                        $cellIterator = $row->getCellIterator();
+                        foreach($cellIterator as $k_cell => $cell)
+                        {
+                            $tmp_row[$tmp_head[$k_cell]] = $cell->getValue();
+                        }
+                        $tmp[$table_name][$k_row - 2][$table_name] = $tmp_row;
+                    }
+                    App::import('Model', $table_name);
+                    $this->myModel = new $table_name();
+                    foreach($tmp[$table_name] AS $table_row)
+                    {
+                        $this->myModel->save($table_row);
+                    }
+                }
 
-		// If they pressed cancel
-		if(isset($this->data['cancelbutton']))
-		{
-			$this->redirect('/import_export/admin/');
-			die();
-		}
-		
-  $val = $this->data['ImportExport']['submittedfile'];
- if ( (!empty( $this->data['ImportExport']['submittedfile']['tmp_name']) && $this->data['ImportExport']['submittedfile']['tmp_name'] != 'none')) {
- $this->Session->setFlash( __('File Uploaded', true));		
- }
- $this->Session->setFlash( __('File Not Uploaded', true));		
-
-	$xml = simplexml_load_file($this->data['ImportExport']['submittedfile']["tmp_name"]);
-
-		$this->set('xml', $xml);
-		
-		//$this->redirect('/import_export/admin/');
-	
-	}
-	
+                $this->Session->setFlash(__('Import sucess!', true));
+                $this->redirect('/import_export/admin');
+            }
+            die();
+        }
+        
+        public function export ()
+	{
+            App::import('Vendor', 'PHPExcel/Classes/PHPExcel');
+            if (!class_exists('PHPExcel')) 
+            {
+		throw new CakeException('Vendor class PHPExcel not found!');
+            }
+            $sfx = rand();
+            $zip = new ZipArchive();
+            $res = $zip->open('./files/vc_content' . $sfx . '.zip', ZIPARCHIVE::CREATE | ZIPARCHIVE::OVERWRITE);
+            if ($res === TRUE) 
+            {            
+                $xls = new PHPExcel();
+                $xls->removeSheetByIndex(0);
+                $index = 0;
+                $tmp_table_name = $this->Session->read('import_export.table_name');
+                foreach ($tmp_table_name AS $k_name_table => $fields)
+                {
+                    if($this->data['form_Export'][$k_name_table] == 1)
+                    {
+                        $myWorkSheet = new PHPExcel_Worksheet($xls, $k_name_table);
+                        $myModelname = $k_name_table;
+                        App::import('Model', $myModelname);
+                        $this->myModel = new $myModelname();
+                        $this->myModel->unbindAll();
+                        $rows = $this->myModel->find('all');
+                        $k_cell = 0;
+                        foreach ($fields AS $field_name => $field_type)
+                        {
+                                $myWorkSheet->setCellValueByColumnAndRow($k_cell++, 1, $field_name);    
+                        }
+                        foreach ($rows AS $num_row => $row)
+                        {
+                            $k_cell = 0;
+                            foreach ($row[$k_name_table] AS $field_name => $field_value)
+                            {
+                                $myWorkSheet->setCellValueByColumnAndRow($k_cell++, $num_row + 2, $field_value);    
+                            }
+                        }
+                        $xls->addSheet($myWorkSheet, $index);
+                        $xls->setActiveSheetIndex($index++);
+                    }
+                }
+    
+                $objWriter = PHPExcel_IOFactory::createWriter($xls, 'Excel5');//new PHPExcel_Writer_Excel5($xls);
+                $objWriter->save('./files/vc_content' . $sfx . '.xls');
+                $xls->disconnectWorksheets();                
+                $zip->addFile('./files/vc_content' . $sfx . '.xls','vc_content.xls');
+                $zip->close();                
+                header('Content-Type: application/octet-stream');
+                header('Content-Disposition: attachment; filename="vc_content.zip"');
+                readfile('./files/vc_content' . $sfx . '.zip');
+                @unlink('./files/vc_content' . $sfx . '.zip');
+                @unlink('./files/vc_content' . $sfx . '.xls');
+                /*$this->Session->setFlash(__('Export sucess!', true));
+                $this->redirect('/import_export/admin');*/
+            }
+            else {}            
+            die();          
+        }
+        
 }
 
 ?>
--- trunk/app/View/ImportExport/admin.ctp
+++ trunk/app/View/ImportExport/admin.ctp
@@ -1,26 +1,47 @@
 <?php
-/* -----------------------------------------------------------------------------------------
-   VamCart - http://vamcart.com
-   -----------------------------------------------------------------------------------------
-   Copyright (c) 2011 VamSoft Ltd.
-   License - http://vamcart.com/license.html
-   ---------------------------------------------------------------------------------------*/
+$this->Html->script(array(
+	'selectall.js'
+), array('inline' => false));
 
-echo $this->Admin->ShowPageHeaderStart($current_crumb, 'cus-arrow-in');
+echo $this->Admin->ShowPageHeaderStart($current_crumb, 'cus-database-refresh');
 
-	echo $this->Form->create('ImportExport', array('enctype' => 'multipart/form-data', 'id' => 'contentform', 'action' => '/import_export/import/', 'url' => '/import_export/import/'));
+echo '<ul id="myTab" class="nav nav-tabs">';
+echo $this->Admin->CreateTab('import',__('Import',true), 'cus-arrow-in');
+echo $this->Admin->CreateTab('export',__('Export',true), 'cus-arrow-out');
+echo '</ul>';
 
-	echo $this->Form->input('ImportExport.submittedfile', 
-						array(
-				   		'type' => 'file',
-				   		'label' => __('YML Import'),
-							'between'=>'<br />'
-	               ));
+echo $this->Admin->StartTabs();
 
-	echo $this->Admin->formButton(__('Submit'), 'cus-tick', array('class' => 'btn', 'type' => 'submit', 'name' => 'submit')) . $this->Admin->formButton(__('Cancel'), 'cus-cancel', array('class' => 'btn', 'type' => 'submit', 'name' => 'cancelbutton'));
-	
-	echo '<div class="clear"></div>';
-	echo $this->Form->end();
+echo $this->Admin->StartTabContent('import');
+
+    echo $this->Form->create('form_Import', array('enctype' => 'multipart/form-data', 'id' => 'contentform_import', 'action' => '/import_export/import/', 'url' => '/import_export/import/'));
+    /*echo $this->Form->input('ImportExport.submittedfile', array('type' => 'file','label' => __('YML Import'),'between'=>'<br />'));*/
+    echo $this->Form->file('submittedfile');
+    echo '<br />' . $this->Admin->formButton(__('Submit'), 'cus-tick', array('class' => 'btn', 'type' => 'submit', 'name' => 'submit'));// . $this->Admin->formButton(__('Cancel'), 'cus-cancel', array('class' => 'btn', 'type' => 'submit', 'name' => 'cancelbutton'));	
+    echo $this->Form->end();
+    
+echo $this->Admin->EndTabContent();
+
+echo $this->Admin->StartTabContent('export');
+    //var_dump($table_names);
+    echo $this->Form->create('form_Export', array('id' => 'contentform_export', 'action' => '/import_export/export/', 'url' => '/import_export/export/'));
+    
+    echo '<table class="contentTable">';
+    echo $this->Html->tableHeaders(array(__('Name table'), '<input type="checkbox" onclick="checkAll(this)" />'));
+    foreach ($table_names AS $table_name => $fields)
+    {
+        echo $this->Admin->TableCells(array($table_name
+                                        ,array($this->Form->checkbox($table_name, array('value' => 1)), array('align'=>'center'))
+                                      ));
+    }
+    echo '</table>';
+
+    echo $this->Admin->formButton(__('Submit'), 'cus-tick', array('class' => 'btn', 'type' => 'submit', 'name' => 'submit'));// . $this->Admin->formButton(__('Cancel'), 'cus-cancel', array('class' => 'btn', 'type' => 'submit', 'name' => 'cancelbutton'));	
+    echo $this->Form->end();
+
+echo $this->Admin->EndTabContent();
+
+echo $this->Admin->EndTabs();
 
 echo $this->Admin->ShowPageHeaderEnd();
 
