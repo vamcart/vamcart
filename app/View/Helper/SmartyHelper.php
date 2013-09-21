6169df2682b713d47d84140b925313dfd0b0f9a1
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2013-09-21 07:45:50 +0000

    Добавлен SmartyHelper

---
+++ app/View/Helper/SmartyHelper.php
@@ -0,0 +1,27 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   VamCart - http://vamcart.com
+   -----------------------------------------------------------------------------------------
+   Copyright (c) 2013 VamSoft Ltd.
+   License - http://vamcart.com/license.html
+   ---------------------------------------------------------------------------------------*/
+
+App::uses('AppHelper', 'Helper');
+
+class SmartyHelper extends AppHelper 
+{
+	public function load_smarty ()
+	{
+            App::uses('SmartyComponent', 'Controller/Component');
+            $smarty =& new SmartyComponent(new ComponentCollection());       
+            return $smarty;
+	}
+
+	public function display($str, $assigns = array())
+	{
+            $smarty = $this->load_smarty();
+            $smarty->display($str, $assigns);
+	}
+}
+
+?>
