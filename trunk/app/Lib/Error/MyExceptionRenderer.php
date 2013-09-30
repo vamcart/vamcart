c86ab7dae22ba53e41e887ff7a3745b209b2092d
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2013-09-30 19:01:11 +0000

    Упрощён код MyExeptionRendered

--- trunk/app/Lib/Error/MyExceptionRenderer.php
+++ trunk/app/Lib/Error/MyExceptionRenderer.php
@@ -10,20 +10,8 @@
 
  class MyExceptionRenderer extends ExceptionRenderer 
  {
-    /**
-     * Overrided, to always use a bare controller.
-     * 
-     * @param Exception $exception The exception to get a controller for.
-     * @return Controller
-     */
-    protected function _getController($exception) {
-        if (!$request = Router::getRequest(true)) {
-            $request = new CakeRequest();
-        }
-        $response = new CakeResponse(array('charset' => Configure::read('App.encoding')));
-        $controller = new Controller($request, $response);
-        $controller->viewPath = 'Errors';
-        $controller->layout = 'error';
-        return $controller;
-    }
+  protected function _outputMessage($template) {
+    $this->controller->layout = 'error';
+    parent::_outputMessage($template);
+  }
  }
