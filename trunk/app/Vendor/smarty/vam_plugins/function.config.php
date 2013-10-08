2af3d202fb175278a0c7242250769b0860b5644b
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2013-10-08 05:59:14 +0000

    Добавлен новый смарти-тэг config

---
+++ trunk/app/Vendor/smarty/vam_plugins/function.config.php
@@ -0,0 +1,39 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   VamCart - http://vamcart.com
+   -----------------------------------------------------------------------------------------
+   Copyright (c) 2011 VamSoft Ltd.
+   License - http://vamcart.com/license.html
+   ---------------------------------------------------------------------------------------*/
+
+function smarty_function_config($params, $template)
+{
+	global $config;	
+
+	$value = null;
+	
+	if(isset ($params['value']))
+		$params['value'] = strtoupper($params['value']);
+
+	if (array_key_exists($params['value'], $config))
+		$value = $config[$params['value']];
+
+	return $value;
+}
+
+function smarty_help_function_config() {
+	?>
+	<h3><?php echo __('What does this tag do?') ?></h3>
+	<p><?php echo __('Displays the configuration values.') ?></p>
+	<h3><?php echo __('How do I use it?') ?></h3>
+	<p><?php echo __('Just insert the tag into your template like:') ?> <code>{config value=site_name}</code></p>
+	<h3><?php echo __('What parameters does it take?') ?></h3>
+	<ul>
+		<li><em>(<?php echo __('None') ?>)</em></li>
+	</ul>
+	<?php
+}
+
+function smarty_about_function_config() {
+}
+?>
