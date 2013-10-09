bcccdf011a9c1d543f656951d6bd60659d66efba
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2013-10-09 09:36:18 +0000

    Корректирока шаблонов

--- trunk/app/Plugin/ModuleReviews/Controller/ActionController.php
+++ trunk/app/Plugin/ModuleReviews/Controller/ActionController.php
@@ -21,8 +21,19 @@ class ActionController extends ModuleReviewsAppController {
 	
 	function create ()
 	{
+			global $content, $config;
+
 		if(!empty($_POST))
 		{
+			
+			if(empty($_POST['content_id']))
+			{
+				global $content;
+				$content_id = $content['Content']['id'];	
+			} else {
+				$content_id = $_POST['content_id'];
+			}
+			
 			$new_review = array();
 			$new_review['ModuleReview']['content_id'] = $_POST['content_id'];
 			$new_review['ModuleReview']['name'] = $_POST['name'];
@@ -34,20 +45,33 @@ class ActionController extends ModuleReviewsAppController {
 			$Clean->paranoid($new_review);
 			
 			$this->ModuleReview->save($new_review);
-			
-			global $config;
-			
-			$this->redirect('/page/read-reviews'.$config['URL_EXTENSION'].'?content_id=' . $_POST['content_id']);
+		
+			$this->redirect(BASE . '/' . $content['ContentType']['name'] . '/' . $content['Content']['alias'] . $config['URL_EXTENSION']);
 		}
 		else
 		{
-			$content = $this->ContentBase->get_content_information($_GET['content_id']);
-			$content_description = $this->ContentBase->get_content_description($_GET['content_id']);			
+
+			if(empty($_GET['content_id']))
+			{
+				global $content;
+				$content_id = $content['Content']['id'];	
+			} else {
+				$content_id = $_GET['content_id'];
+			}
+			
+			$content_description = $this->ContentBase->get_content_description($content_id);			
+			
+			$CurrencyBase =& new CurrencyBaseComponent(new ComponentCollection());
 			
 			$assignments = array();
-			$assignments['content_id'] = $_GET['content_id'];
+			
+			$assignments['content_id'] = $content_id;
 			$assignments['content_alias'] = $content['Content']['alias'];
 			$assignments['content_name'] = $content_description['ContentDescription']['name'];
+			$assignments['content_description'] = $content_description['ContentDescription']['description'];
+			$assignments['content_price'] = $CurrencyBase->display_price($content['ContentProduct']['price']);	
+			$assignments['content_url'] = BASE . '/' . $content_description['ContentDescription']['name'] . '/' . $content['Content']['alias'] . $config['URL_EXTENSION'];
+			
 			return $assignments;
 		}
 	}
@@ -55,6 +79,9 @@ class ActionController extends ModuleReviewsAppController {
 	function display () 
 	{
 		$assignments = array();
+		
+		$CurrencyBase =& new CurrencyBaseComponent(new ComponentCollection());
+		
 		if(empty($_GET['content_id']))
 		{
 			global $content;
@@ -69,21 +96,26 @@ class ActionController extends ModuleReviewsAppController {
 		App::uses('CakeTime', 'Utility');
 		
 		$assigned_reviews = array();
+		$col = 0;
 		foreach($reviews AS $review)
 		{
-			$review['ModuleReview']['created'] = CakeTime::niceShort($review['ModuleReview']['created']);
+			$col++;
+			$review['ModuleReview']['created'] = CakeTime::timeAgoInWords($review['ModuleReview']['created']);
 			$assigned_reviews[] = $review['ModuleReview'];
 		}
 
 		// Assign some content vars
-		$content = $this->ContentBase->get_content_information($content_id);
 		$content_description = $this->ContentBase->get_content_description($content_id);			
 			
 		$assignments = array();
 		$assignments['content_id'] = $content_id;
 		$assignments['content_alias'] = $content['Content']['alias'];
 		$assignments['content_name'] = $content_description['ContentDescription']['name'];		
+		$assignments['content_description'] = $content_description['ContentDescription']['description'];
+		$assignments['content_price'] = $CurrencyBase->display_price($content['ContentProduct']['price']);	
+		$assignments['content_url'] = BASE . '/' . $content_description['ContentDescription']['name'] . '/' . $content['Content']['alias'] . $config['URL_EXTENSION'];
 		
+		$assignments['total'] = $col;
 		$assignments['reviews'] = $assigned_reviews;
 		
 		return $assignments;
--- trunk/app/Plugin/ModuleReviews/View/Action/display.ctp
+++ trunk/app/Plugin/ModuleReviews/View/Action/display.ctp
@@ -1,10 +1,21 @@
-<div id="review_listing">
-{foreach from=$reviews item=review}		
-	<div class="review">	
-		<div class="author">{$review.name}, {$review.created}</div>
-		<div class="content">{$review.content}</div>
-	</div>
-{foreachelse}	
-	{lang}No reviews were found for this product.{/lang}
-{/foreach}				
+{if $reviews}
+<div class="row-fluid reviews-title">
+	<div class="span8 title"><h3>{lang}Reviews{/lang}: {$total}.</h3></div>
 </div>
+
+{foreach from=$reviews item=review}
+<div class="media">
+	<a href="" class="pull-left"><img class="media-object" src="{base_path}/img/avatar.png" alt=""/></a>
+	<div class="media-body">
+		<div class="inner">
+			<h4 class="media-heading">{$review.name} - {$review.created}:</h4>
+				<div class="description">
+					{$review.content}
+				</div>
+		</div>
+	</div>
+</div>
+{/foreach}
+{else}
+{lang}No reviews were found for this product.{/lang}
+{/if}
