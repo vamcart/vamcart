e188d993c4b3d6d5550b5408530dc5f5e56b7e07
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2013-10-20 16:16:14 +0000

    Добавлена возможность массового удаления отзывов

--- trunk/app/Plugin/ModuleReviews/Controller/AdminController.php
+++ trunk/app/Plugin/ModuleReviews/Controller/AdminController.php
@@ -8,31 +8,55 @@
 App::uses('ModuleReviewsAppController', 'ModuleReviews.Controller');
 
 class AdminController extends ModuleReviewsAppController {
-	var $uses = array('ModuleReviews.ModuleReview');
-	var $helpers = array('Time','Admin');
+	public $uses = array('ModuleReviews.ModuleReview');
+	public $helpers = array('Time','Admin');
 	
-	function admin_delete ($id)
+	public function admin_delete ($id)
 	{
 		$this->ModuleReview->delete($id);
 		$this->Session->setFlash(__('Record Deleted.'));
 		$this->redirect('/module_reviews/admin/admin_index/');
 	}
-	
-	function admin_edit ($id)
+
+	public function admin_modify_selected() 	
+	{
+		$build_flash = "";
+		foreach($this->params['data']['ModuleReview']['modify'] AS $value)
+		{
+			// Make sure the id is valid
+			if($value > 0)
+			{
+				$this->ModuleReview->id = $value;
+				$review = $this->ModuleReview->read();
+		
+				switch ($this->data['multiaction']) 
+				{
+					case "delete":
+						$this->ModuleReview->delete($value);
+						$build_flash .= __('Record deleted.', true) . ' ' . $review['ModuleReview']['name'] . '<br />';									
+					break;								
+				}
+			}
+		}
+		$this->Session->setFlash($build_flash);
+		$this->redirect('/module_reviews/admin/admin_index/');
+	}	
+		
+	public function admin_edit ($id)
 	{
 		$this->set('current_crumb',__('Read Review'));
 		$this->set('title_for_layout', __('Read Review'));
 		$this->set('data',$this->ModuleReview->read(null,$id));
 	}
 	
-	function admin_index()
+	public function admin_index()
 	{
 		$this->set('current_crumb',__('Manage Reviews'));
 		$this->set('title_for_layout', __('Manage Reviews'));
 		$this->set('reviews',$this->ModuleReview->find('all'));
 	}
 	
-	function admin_help()
+	public function admin_help()
 	{
 		$this->set('current_crumb',__('Reviews'));
 		$this->set('title_for_layout', __('Reviews'));
--- trunk/app/Plugin/ModuleReviews/View/Admin/admin_index.ctp
+++ trunk/app/Plugin/ModuleReviews/View/Admin/admin_index.ctp
@@ -6,10 +6,16 @@
    License - http://vamcart.com/license.html
    ---------------------------------------------------------------------------------------*/
 
+$this->Html->script(array(
+	'selectall.js'
+), array('inline' => false));
+
 echo $this->Admin->ShowPageHeaderStart($current_crumb, 'cus-user-comment');
 
-echo '<table class="contentTable">';
-echo $this->Html->tableHeaders(array( __('Author'), __('Date'), __('Action')));
+echo $this->Form->create('ModuleReview', array('action' => '/module_reviews/admin/admin_modify_selected/', 'url' => '/module_reviews/admin/admin_modify_selected/'));
+
+echo '<table class="contentTable" border="0">';
+echo $this->Html->tableHeaders(array( __('Author'), __('Date'), __('Action'), '<input type="checkbox" onclick="checkAll(this)" />'));
 
 foreach ($reviews AS $review)
 {
@@ -17,12 +23,16 @@ foreach ($reviews AS $review)
 		  array(
 			$this->Html->link($review['ModuleReview']['name'],'/module_reviews/admin/admin_edit/' . $review['ModuleReview']['id']),
 			$this->Time->i18nFormat($review['ModuleReview']['created']),
-			$this->Admin->ActionButton('edit','/module_reviews/admin/admin_edit/' . $review['ModuleReview']['id']) . $this->Admin->ActionButton('delete','/module_reviews/admin/admin_delete/' . $review['ModuleReview']['id'])
+			$this->Admin->ActionButton('edit','/module_reviews/admin/admin_edit/' . $review['ModuleReview']['id']) . $this->Admin->ActionButton('delete','/module_reviews/admin/admin_delete/' . $review['ModuleReview']['id']),
+			array($this->Form->checkbox('modify][', array('value' => $review['ModuleReview']['id'])), array('align'=>'center'))
 		   ));
 }
 echo '</table>';
 
 echo $this->Admin->EmptyResults($reviews);
+
+echo $this->Admin->ActionBar(array('delete'=>__('Delete')),false);
+echo $this->Form->end();
 
 echo $this->Admin->ShowPageHeaderEnd();
 
