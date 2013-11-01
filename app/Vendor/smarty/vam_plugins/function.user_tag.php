c47ec87ee667f4c05d7205bc240324a056de041f
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2013-11-01 06:33:07 +0000

    Корректировка шаблонов

--- app/Vendor/smarty/vam_plugins/function.user_tag.php
+++ app/Vendor/smarty/vam_plugins/function.user_tag.php
@@ -21,11 +21,11 @@ function smarty_help_function_user_tag() {
 	<h3><?php echo __('What does this tag do?') ?></h3>
 	<p><?php echo __('Calls the user tag specified by alias') ?></p>
 	<h3><?php echo __('How do I use it?') ?></h3>
-	<p><?php echo __('Just insert the tag into your template like:') ?> <code>{user_tag alias='user-agent'}</code></p>
+	<p><?php echo __('Just insert the tag into your template like:') ?> <code>{user_tag alias='tag-alias'}</code></p>
 	<h3><?php echo __('What parameters does it take?') ?></h3>
 	<ul>
 		<li><em><?php echo __('(alias)') ?></em> - <?php echo __('Alias of the user tag to call.') ?></li>
-		<li><em><?php echo __('(var)') ?></em> - <?php echo __('Pass any other smarty variables or information to the user tag like:') ?> {user_tag alias='user-agent' var1='abcd' var2='1234'}.  <?php echo __('These will be available for the user tag to use in the $params array.') ?></li>		
+		<li><em><?php echo __('(var)') ?></em> - <?php echo __('Pass any other smarty variables or information to the user tag like:') ?> {user_tag alias='tag-alias' var1='abcd' var2='1234'}.  <?php echo __('These will be available for the user tag to use in the $params array.') ?></li>		
 	</ul>
 	<?php
 }
