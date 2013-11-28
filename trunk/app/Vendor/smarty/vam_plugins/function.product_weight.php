74a8179b058718ef443222dcc9db236a511dbcf3
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2013-11-28 10:55:34 +0000

    Корректировка перевода

---
+++ trunk/app/Vendor/smarty/vam_plugins/function.product_weight.php
@@ -0,0 +1,33 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   VamCart - http://vamcart.com
+   -----------------------------------------------------------------------------------------
+   Copyright (c) 2013 VamSoft Ltd.
+   License - http://vamcart.com/license.html
+   ---------------------------------------------------------------------------------------*/
+
+function smarty_function_product_weight($params, $template)
+{
+	global $content;
+	
+	$weight = $content['ContentProduct']['weight'];
+	
+	echo $weight;
+}
+
+function smarty_help_function_product_weight () {
+	?>
+	<h3><?php echo __('What does this tag do?') ?></h3>
+	<p><?php echo __('Displays the product weight for the current content.') ?></p>
+	<h3><?php echo __('How do I use it?') ?></h3>
+	<p><?php echo __('Just insert the tag into your template like:') ?> <code>{product_weight}</code></p>
+	<h3><?php echo __('What parameters does it take?') ?></h3>
+	<ul>
+		<li><em>(<?php echo __('None') ?>)</em></li>
+	</ul>
+	<?php
+}
+
+function smarty_about_function_product_weight () {
+}
+?>
