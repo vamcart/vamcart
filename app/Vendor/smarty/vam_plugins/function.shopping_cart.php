a9bff1fca9c425ea1d580db93288dd7d9505fc8b
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2014-03-12 09:25:35 +0000

    Корректировка вывода fontawesome иконок

--- app/Vendor/smarty/vam_plugins/function.shopping_cart.php
+++ app/Vendor/smarty/vam_plugins/function.shopping_cart.php
@@ -24,7 +24,7 @@ $template = '
 {foreach from=$order_items item=product}
 		<tr>
 			<td align="center"><img class="media-object" src="{$product.image.image_thumb}" alt="" title=""{if {$product.image.image_width} > 0} width="{$product.image.image_width}"{/if}{if {$product.image.image_height} > 0} height="{$product.image.image_height}"{/if} /></td>
-			<td><a href="{$product.link}">{$product.name}</a> <a href="{base_path}/cart/remove_product/{$product.id}/1" class="remove" title="{lang}Remove{/lang}"><i class="fa fa-trash"></i></a></td>
+			<td><a href="{$product.link}">{$product.name}</a> <a href="{base_path}/cart/remove_product/{$product.id}/1" class="remove" title="{lang}Remove{/lang}"><i class="fa fa-trash-o"></i></a></td>
 			<td>{$product.price}</td>
 			<td><input type="text" name="qty[{$product.id}]" class="input-small" value="{$product.qty}" size="3" /></td>
 			<td>{$product.line_total}</td>
