386f4ad71a85f8b4d16e9ff8a5e400bd1a911ded
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2014-04-02 20:02:25 +0000

    Корректировка шаблонов

--- trunk/app/Vendor/smarty/vam_plugins/function.content_listing.php
+++ trunk/app/Vendor/smarty/vam_plugins/function.content_listing.php
@@ -28,7 +28,7 @@ $template = '
 <div class="row-fluid shop-products">
 	<ul class="thumbnails">
 		{foreach from=$content_list item=node}
-		<li class="item span4 {if $node@index is div by 3}first{/if}">
+		<li class="item span4{if $node@index is div by 3} first{/if}">
 			<div class="thumbnail text-center">
 				<a href="{$node.url}" class="image"><img src="{$node.image}" alt="{$node.name}"{if isset($thumbnail_width)} width="{$thumbnail_width}"{/if} /><span class="frame-overlay"></span><span class="price">{$node.price}</span></a>
 			<div class="inner notop nobottom text-left">
