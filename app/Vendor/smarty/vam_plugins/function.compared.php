1f079fe77571c3ce781672e66c37595a6baac2e8
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2014-04-02 20:02:25 +0000

    Корректировка шаблонов

--- app/Vendor/smarty/vam_plugins/function.compared.php
+++ app/Vendor/smarty/vam_plugins/function.compared.php
@@ -17,7 +17,7 @@ $template = '
 		<div class="row-fluid shop-products">
 			<ul class="thumbnails">
 				{foreach from=$element_list[0]["attributes_product"] item=node}
-				<li class="item span4 {if $node@index is div by 3}first{/if}">
+				<li class="item span4{if $node@index is div by 3} first{/if}">
 					<div class="thumbnail text-center">
 						<a href="{$node.url}" class="image"><img src="{$node.image}" alt="{$node.name}"{if {$node.image_width} > 0} width="{$node.image_width}"{/if}{if {$node.image_height} > 0} height="{$node.image_height}"{/if} /><span class="frame-overlay"></span><span class="price">{$node.price}</span></a>
 					<div class="inner notop nobottom text-left">
