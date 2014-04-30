b4780c7828098d77ea415706b94275bc485d80ed
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2014-04-30 13:48:58 +0000

    Корректировка модели AddressBook

--- trunk/app/Model/AddressBook.php
+++ trunk/app/Model/AddressBook.php
@@ -11,40 +11,40 @@ class AddressBook extends AppModel {
 	
 	public function _validationRules() 
 	{
-	$this->validate = array(
-		'ship_name' => array(
-			'rule' => 'notEmpty',
-			'message' => __('Name must only contain letters and numbers.', true)
-		),
-		'ship_line_1' => array(
-			'rule' => 'notEmpty',
-			'message' => __('Address Line 1 must only contain letters and numbers.', true)
-		),
-		'ship_city' => array(
-			'rule' => 'notEmpty',
-			'message' => __('City must only contain letters and numbers.', true)
-		),
-		'ship_state' => array(
-			'rule' => 'notEmpty',
-			'message' => __('State must only contain letters and numbers.', true)
-		),
-		'ship_country' => array(
-			'rule' => 'notEmpty',
-			'message' => __('Country must only contain letters and numbers.', true)
-		),
-		'ship_zip' => array(
-			'rule' => 'Numeric',
-			'required' => true,
-			'allowEmpty' => false,
-			'message' => __('Zipcode must only contain numbers.', true)
-		),
-		'phone' => array(
-			'rule' => 'Numeric',
-			'required' => true,
-			'allowEmpty' => false,
-			'message' => __('Phone must only contain numbers.', true)
-		),
-	);
+	//$this->validate = array(
+		//'ship_name' => array(
+			//'rule' => 'notEmpty',
+			//'message' => __('Name must only contain letters and numbers.', true)
+		//),
+		//'ship_line_1' => array(
+			//'rule' => 'notEmpty',
+			//'message' => __('Address Line 1 must only contain letters and numbers.', true)
+		//),
+		//'ship_city' => array(
+			//'rule' => 'notEmpty',
+			//'message' => __('City must only contain letters and numbers.', true)
+		//),
+		//'ship_state' => array(
+			//'rule' => 'notEmpty',
+			//'message' => __('State must only contain letters and numbers.', true)
+		//),
+		//'ship_country' => array(
+			//'rule' => 'notEmpty',
+			//'message' => __('Country must only contain letters and numbers.', true)
+		//),
+		//'ship_zip' => array(
+			//'rule' => 'Numeric',
+			//'required' => true,
+			//'allowEmpty' => false,
+			//'message' => __('Zipcode must only contain numbers.', true)
+		//),
+		//'phone' => array(
+			//'rule' => 'Numeric',
+			//'required' => true,
+			//'allowEmpty' => false,
+			//'message' => __('Phone must only contain numbers.', true)
+		//),
+	//);
 	}
 
 }
