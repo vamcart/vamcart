50df40c5d9e6e3eeb4f423ff55adc1fda628c429
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2014-04-30 11:59:56 +0000

    Добавлен перевод дял групповых скидок, скидов от количества в каталоге

--- app/Controller/DiscountsController.php
+++ app/Controller/DiscountsController.php
@@ -1,8 +1,14 @@
 <?php
+/* -----------------------------------------------------------------------------------------
+   VamShop - http://vamshop.com
+   -----------------------------------------------------------------------------------------
+   Copyright (c) 2014 VamSoft Ltd.
+   License - http://vamshop.com/license.html
+   ---------------------------------------------------------------------------------------*/
 
 class DiscountsController extends AppController {
 	public $name = 'Discounts';
-        public $uses = array('ContentProductPrice');
+	public $uses = array('ContentProductPrice');
 
 	public function admin_delete ($content_id, $id)
 	{
@@ -43,61 +49,35 @@ class DiscountsController extends AppController {
 		$this->redirect('/discounts/admin_edit/'.$content_id);
 	}
 
-	public function admin_modify_selected()
+	public function admin_modify_selected() 	
 	{
-		$build_flash = "";
-		foreach($this->params['data']['Currency']['modify'] AS $value)
+	$build_flash = "";
+	foreach($this->params['data']['ContentProductPrice']['modify'] AS $value)
+	{
+		// Make sure the id is valid
+		if($value > 0)
 		{
-			// Make sure the id is valid
-			if($value > 0)
+			$this->ContentProductPrice->id = $value;
+			$currency = $this->ContentProductPrice->read();
+	
+			switch ($this->data['multiaction']) 
 			{
-				$this->Currency->id = $value;
-				$currency = $this->Currency->read();
-
-				switch ($this->data['multiaction'])
-				{
-					case "delete":
-						// Make sure it's not the default currency
-						if($currency['Currency']['default'] == 0)
-						{
-						    $this->Currency->delete($value);
-							$build_flash .= __('Record deleted.', true) . ' (' . $currency['Currency']['name'] . ')<br />';
-						}
-						else
-						{
-							$build_flash .= __('Error: Could not delete default record.', true) . ' (' . $currency['Currency']['name'] . ')<br />';
-						}
-					break;
-					case "activate":
-						$currency['Currency']['active'] = 1;
-						$this->Currency->save($currency);
-						$build_flash .= __('Record activated.', true) . ' (' . $currency['Currency']['name'] . ')<br />';
-					break;
-					case "deactivate":
-						// Don't let them deactivate the default currency
-						if($currency['Currency']['default'] == 1)
-						{
-							$build_flash .=  __('Error: Could not deactivate default record.', true) .' (' . $currency['Currency']['name'] . ')<br />';
-						}
-						else
-						{
-							$currency['Currency']['active'] = 0;
-							$this->Currency->save($currency);
-							$build_flash .= __('Record deactivated.', true) . ' (' . $currency['Currency']['name'] . ')<br />';
-						}
-					break;
-				}
+				case "delete":
+					    $this->ContentProductPrice->delete($value);
+						$build_flash .= __('Record deleted.', true) . '<br />';									
+				break;
 			}
 		}
-		$this->Session->setFlash($build_flash);
-		$this->redirect('/currencies/admin/');
+	}
+	$this->Session->setFlash($build_flash);
+	$this->redirect('/discounts/admin/');
 	}
 
 	public function admin($content_id = null, $ajax = false)
 	{
 		$this->set('current_crumb', __('Discounts Listing', true));
 		$this->set('title_for_layout', __('Discounts Listing', true));
-                $this->set('content_product_id', $content_id);
+      $this->set('content_product_id', $content_id);
 		$this->set('discount_data',$this->ContentProductPrice->find('all', array('conditions' => array('content_product_id' => $content_id), 'order' => 'quantity ASC')));
 	}
 }
