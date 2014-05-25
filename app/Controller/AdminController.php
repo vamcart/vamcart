14b58ed846a5306ee5866d4519828afbbc35606c
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2014-05-25 14:58:20 +0000

    Корректировка кода uhграфиков

--- app/Controller/AdminController.php
+++ app/Controller/AdminController.php
@@ -25,12 +25,12 @@ class AdminController extends AppController {
                 $l = $this->Session->read('Customer.language');
             }
                 
-            $order_day = $this->Order->find('all', array('fields' => array('DATE_FORMAT(Order.created, \'%m/%d\') as dat','TRUNCATE(SUM(Order.total),2) as summ','COUNT(Order.id) as cnt')
+            $order_day = $this->Order->find('all', array('fields' => array('DATE_FORMAT(Order.created, \'%m-%d-%Y\') as dat','TRUNCATE(SUM(Order.total),2) as summ','COUNT(Order.id) as cnt')
                                                     ,'conditions' => array(/*'Order.order_status_id' => $listStatuses
                                                                           ,*/'Order.created >' => date("Y-m-d H:i:s",time()-(14*24*3600)))
                                                     ,'group' => array('dat')
                                                     ,'order' => array('dat')));
-            $order_month = $this->Order->find('all', array('fields' => array('DATE_FORMAT(Order.created, \'%Y/%m\') as dat','TRUNCATE(SUM(Order.total),2) as summ','COUNT(Order.id) as cnt')
+            $order_month = $this->Order->find('all', array('fields' => array('DATE_FORMAT(Order.created, \'%Y-%m\') as dat','TRUNCATE(SUM(Order.total),2) as summ','COUNT(Order.id) as cnt')
                                                     ,'conditions' => array(/*'Order.order_status_id' => $listStatuses
                                                                           ,*/'Order.created >' => date("Y-m-d H:i:s",time()-(365*24*3600)))
                                                     ,'group' => array('dat')
