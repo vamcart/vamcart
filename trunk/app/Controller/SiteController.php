8493493a7b57a6223364c44a3a0d7f3190363eae
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2014-07-09 20:10:47 +0000

    Коррективрока оформления заказа, если заказ оформляется авторизованный пользователь с заполненой адресной книгой, то поля формы на странице оформления уже автоматически заполнены данными из адресной книги

--- trunk/app/Controller/SiteController.php
+++ trunk/app/Controller/SiteController.php
@@ -185,16 +185,27 @@ class SiteController extends AppController {
 		if ($customer_id['Customer']['password'] == Security::hash( $_POST['data']['Customer']['password'], 'sha1', true)) {
 
 		$this->Session->write('Customer.customer_id', $customer_id['Customer']['id']);
-                if(isset($customer_id['GroupsCustomer']['id']))$this->Session->write('Customer.customer_group_id', $customer_id['GroupsCustomer']['id']);
-                else $this->Session->write('Customer.customer_group_id', 0);
+
+		if(isset($customer_id['GroupsCustomer']['id']))$this->Session->write('Customer.customer_group_id', $customer_id['GroupsCustomer']['id']);
+		else $this->Session->write('Customer.customer_group_id', 0);
+
+		if ($customer_id['AddressBook']['ship_name'] != '') $this->Session->write('AddressBook.ship_name', $customer_id['AddressBook']['ship_name']);
+		if ($customer_id['AddressBook']['ship_line_1'] != '') $this->Session->write('AddressBook.ship_line_1', $customer_id['AddressBook']['ship_line_1']);
+		if ($customer_id['AddressBook']['ship_line_2'] != '') $this->Session->write('AddressBook.ship_line_2', $customer_id['AddressBook']['ship_line_2']);
+		if ($customer_id['AddressBook']['ship_city'] != '') $this->Session->write('AddressBook.ship_city', $customer_id['AddressBook']['ship_city']);
+		if ($customer_id['AddressBook']['ship_country'] != '') $this->Session->write('AddressBook.ship_country', $customer_id['AddressBook']['ship_country']);
+		if ($customer_id['AddressBook']['ship_state'] != '') $this->Session->write('AddressBook.ship_state', $customer_id['AddressBook']['ship_state']);
+		if ($customer_id['AddressBook']['ship_zip'] != '') $this->Session->write('AddressBook.ship_zip', $customer_id['AddressBook']['ship_zip']);
+		if ($customer_id['AddressBook']['phone'] != '') $this->Session->write('AddressBook.phone', $customer_id['AddressBook']['phone']);
+		if ($customer_id['Customer']['email'] != '') $this->Session->write('Customer.email', $customer_id['Customer']['email']);
 
 		} else {
 
 		$this->Session->setFlash(__('Login error. Check your email and/or password!'), 'bootstrap_alert_error');
 
 		$this->Session->delete('Customer.customer_id');
-                $this->Session->delete('Customer.customer_group_id');
-			
+		$this->Session->delete('Customer.customer_group_id');
+
 		}
 
 		$this->redirect($_SERVER['HTTP_REFERER']);
@@ -205,6 +216,8 @@ class SiteController extends AppController {
 	{
 
 		$this->Session->delete('Customer.customer_id');
+		$this->Session->delete('Customer.email');
+		$this->Session->delete('AddressBook');
 
 		//$this->Auth->logout();
 
