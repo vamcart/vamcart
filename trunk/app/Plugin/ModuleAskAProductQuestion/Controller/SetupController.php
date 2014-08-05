1c4a1008eba36e3f67e0e3d1e7fb7eebf4fa0acf
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2014-08-05 20:30:30 +0000

    Добавлен новый модуль - Задать вопрос о товаре

---
+++ trunk/app/Plugin/ModuleAskAProductQuestion/Controller/SetupController.php
@@ -0,0 +1,69 @@
+<?php 
+/* -----------------------------------------------------------------------------------------
+   VamShop - http://vamshop.com
+   -----------------------------------------------------------------------------------------
+   Copyright (c) 2014 VamSoft Ltd.
+   License - http://vamshop.com/license.html
+   ---------------------------------------------------------------------------------------*/
+class SetupController extends ModuleAskAProductQuestionAppController {
+	public $uses = array('Module', 'Content');
+	public $components = array('ModuleBase');
+	
+	public function upgrade ()
+	{
+		$this->ModuleBase->upgrade();
+		$this->Session->setFlash(__('Module Upgraded'));
+		$this->redirect('/modules/admin/');		
+	}
+		
+	public function install()
+	{
+
+		// Create the new module record		
+		$new_module = array();
+		$new_module['Module']['name'] = __d('module_ask_a_product_question', 'Ask A Product Question');
+		$new_module['Module']['icon'] = 'cus-user-comment';
+		$new_module['Module']['alias'] = 'ask_a_product_question';
+		$new_module['Module']['version'] = $this->ModuleBase->get_version();
+		$new_module['Module']['nav_level'] =  '-1';				
+		$this->Module->save($new_module);
+		
+		// Create new core pages
+		$default_alias = 'ask_a_product_question';
+		$default_name = __d('module_ask_a_product_question', 'Ask A Product Question');
+		$default_description = '{module alias=\'ask_a_product_question\' controller=\'get\' action=\'ask_success\'}';
+		
+		$this->ModuleBase->create_core_page($default_alias,$default_name,$default_description);
+
+		// Create the database table
+		$install_query = "
+		INSERT INTO `defined_languages` (`language_id`, `key`, `value`, `created`, `modified`) VALUES 
+		(1, 'Ask A Product Question', 'Ask A Product Question', '2013-10-10 20:08:49', '2013-10-10 20:08:49'),
+		(2, 'Ask A Product Question', 'Задать вопрос о товаре', '2013-10-10 20:08:49', '2013-10-10 20:08:49')
+		";		
+		$this->Module->query($install_query);
+
+		$this->Session->setFlash(__('Module Installed'));
+		$this->Session->setFlash(__('Module Installed'));
+		$this->redirect('/module_ask_a_product_question/admin/admin_help');
+	}
+	
+	
+	public function uninstall()
+	{
+		// Delete the module record
+		$module = $this->Module->findByAlias('ask_a_product_question');
+		$this->Module->delete($module['Module']['id']);
+		
+		$core_page = $this->Content->find('first', array('conditions' => array('Content.parent_id' => '-1','alias' => 'ask_a_product_question')));
+		$this->Content->delete($core_page['Content']['id'],true);
+
+		// Delete the module record
+		$uninstall_query = "DELETE FROM `defined_languages` WHERE `key` = 'Ask A Product Question'";
+		$this->Module->query($uninstall_query);
+
+		$this->Session->setFlash(__('Module Uninstalled')); 
+		$this->redirect('/modules/admin/');
+	}
+
+}
---
+++ trunk/app/Plugin/ModuleAskAProductQuestion/View/Admin/admin_help.ctp
@@ -0,0 +1,17 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   VamShop - http://vamshop.com
+   -----------------------------------------------------------------------------------------
+   Copyright (c) 2014 VamSoft Ltd.
+   License - http://vamshop.com/license.html
+   ---------------------------------------------------------------------------------------*/
+?>
+<?php echo $this->Admin->ShowPageHeaderStart($title_for_layout, 'cus-help'); ?>
+<h3><?php echo __('What does this do?'); ?></h3>
+<p><?php echo __d('module_ask_a_product_question', 'Customer can send question to store admin email about any product in your store from product page.'); ?></p>
+<h3><?php echo __('How do I use this?'); ?></h3>
+<h3><?php echo __d('module_ask_a_product_question', 'Template placeholder to create a link to a product question form:'); ?></h3>
+<p>{module alias='ask_a_product_question' controller='get' action='ask_link'}</p>
+<p><?php echo __d('module_ask_a_product_question', 'This call will create link to javascript popup window with product question form.'); ?></p>
+<h3><?php echo __d('module_ask_a_product_question', 'Insert this placeholder at product page template (Admin - Layout - Template - Product Info).'); ?></h3>
+<?php echo $this->Admin->ShowPageHeaderEnd(); ?>
