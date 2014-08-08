0985246cc8bd66d0ec3498b013b471c5734aa78d
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2014-08-08 14:45:14 +0000

    Корректировка модулей задать вопрос о тоаре, заказ за 1 клик, брошенные козины, добавлен код для шаблоно писем

--- app/Plugin/ModuleOneClickBuy/Controller/SetupController.php
+++ app/Plugin/ModuleOneClickBuy/Controller/SetupController.php
@@ -6,7 +6,7 @@
    License - http://vamshop.com/license.html
    ---------------------------------------------------------------------------------------*/
 class SetupController extends ModuleOneClickBuyAppController {
-	public $uses = array('Module', 'Content');
+	public $uses = array('Module', 'Content', 'EmailTemplate',);
 	public $components = array('ModuleBase');
 	
 	public function upgrade ()
@@ -35,13 +35,31 @@ class SetupController extends ModuleOneClickBuyAppController {
 		
 		$this->ModuleBase->create_core_page($default_alias,$default_name,$default_description);
 
-		// Create the database table
-		$install_query = "
+		$install_query = array();
+
+		$install_query[] = "
 		INSERT INTO `defined_languages` (`language_id`, `key`, `value`, `created`, `modified`) VALUES 
 		(1, 'One Click Buy', 'One Click Buy', '2013-10-10 20:08:49', '2013-10-10 20:08:49'),
 		(2, 'One Click Buy', 'Купить за 1 клик', '2013-10-10 20:08:49', '2013-10-10 20:08:49')
 		";		
-		$this->Module->query($install_query);
+
+		$email_template = $this->EmailTemplate->findByAlias('one_click_buy');
+		if (empty($email_template)) {
+
+		$install_query[] = "
+INSERT INTO email_templates (`id`, `alias`, `default`, `order`) VALUES (NULL, 'one_click_buy', 0, 0);
+";
+		$install_query[] = "
+INSERT INTO `email_template_descriptions` (`id`, `email_template_id`, `language_id`, `subject`, `content`) VALUES
+(NULL, LAST_INSERT_ID(), 1, 'One Click Buy - {\$product_name}', 'Hello!\r\n\r\nThank you for stopping by {\$store_name} and considering us for your purchase.Ordered product:\r\n\r\n{\$product_name}\r\n\r\nContact information: {\$contact}'),
+(NULL, LAST_INSERT_ID(), 2, 'Купить за 1 клик - {\$product_name}', 'Здравствуйте!\r\n\r\nВы оформили заказ в интернет-магазине {\$store_name}!\r\n\r\nТовар, который Вы заказали:\r\n\r\n{\$product_name}\r\n\r\nКонтактные данные: {\$contact}');
+		";
+				
+		foreach($install_query AS $query)
+		{
+			$this->Module->query($query);
+		}		
+		}
 
 		$this->Session->setFlash(__('Module Installed'));
 		$this->Session->setFlash(__('Module Installed'));
@@ -61,6 +79,12 @@ class SetupController extends ModuleOneClickBuyAppController {
 		// Delete the module record
 		$uninstall_query = "DELETE FROM `defined_languages` WHERE `key` = 'One Click Buy'";
 		$this->Module->query($uninstall_query);
+
+		// Deletes the email templates records
+		$email_template = $this->EmailTemplate->findByAlias('one_click_buy');
+		if (!empty($email_template)) {
+		$this->EmailTemplate->delete($email_template['EmailTemplate']['id']);
+		}
 
 		$this->Session->setFlash(__('Module Uninstalled')); 
 		$this->redirect('/modules/admin/');
