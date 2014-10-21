84d2f5050079b58aec60720c8513d51e5a7af19b
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2014-10-21 11:42:12 +0000

    Корректировка модели СontentProduct, добавлена свзяь товара с Manufacturer

--- trunk/app/Model/ContentProduct.php
+++ trunk/app/Model/ContentProduct.php
@@ -9,7 +9,7 @@
 App::uses('Model', 'AppModel');
 class ContentProduct extends AppModel {
 	public $name = 'ContentProduct';
-	public $belongsTo = array('Tax');
+	public $belongsTo = array('Tax', 'Manufacturer');
 	public $hasMany = array('ContentProductPrice' => array('dependent' => true));
 	
 	public $validate = array(
