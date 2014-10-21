c7df6a9bbfb86a0bd5b7157febf477956fc843c3
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2014-10-21 11:42:57 +0000

    Добавлена смарти-метка product_manufacturer, выводящая производителя товара

---
+++ app/Vendor/smarty/vam_plugins/function.product_manufacturer.php
@@ -0,0 +1,38 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   VamShop - http://vamshop.com
+   -----------------------------------------------------------------------------------------
+   Copyright (c) 2014 VamSoft Ltd.
+   License - http://vamshop.com/license.html
+   ---------------------------------------------------------------------------------------*/
+
+function smarty_function_product_manufacturer($params, $template)
+{
+	global $content;
+
+	if ($content['ContentProduct']['manufacturer_id'] > 0) {
+	
+	$manufacturer = $content['Manufacturer']['name'];
+	
+	echo $manufacturer;
+
+	}
+	
+}
+
+function smarty_help_function_product_manufacturer () {
+	?>
+	<h3><?php echo __('What does this tag do?') ?></h3>
+	<p><?php echo __('Displays the product manufacturer for the current content.') ?></p>
+	<h3><?php echo __('How do I use it?') ?></h3>
+	<p><?php echo __('Just insert the tag into your template like:') ?> <code>{product_model}</code></p>
+	<h3><?php echo __('What parameters does it take?') ?></h3>
+	<ul>
+		<li><em>(<?php echo __('None') ?>)</em></li>
+	</ul>
+	<?php
+}
+
+function smarty_about_function_product_manufacturer () {
+}
+?>
