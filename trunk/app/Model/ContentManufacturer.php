8e91db80c97312d4bdfc128db170dedf31db8a17
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2014-10-21 16:48:45 +0000

    Добавнеа новая модель для производителей

---
+++ trunk/app/Model/ContentManufacturer.php
@@ -0,0 +1,19 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   VamShop - http://vamshop.com
+   -----------------------------------------------------------------------------------------
+   Copyright (c) 2014 VamSoft Ltd.
+   License - http://vamshop.com/license.html
+   ---------------------------------------------------------------------------------------*/
+App::uses('Model', 'AppModel');
+class ContentManufacturer extends AppModel {
+	public $name = 'ContentManufacturer';
+	
+	public $validate = array(
+	'content_id' => array(
+		'rule' => 'notEmpty'
+	)
+	);
+	
+}
+?>
