ee51b3fa39a9cdf5e84e27ceb8abc502853860ba
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2014-10-21 13:56:44 +0000

    Корректировка описания тэга product_manufacturer

--- app/Vendor/smarty/vam_plugins/function.product_manufacturer.php
+++ app/Vendor/smarty/vam_plugins/function.product_manufacturer.php
@@ -25,7 +25,7 @@ function smarty_help_function_product_manufacturer () {
 	<h3><?php echo __('What does this tag do?') ?></h3>
 	<p><?php echo __('Displays the product manufacturer for the current content.') ?></p>
 	<h3><?php echo __('How do I use it?') ?></h3>
-	<p><?php echo __('Just insert the tag into your template like:') ?> <code>{product_model}</code></p>
+	<p><?php echo __('Just insert the tag into your template like:') ?> <code>{product_manufacturer}</code></p>
 	<h3><?php echo __('What parameters does it take?') ?></h3>
 	<ul>
 		<li><em>(<?php echo __('None') ?>)</em></li>
