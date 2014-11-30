af1e2fa662f87491ba3be390290a19cc6e0804dc
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2014-11-30 12:31:42 +0000

    Добавлены новые метки product_price_old и product_price_save для вывода информации об акционных товарах

---
+++ app/Vendor/smarty/vam_plugins/function.product_price_old.php
@@ -0,0 +1,48 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   VamShop - http://vamshop.com
+   -----------------------------------------------------------------------------------------
+   Copyright (c) 2014 VamSoft Ltd.
+   License - http://vamshop.com/license.html
+   ---------------------------------------------------------------------------------------*/
+
+function smarty_function_product_price_old($params, $template)
+{
+	global $content;
+	
+	App::uses('CurrencyBaseComponent', 'Controller/Component');
+	$CurrencyBase =& new CurrencyBaseComponent(new ComponentCollection());
+
+	if ($content['Content']['content_type_id'] == 7) {
+		$price_old = $content['ContentDownloadable']['old_price'];
+	} else {
+		$price_old = $content['ContentProduct']['old_price'];
+	}
+
+	if ($content['ContentProduct']['price'] > $price_old) {
+		return;
+	}
+
+	if(isset($params['price'])) $price_old = $params['price'];
+			
+	$price_old = $CurrencyBase->display_price($price_old);
+	
+	echo $price_old;
+}
+
+function smarty_help_function_product_price_old () {
+	?>
+	<h3><?php echo __('What does this tag do?') ?></h3>
+	<p><?php echo __('Displays the product price for the current content.') ?></p>
+	<h3><?php echo __('How do I use it?') ?></h3>
+	<p><?php echo __('Just insert the tag into your template like:') ?> <code>{product_price_old}</code></p>
+	<h3><?php echo __('What parameters does it take?') ?></h3>
+	<ul>
+		<li><em>(<?php echo __('None') ?>)</em></li>
+	</ul>
+	<?php
+}
+
+function smarty_about_function_product_price_old () {
+}
+?>
---
+++ app/Vendor/smarty/vam_plugins/function.product_price_save.php
@@ -0,0 +1,53 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   VamShop - http://vamshop.com
+   -----------------------------------------------------------------------------------------
+   Copyright (c) 2014 VamSoft Ltd.
+   License - http://vamshop.com/license.html
+   ---------------------------------------------------------------------------------------*/
+
+function smarty_function_product_price_save($params, $template)
+{
+	global $content;
+	
+	App::uses('CurrencyBaseComponent', 'Controller/Component');
+	$CurrencyBase =& new CurrencyBaseComponent(new ComponentCollection());
+
+	if ($content['Content']['content_type_id'] == 7) {
+		$price_value = $content['ContentDownloadable']['price'];
+		$price_special = $content['ContentDownloadable']['old_price'];
+	} else {
+		$price_value = $content['ContentProduct']['price'];
+		$price_special = $content['ContentProduct']['old_price'];
+	}
+
+	if ($content['ContentProduct']['price'] > $price_special) {
+		return;
+	}
+
+	if(isset($params['price'])) $price_special = $params['price'];
+
+	$price_save	= $CurrencyBase->display_price($price_special-$price_value);	
+	$price_special_percent	= ($price_special > $price_value) ? 100-($price_value*100/$price_special) : 0;	
+	
+	$price_special = $price_save . '('.round($price_special_percent).'%)';
+	
+	echo $price_special;
+}
+
+function smarty_help_function_product_price_save () {
+	?>
+	<h3><?php echo __('What does this tag do?') ?></h3>
+	<p><?php echo __('Displays the product price for the current content.') ?></p>
+	<h3><?php echo __('How do I use it?') ?></h3>
+	<p><?php echo __('Just insert the tag into your template like:') ?> <code>{product_price_save}</code></p>
+	<h3><?php echo __('What parameters does it take?') ?></h3>
+	<ul>
+		<li><em>(<?php echo __('None') ?>)</em></li>
+	</ul>
+	<?php
+}
+
+function smarty_about_function_product_price_save () {
+}
+?>
