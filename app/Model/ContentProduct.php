48e72261cee7a7b90630f79113b905fa70610694
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2014-11-30 11:35:12 +0000

    Корректировка модели ContentProduct

--- app/Model/ContentProduct.php
+++ app/Model/ContentProduct.php
@@ -61,6 +61,7 @@ class ContentProduct extends AppModel {
                         $special_price = $Special->find('first',array('conditions' => array('ContentSpecial.content_id' => $value['ContentProduct']['content_id'])));
 
                         if($special_price['ContentSpecial']['price'] > 0) { 
+                        $results[$key]['ContentProduct']['old_price'] = $results[$key]['ContentProduct']['price'];
                         if((isset($special_price['ContentSpecial']['date_start']) && time() >= strtotime($special_price['ContentSpecial']['date_start'])) or !isset($special_price['ContentSpecial']['date_start'])) { 
                         if(isset($special_price['ContentSpecial']['date_end']) && time() <= strtotime($special_price['ContentSpecial']['date_end']) or !isset($special_price['ContentSpecial']['date_end'])) { 
                         $price = $special_price['ContentSpecial']['price'];
