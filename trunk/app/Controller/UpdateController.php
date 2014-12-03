78f049d3d5a0093334d66db840119fa758138ea7
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2014-12-03 16:02:09 +0000

    Корректировка php предупреждений

--- trunk/app/Controller/UpdateController.php
+++ trunk/app/Controller/UpdateController.php
@@ -14,8 +14,8 @@ class UpdateController extends AppController {
 
 	public function admin_update($ajax_request = false)
 	{
-		$this->data->current_version = file_get_contents('./version.txt');
-		$this->data->latest_version = $this->Check->get_latest_update_version();
+		$this->data['current_version'] = file_get_contents('./version.txt');
+		$this->data['latest_version'] = $this->Check->get_latest_update_version();
 
      	$this->set('error','0');
      	$this->set('success','0');
@@ -24,44 +24,44 @@ class UpdateController extends AppController {
 			$this->set('current_crumb', __('Update', true));
 			$this->set('title_for_layout', __('Update', true));
 
-		if($this->data->current_version >= $this->data->latest_version) {
+		if($this->data['current_version'] >= $this->data['latest_version']) {
 			$this->set('error','1');
 		} else {
-            $this->data->versions = $this->Check->get_list_update_version($this->data->current_version);
-            $this->data->versions = explode(';',$this->data->versions);
-            $this->data->current_version = '';
-            foreach($this->data->versions as $version) {
+            $this->data['versions'] = $this->Check->get_list_update_version($this->data['current_version']);
+            $this->data['versions'] = explode(';',$this->data['versions']);
+            $this->data['current_version'] = '';
+            foreach($this->data['versions'] as $version) {
             	$this->Check->get_update_archive($version);
         		App::import('Vendor', 'PclZip', array('file' => 'pclzip'.DS.'zip.php'));
-				$this->data->zip_dir = basename($version);
-            	if(!@mkdir('../tmp/updates/'.$this->data->zip_dir, 0777))
+				$this->data['zip_dir'] = basename($version);
+            	if(!@mkdir('../tmp/updates/'.$this->data['zip_dir'], 0777))
 					die("<font color='red'>Error : Unable to create directory</font><br />");
-				$this->data->archive = new PclZip('../tmp/updates/'.$version.'.zip');
-				if ($this->data->archive->extract(PCLZIP_OPT_PATH,'../tmp/updates/'.$this->data->zip_dir) == 0)
+				$this->data['archive'] = new PclZip('../tmp/updates/'.$version.'.zip');
+				if ($this->data['archive']->extract(PCLZIP_OPT_PATH,'../tmp/updates/'.$this->data['zip_dir']) == 0)
 					die("<font color='red'>Error : Unable to unzip archive</font>");
-                $this->data->description = file_get_contents('../tmp/updates/'.$this->data->zip_dir.'/description.xml');
-                $this->data->description = $this->xml2array($this->data->description);
-                if(isset($this->data->description['files']['file']['value']) && $this->data->description['files']['file']['value'] != '') {
-                	$this->data->tmp[0]['value'] = $this->data->description['files']['file']['value'];
-                	$this->data->tmp[0]['attr'] = $this->data->description['files']['file']['attr'];
-                	$this->data->description['files']['file'] = $this->data->tmp;
+                $this->data['description'] = file_get_contents('../tmp/updates/'.$this->data['zip_dir'].'/description.xml');
+                $this->data['description'] = $this->xml2array($this->data['description']);
+                if(isset($this->data['description']['files']['file']['value']) && $this->data['description']['files']['file']['value'] != '') {
+                	$this->data['tmp'][0]['value'] = $this->data['description']['files']['file']['value'];
+                	$this->data['tmp'][0]['attr'] = $this->data['description']['files']['file']['attr'];
+                	$this->data['description']['files']['file'] = $this->data['tmp'];
                 }
 
 
-                if(count($this->data->description['files']['file']) > 0) {
-                	foreach($this->data->description['files']['file'] as $file) {
+                if(count($this->data['description']['files']['file']) > 0) {
+                	foreach($this->data['description']['files']['file'] as $file) {
 						if($file['attr']['action'] == 'create')
-							copy('../tmp/updates/'.$this->data->zip_dir.$file['attr']['path'].$file['value'], '../..'.$file['attr']['path'].$file['value']);
+							copy('../tmp/updates/'.$this->data['zip_dir'].$file['attr']['path'].$file['value'], '../..'.$file['attr']['path'].$file['value']);
 							//chmod('../..'.$file['attr']['path'].$file['value'],0755);
 						if($file['attr']['action'] == 'update') {
 							@unlink('../..'.$file['attr']['path'].$file['value']);
-							@copy('../tmp/updates/'.$this->data->zip_dir.$file['attr']['path'].$file['value'], '../..'.$file['attr']['path'].$file['value']);
+							@copy('../tmp/updates/'.$this->data['zip_dir'].$file['attr']['path'].$file['value'], '../..'.$file['attr']['path'].$file['value']);
 							//chmod('../..'.$file['attr']['path'].$file['value'],0755);
 						}
 						if($file['attr']['action'] == 'delete')
 							@unlink('../..'.$file['attr']['path'].$file['value']);
                         if($file['attr']['action'] == 'sql') {
-							$lines = file('../tmp/updates/'.$this->data->zip_dir.'/'.$file['value']);
+							$lines = file('../tmp/updates/'.$this->data['zip_dir'].'/'.$file['value']);
 							foreach ($lines as $line) {
     							if (substr($line, 0, 2) == '--' || $line == '')
         						continue;
@@ -75,13 +75,13 @@ class UpdateController extends AppController {
                 	}
                 }
       		App::import('Vendor', 'DeleteAll', array('file' => 'deleteall'.DS.'deleteall.php'));    
-      		@deleteAll('../tmp/updates/'.$this->data->zip_dir);
+      		@deleteAll('../tmp/updates/'.$this->data['zip_dir']);
       		@unlink('../tmp/updates/'.$version.'.zip');
-      		$this->data->current_version = $version;
+      		$this->data['current_version'] = $version;
             }
             $filename = './version.txt';
 			$handle = fopen($filename, 'w+');
-			fwrite($handle, $this->data->current_version);
+			fwrite($handle, $this->data['current_version']);
 			fclose($handle);
          	$this->set('success','1');
 		}
@@ -91,8 +91,8 @@ class UpdateController extends AppController {
 	{
   		$this->set('current_crumb', __('Update', true));
 		$this->set('title_for_layout', __('Update', true));
-		$this->data->current_version = file_get_contents('./version.txt');
-		$this->data->latest_version = $this->Check->get_latest_update_version();
+		$this->data['current_version'] = file_get_contents('./version.txt');
+		$this->data['latest_version'] = $this->Check->get_latest_update_version();
 		$this->set('update_data',$this->data);
 	}
 	
--- trunk/app/View/Update/admin.ctp
+++ trunk/app/View/Update/admin.ctp
@@ -8,11 +8,11 @@
 
 echo $this->Admin->ShowPageHeaderStart($current_crumb, 'cus-arrow-refresh');
 
-echo '<p>'.__('VamShop Version:').' <strong>'.$update_data->current_version.'</strong></p>';
+echo '<p>'.__('VamShop Version:').' <strong>'.$update_data['current_version'].'</strong></p>';
 
-if($update_data->current_version >= $update_data->latest_version) {	echo '<p>'.__('You have latest version of VamShop. No update needed.').'</p>';
+if($update_data['current_version'] >= $update_data['latest_version']) {	echo '<p>'.__('You have latest version of VamShop. No update needed.').'</p>';
 } else { 
-	echo '<p>'.__('Current VamShop Version:').' <strong>'.$update_data->latest_version.'</strong></p>';
+	echo '<p>'.__('Current VamShop Version:').' <strong>'.$update_data['latest_version'].'</strong></p>';
 	echo '<p>'.__('Click Update button to start VamShop AutoUpdate.').'</p>';
 	echo '<div class="alert alert-error"><i class="cus-error"></i> '.__('Don\'t forget backup your files and database before update at Admin - Tools - Database Backup.').'</div>';
 	echo '<br />';
