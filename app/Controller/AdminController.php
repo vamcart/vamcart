f302ebbb095b302d709dafcd2b48e7fd367292a2
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2014-12-16 19:15:50 +0000

    Корректировка главной страницы админки, корректировка суммы

--- app/Controller/AdminController.php
+++ app/Controller/AdminController.php
@@ -162,7 +162,7 @@ class AdminController extends AppController {
 
 		$this->set('total_orders',$total_orders[0]['cnt']);
 		$this->set('pending_orders',$pending_orders);
-		$this->set('total_sales',($total_orders[0]['summ'] > 0) ? $total_orders[0]['summ'] : 0);
+		$this->set('total_sales',($total_orders[0]['summ'] > 0) ? number_format($total_orders[0]['summ'], 0) : 0);
 		$this->set('total_customers',$total_customers);
 
 			
