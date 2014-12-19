a93c5713d269fee508c3006f002ee3cfe8fe7e9e
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2014-12-19 20:23:26 +0000

    Добавлен новый шаблон на bootstrap 3

--- trunk/app/webroot/js/vamshop3/vamshop3.js
+++ trunk/app/webroot/js/vamshop3/vamshop3.js
@@ -1,9 +1,37 @@
+//<![CDATA[
 $(document).ready(function(){
-
-  $.material.init();
-
-  $(".toggle-menu").jPushMenu({closeOnClickLink: false});
-  $(".dropdown-toggle").dropdown();
-
+  $('.toggle-menu').jPushMenu({closeOnClickLink: false});
+  $('.dropdown-toggle').dropdown();
+});
+//]]>
+$(document).ready(function() {
+    $.material.init();
 });
 
+$(function(){
+ 
+	$(document).on( 'scroll', function(){
+ 
+		if ($(window).scrollTop() > 100) {
+			$('.scroll-top-wrapper').addClass('show');
+		} else {
+			$('.scroll-top-wrapper').removeClass('show');
+		}
+	});
+ 
+	$('.scroll-top-wrapper').on('click', scrollToTop);
+});
+ 
+function scrollToTop() {
+	verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
+	element = $('body');
+	offset = element.offset();
+	offsetTop = offset.top;
+	$('html, body').animate({scrollTop: offsetTop}, 500, 'linear');
+}
+
+$('#nav').affix({
+      offset: {
+        top: $('header').height()
+      }
+});
