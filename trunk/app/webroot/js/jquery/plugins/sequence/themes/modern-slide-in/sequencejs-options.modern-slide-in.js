394b646fd2f1360a5f799e1d43a6a6c80058f307
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2014-12-20 12:35:13 +0000

    Корректировка настроек слайдера

--- trunk/app/webroot/js/jquery/plugins/sequence/themes/modern-slide-in/sequencejs-options.modern-slide-in.js
+++ trunk/app/webroot/js/jquery/plugins/sequence/themes/modern-slide-in/sequencejs-options.modern-slide-in.js
@@ -2,17 +2,7 @@ $(document).ready(function(){
     var options = {
         nextButton: true,
         prevButton: true,
-        pagination: true,
-        animateStartingFrameIn: true,
-        autoPlay: true,
-        autoPlayDelay: 3000,
-        preloader: true,
-        preloadTheseFrames: [1],
-        preloadTheseImages: [
-            "images/tn-model1.png",
-            "images/tn-model2.png",
-            "images/tn-model3.png"
-        ]
+        autoPlay: false,
     };
     
     var mySequence = $("#sequence").sequence(options).data("sequence");
