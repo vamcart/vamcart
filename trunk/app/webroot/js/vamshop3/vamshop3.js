b238fea9fc0b168d372aa19a0b83887aa97446e6
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2014-12-20 15:03:47 +0000

    Корректировка js кода

--- trunk/app/webroot/js/vamshop3/vamshop3.js
+++ trunk/app/webroot/js/vamshop3/vamshop3.js
@@ -38,3 +38,36 @@ $('#nav').affix({
         top: $('header').height()
       }
 });
+
+// Color box for product images
+$(document).ready(function(){
+  $(".colorbox").colorbox({rel: "colorbox"});
+  $(".ask").colorbox({});
+  $(".buy").colorbox({});
+});
+
+// Anti-spam for forms
+$(document).ready(function(){
+$(function($){
+
+    $('.form-anti-bot, .form-anti-bot-2').hide(); // hide inputs from users
+    var answer = $('.form-anti-bot input#anti-bot-a').val(); // get answer
+    $('.form-anti-bot input#anti-bot-q').val( answer ); // set answer into other input
+
+    if ( $('form input#anti-bot-q').length == 0 ) {
+        var current_date = new Date();
+        var current_year = current_date.getFullYear();
+        $('form').append('<input type="hidden" name="anti-bot-q" id="anti-bot-q" value="'+current_year+'" />'); // add whole input with answer via javascript to form
+    }
+
+});
+});
+
+// Focus input
+$(document).ready(function(){
+$(function () {
+  $(window).load(function () {
+    $('#contentform :input:text:visible:enabled:first').focus();
+  });
+})
+});
