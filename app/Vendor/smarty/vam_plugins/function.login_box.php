1b9a99a3fd1726e864983c2d649ea3663fc41b80
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2014-12-23 20:15:34 +0000

    Корректировка шаблонов

--- app/Vendor/smarty/vam_plugins/function.login_box.php
+++ app/Vendor/smarty/vam_plugins/function.login_box.php
@@ -14,15 +14,15 @@ $template = '
 	<h3 class="widget-title">{lang}Login{/lang}</h3>
 		<form action="{base_path}/site/login?return_url={$return_url}" method="post">
 			<div class="form-group">
-			<label class="col-sm-3 control-label" for="inputEmail">{lang}E-mail{/lang}</label>
-				<div class="col-sm-9">
-					<input type="text" class="form-control" name="data[Customer][email]" id="inputEmail" placeholder="{lang}E-mail{/lang}">
+			<label class="col-sm-2 control-label" for="inputEmail">{lang}E-mail{/lang}</label>
+				<div class="col-sm-10">
+					<input type="text" class="form-control" name="data[Customer][email]" id="inputEmail"">
 				</div>
 			</div>
 			<div class="form-group">
-			<label class="col-sm-3 control-label" for="inputPassword">{lang}Password{/lang}</label>
-				<div class="col-sm-9">
-					<input type="password" class="form-control" name="data[Customer][password]" id="inputPassword" placeholder="{lang}Password{/lang}" autocomplete="off">
+			<label class="col-sm-2 control-label" for="inputPassword">{lang}Password{/lang}</label>
+				<div class="col-sm-10">
+					<input type="password" class="form-control" name="data[Customer][password]" id="inputPassword" autocomplete="off">
 				</div>
 			</div>
 		<button type="submit" class="btn btn-default"><i class="fa fa-sign-in"></i> {lang}Login{/lang}</button>
