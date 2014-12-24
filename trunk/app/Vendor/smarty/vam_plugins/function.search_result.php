914909a6b94bdd20b454723ac0f3778d72f0828e
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2014-12-24 14:36:30 +0000

    Корректировка поиска, не выводятся дубликаты

--- trunk/app/Vendor/smarty/vam_plugins/function.search_result.php
+++ trunk/app/Vendor/smarty/vam_plugins/function.search_result.php
@@ -163,13 +163,13 @@ function smarty_function_search_result($params, $template)
 							      'ContentDescription.description LIKE' => '%' . $_GET['keyword'] . '%')));
 		$Content->recursive = 1;
 
-		$content_total = $content_list_data = $Content->find('count', array('conditions' => $search_conditions));;
+		$content_total = $content_list_data = $Content->find('count', array('conditions' => $search_conditions, 'group' => array('Content.alias')));
 
 
 		if ($vars['page'] == 'all') {
-			$content_list_data = $Content->find('all', array('conditions' => $search_conditions));
+			$content_list_data = $Content->find('all', array('conditions' => $search_conditions, 'group' => array('Content.alias')));
 		} else {
-			$content_list_data = $Content->find('all', array('conditions' => $search_conditions, 'limit' => $params['limit'], 'page' => $vars['page']));
+			$content_list_data = $Content->find('all', array('conditions' => $search_conditions, 'group' => array('Content.alias'), 'limit' => $params['limit'], 'page' => $vars['page']));
 		}
 
 		$content_list = array();
