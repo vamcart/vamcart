89c3dcfaf59a195c1046c16827867f9f6b71872b
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2014-12-24 09:39:57 +0000

    Перевод для новой метки shopping_cart_quantity

---
+++ app/Vendor/smarty/vam_plugins/function.shopping_cart_total.php
@@ -0,0 +1,48 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   VamShop - http://vamshop.com
+   -----------------------------------------------------------------------------------------
+   Copyright (c) 2014 VamSoft Ltd.
+   License - http://vamshop.com/license.html
+   ---------------------------------------------------------------------------------------*/
+
+function smarty_function_shopping_cart_total($params, $template)
+{
+	global $order;
+	global $config;
+	global $content;
+
+
+	$order_items = array();
+	if (!isset($order['OrderProduct'])) {
+			return;
+	}
+
+	$total_quantity = null;
+	$total_weight = null;
+
+	foreach($order['OrderProduct'] AS $cart_item)
+	{
+		$total_quantity += $cart_item['quantity'];
+		$total_weight += $cart_item['weight']*$cart_item['quantity'];
+	}
+
+	return $total_quantity;
+}
+
+function smarty_help_function_shopping_cart_total() {
+	?>
+	<h3><?php echo __('What does this tag do?') ?></h3>
+	<p><?php echo __('Displays the total quantity products in shopping cart.') ?></p>
+	<h3><?php echo __('How do I use it?') ?></h3>
+	<p><?php echo __('Just insert the tag into your template like:') ?> <code>{shopping_cart_total}</code></p>
+	<h3><?php echo __('What parameters does it take?') ?></h3>
+	<ul>
+		<li><em>(<?php echo __('None') ?>)</em></li>
+	</ul>
+	<?php
+}
+
+function smarty_about_function_shopping_cart_total() {
+}
+?>
