7a05723d5800dedfb9561584a01657524d7fdd10
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2014-12-26 07:30:15 +0000

    Добавлена метка shopping_cart_order_total для вывода суммы заказа

---
+++ app/Vendor/smarty/vam_plugins/function.shopping_cart_order_total.php
@@ -0,0 +1,48 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   VamShop - http://vamshop.com
+   -----------------------------------------------------------------------------------------
+   Copyright (c) 2014 VamSoft Ltd.
+   License - http://vamshop.com/license.html
+   ---------------------------------------------------------------------------------------*/
+
+function smarty_function_shopping_cart_order_total($params, $template)
+{
+	global $order;
+	global $config;
+	global $content;
+
+	App::uses('CurrencyBaseComponent', 'Controller/Component');
+	$CurrencyBase = new CurrencyBaseComponent(new ComponentCollection());
+
+  $order_total = 0;
+
+	$order_items = array();
+	if (!isset($order['OrderProduct'])) {
+			return;
+	}
+
+	if ($order['Order']['total'] > 0) {
+  $order_total = $CurrencyBase->display_price($order['Order']['total']);
+	}
+
+
+	return $order_total;
+}
+
+function smarty_help_function_shopping_cart_order_total() {
+	?>
+	<h3><?php echo __('What does this tag do?') ?></h3>
+	<p><?php echo __('Displays the order total in shopping cart.') ?></p>
+	<h3><?php echo __('How do I use it?') ?></h3>
+	<p><?php echo __('Just insert the tag into your template like:') ?> <code>{shopping_cart_order_total}</code></p>
+	<h3><?php echo __('What parameters does it take?') ?></h3>
+	<ul>
+		<li><em>(<?php echo __('None') ?>)</em></li>
+	</ul>
+	<?php
+}
+
+function smarty_about_function_shopping_cart_order_total() {
+}
+?>
