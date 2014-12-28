4d31f0b6f3c6634a2ce80fa26ca03e5f7eca464d
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2014-12-28 14:27:29 +0000

    Корректировка кода

--- trunk/app/webroot/js/vamshop.js
+++ trunk/app/webroot/js/vamshop.js
@@ -1,3 +1,39 @@
+// Slide menu
+$(document).ready(function(){
+  $('.toggle-menu').jPushMenu({closeOnClickLink: false});
+  $('.dropdown-toggle').dropdown();
+});
+
+// Scroll to top button 
+$(function(){
+ 
+	$(document).on( 'scroll', function(){
+ 
+		if ($(window).scrollTop() > 100) {
+			$('.scroll-top-wrapper').addClass('show');
+		} else {
+			$('.scroll-top-wrapper').removeClass('show');
+		}
+	});
+ 
+	$('.scroll-top-wrapper').on('click', scrollToTop);
+});
+ 
+function scrollToTop() {
+	verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
+	element = $('body');
+	offset = element.offset();
+	offsetTop = offset.top;
+	$('html, body').animate({scrollTop: offsetTop}, 500, 'linear');
+}
+
+// Fixed nav
+$('#nav').affix({
+      offset: {
+        top: $('header').height()
+      }
+});
+
 // Responsive equal height
 // http://codepen.io/micahgodbolt/pen/FgqLc
 
@@ -34,208 +70,12 @@ var currentTallest = 0,
 
 if ($(window).width() > 767) {
 $(window).load(function() {
-  equalheight('.thumbnails .thumbnail');
+  equalheight('.featured-categories .thumbnails .thumbnail');
+  equalheight('.shop-products .thumbnails .thumbnail');
 });
 
 $(window).resize(function(){
-  equalheight('.thumbnails .thumbnail');
+  equalheight('.featured-categories .thumbnails .thumbnail');
+  equalheight('.shop-products .thumbnails .thumbnail');
 });
 }
-
-/*
-|--------------------------------------------------------------------------
-| UItoTop jQuery Plugin 1.2 by Matt Varone
-| http://www.mattvarone.com/web-design/uitotop-jquery-plugin/
-|--------------------------------------------------------------------------
-*/
-(function($){
-	$.fn.UItoTop = function(options) {
-
- 		var defaults = {
-    			text: '',
-    			min: 200,
-    			inDelay:600,
-				outDelay:400,
-				containerID: 'toTop',
-    			containerHoverID: 'toTopHover',
-    			scrollSpeed: 1200,
-    			easingType: 'linear'
- 		    },
-            settings = $.extend(defaults, options),
-            containerIDhash = '#' + settings.containerID,
-            containerHoverIDHash = '#'+settings.containerHoverID;
-		
-		$('body').append('<a href="#" id="'+settings.containerID+'">'+settings.text+'</a>');
-		$(containerIDhash).hide().on('click.UItoTop',function(){
-			$('html, body').animate({scrollTop:0}, settings.scrollSpeed, settings.easingType);
-			$('#'+settings.containerHoverID, this).stop().animate({'opacity': 0 }, settings.inDelay, settings.easingType);
-			return false;
-		})
-		.prepend('<span id="'+settings.containerHoverID+'"></span>')
-		.hover(function() {
-				$(containerHoverIDHash, this).stop().animate({
-					'opacity': 1
-				}, 600, 'linear');
-			}, function() { 
-				$(containerHoverIDHash, this).stop().animate({
-					'opacity': 0
-				}, 700, 'linear');
-			});
-					
-		$(window).scroll(function() {
-			var sd = $(window).scrollTop();
-			if(typeof document.body.style.maxHeight === "undefined") {
-				$(containerIDhash).css({
-					'position': 'absolute',
-					'top': sd + $(window).height() - 50
-				});
-			}
-			if ( sd > settings.min ) 
-				$(containerIDhash).fadeIn(settings.inDelay);
-			else 
-				$(containerIDhash).fadeOut(settings.Outdelay);
-		});
-};
-})(jQuery);
-
-// Only apply the fixed stuff to desktop devices
-// -----------------------------------------------------------------------------
-
-$(function(){
-
-    if ( ! /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
-
-        // #navigation fixed
-        if ($(window).width() > 767) {
-            //
-            var menu = $('#navigation'),
-                pos = menu.offset();
-
-            $(window).scroll(function(){
-                if($(this).scrollTop() > pos.top+menu.height() && menu.hasClass('default')){
-                    menu.fadeOut('fast', function(){
-                        $(this).removeClass('default').addClass('fixed').fadeIn('fast');
-                    });
-                } else if($(this).scrollTop() <= pos.top && menu.hasClass('fixed')){
-                    menu.fadeOut('fast', function(){
-                        $(this).removeClass('fixed').addClass('default').fadeIn('fast');
-                    });
-                }
-            });
-        }
-    }
-
-});
-
-// ON DOCUMENT READY
-// -----------------------------------------------------------------------------
-$(document).ready(function(){
-
-    // footer widget hide/show on mobile devices
-    // -----------------------------------------------------------------------------
-    function doFooter (){
-        if ($(window).width() > 767) {
-            $('#footer .widget-inner').slideDown("slow").removeClass("do");
-            $('#footer .widget-title').removeClass("do");
-        } else {
-            $('#footer .widget-inner').slideUp("fast").addClass("do");
-            $('#footer .widget-title').addClass("do");
-        }
-    }
-    $(window).resize(function(){ doFooter(); });
-    $(window).load(function(){ doFooter(); });
-    $('#footer .widget-title').click(function(){
-        $(this).next('.widget-inner.do').toggle("slow");
-    });
-
-    // Shoping cart SHOW/HIDE
-    // -----------------------------------------------------------------------------
-    $('.shopping-cart .cart').hover(
-        function(){
-            $('.shopping-cart .cart-dropdown').show();
-        },
-        function (){
-            $('.shopping-cart .cart-dropdown').hide();
-        }
-    );
-    $('.shopping-cart .cart-dropdown').bind({
-        mouseenter: function(){
-            $(this).show();
-        },
-        mouseleave: function(){
-            $(this).hide();
-        }
-    });
-
-    $('ul.icons.check a').click(function(){
-
-        if ($(this).closest('li').hasClass('on')) {
-            $(this).closest('li').removeClass('on');
-            return false;
-        }
-
-        else {
-            $(this).closest('li').addClass('on');
-            return false;
-        }
-
-    });
-
-    // tooltip
-    // -----------------------------------------------------------------------------
-    $("a[data-toggle=tooltip]").tooltip();
-
-    // Main menu
-    // -----------------------------------------------------------------------------
-    (function() {
-		var $menu = $('.navbar-inner ul.nav'),
-			optionsList = '<option value="" selected>...</option>';
-		$menu.find('li').each(function() {
-			var $this   = $(this),
-				$anchor = $this.children('a'),
-				depth   = $this.parents('ul').length - 1,
-				indent  = '';
-			if( depth ) {
-				while( depth > 0 ) {
-					indent += ' - ';
-					depth--;
-				}
-			}
-			optionsList += '<option value="' + $anchor.attr('href') + '">' + indent + ' ' + $anchor.text() + '</option>';
-		}).end().after('<div class="res-menu-wrap"><select class="res-menu">' + optionsList + '</select><div class="res-menu-title"></div></div>');
-
-		$('.res-menu').on('change', function() {
-			window.location = $(this).val();
-		});
-	})();
-    
-    // To Top Button
-    // -----------------------------------------------------------------------------
-    $(function(){
-        $().UItoTop({ easingType: 'easeOutQuart' });
-    });
-
-    // Placeholder
-    // -----------------------------------------------------------------------------
-    $('[placeholder]').focus(function() {
-        var input = $(this);
-        if (input.val() == input.attr('placeholder')) {
-            input.val('');
-            input.removeClass('placeholder');
-        }
-    }).blur(function() {
-        var input = $(this);
-        if (input.val() == '' || input.val() == input.attr('placeholder')) {
-            input.addClass('placeholder');
-            input.val(input.attr('placeholder'));
-        }
-        }).blur().parents('form').submit(function() {
-            $(this).find('[placeholder]').each(function() {
-                var input = $(this);
-                if (input.val() == input.attr('placeholder')) {
-                    input.val('');
-                }
-            })
-        });
-
-});
