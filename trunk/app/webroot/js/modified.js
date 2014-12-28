9e924a4acfb82ae48df254b7a5245cca500897d0
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2014-12-28 19:27:34 +0000

    Корректировка подсветки формы

--- trunk/app/webroot/js/modified.js
+++ trunk/app/webroot/js/modified.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
   $("form#contentform :input").change(function() {
     $("input[id='" + this.id + "']").addClass("modified");
+    $("radio[id='" + this.id + "']").addClass("modified");
+    $("select[id='" + this.id + "']").addClass("modified");
+    $("checkbox[id='" + this.id + "']").addClass("modified");
+    $("textarea[id='" + this.id + "']").addClass("modified");
   });
 });
