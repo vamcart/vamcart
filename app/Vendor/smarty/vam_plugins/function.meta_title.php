b06449a3df310c9e8c61eabd4ff35a58ffa713cd
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2015-01-18 10:01:13 +0000

    Добавлена новая метка, выводящая meta title

---
+++ app/Vendor/smarty/vam_plugins/function.meta_title.php
@@ -0,0 +1,37 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   VamShop - http://vamshop.com
+   -----------------------------------------------------------------------------------------
+   Copyright (c) 2014 VamSoft Ltd.
+   License - http://vamshop.com/license.html
+   ---------------------------------------------------------------------------------------*/
+
+function smarty_function_meta_title($params, $template)
+{
+	global $content;
+	
+	if ($content['ContentDescription']['meta_title']) { 
+	$result = '<meta name="description" content="'.$content['ContentDescription']['meta_title'].'" />';
+	} else {
+	$result = null;
+	}
+
+	return $result;
+}
+
+function smarty_help_function_meta_title() {
+	?>
+	<h3><?php echo __('What does this tag do?') ?></h3>
+	<p><?php echo __('Displays the current meta title.') ?></p>
+	<h3><?php echo __('How do I use it?') ?></h3>
+	<p><?php echo __('Just insert the tag into your template like:') ?> <code>{meta_title}</code></p>
+	<h3><?php echo __('What parameters does it take?') ?></h3>
+	<ul>
+		<li><em>(<?php echo __('None') ?>)</em></li>
+	</ul>
+	<?php
+}
+
+function smarty_about_function_meta_title() {
+}
+?>
