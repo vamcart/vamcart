ab8ed8ecdc2e82f091b38504d65305b7f448afef
vam@kypi.ru <vam@kypi.ru@fde51352-d033-11de-a95b-c1c8cd607182>
2015-03-08 16:32:15 +0000

    Добавлена напоминалка пароля

--- trunk/app/Controller/SiteController.php
+++ trunk/app/Controller/SiteController.php
@@ -218,4 +218,195 @@ class SiteController extends AppController {
 		$this->redirect('/');
 	}
 
+	public function password_recovery()
+	{
+		global $config;
+
+  	$spam_flag = false;
+
+		if ( trim( $_POST['anti-bot-q'] ) != date('Y') ) { // answer is wrong - maybe spam
+			$spam_flag = true;
+			if ( empty( $_POST['anti-bot-q'] ) ) { // empty answer - maybe spam
+				$antispam_error_message .= 'Error: empty answer. ['.$_POST['anti-bot-q'].']<br> ';
+			} else {
+				$antispam_error_message .= 'Error: answer is wrong. ['.$_POST['anti-bot-q'].']<br> ';
+			}
+		}
+		if ( ! empty( $_POST['anti-bot-e-email-url'] ) ) { // field is not empty - maybe spam
+			$spam_flag = true;
+			$antispam_error_message .= 'Error: field should be empty. ['.$_POST['anti-bot-e-email-url'].']<br> ';
+		}
+					
+		if (isset($_POST['customer']) && $spam_flag == false) {
+			$customer = new Customer();
+			$customer->set($_POST['customer']);
+			if ($customer->validates()) {
+				
+		App::uses('Sanitize', 'Utility');
+		$clean = new Sanitize();
+		$clean->clean($_POST);
+
+			$user = $this->Customer->find('first', array('conditions' => array('email' => $_POST['customer']['email'])));
+
+			if(empty($user))
+			{
+				$this->Session->setFlash(__('No customers found for this email.', true), 'bootstrap_alert_error');
+  			$this->redirect('/customer/password_recovery'  . $config['URL_EXTENSION']);
+			}
+
+			if(!empty($user))
+			{
+			  
+			  $customer_data = array();
+			  
+			  $customer_data['Customer']['id'] = $user['Customer']['id'];
+			  $customer_data['Customer']['code'] = $this->RandomString(32);
+			  
+				$customer->save($customer_data);
+
+				// Retrieve email template
+				$this->EmailTemplate->unbindModel(array('hasMany' => array('EmailTemplateDescription')));
+				$this->EmailTemplate->bindModel(array(
+					'hasOne' => array(
+						'EmailTemplateDescription' => array(
+							'className'  => 'EmailTemplateDescription',
+							'conditions' => 'language_id = ' . $this->Session->read('Customer.language_id')
+						)
+					)
+				));
+
+				// Get email template
+				$email_template = $this->EmailTemplate->findByAlias('password_recovery_verification');
+
+				// Email Subject
+				$subject = $email_template['EmailTemplateDescription']['subject'];
+				$subject = $config['SITE_NAME'] . ' - ' . $subject;
+
+				$body = $email_template['EmailTemplateDescription']['content'];
+				$body = str_replace('{$link}', FULL_BASE_URL . BASE . '/site/new_password/' . $customer_data['Customer']['code'] . '/', $body);
+
+				$this->Email->init();
+				$this->Email->From = $config['NEW_ORDER_FROM_EMAIL'];
+				$this->Email->FromName = __($config['NEW_ORDER_FROM_NAME'],true);
+
+				// Send to customer
+				$this->Email->AddAddress($_POST['customer']['email']);
+				// Send to admin
+				//$this->Email->AddCC($config['SEND_EXTRA_EMAIL']);
+				$this->Email->Subject = $subject;
+
+				// Email Body
+				$this->Email->Body = $body;
+
+				// Sending mail
+				$this->Email->send();
+
+			$this->Session->setFlash(__('New password validation email was send.', true), 'bootstrap_alert_success');
+			$this->redirect('/customer/password_recovery'  . $config['URL_EXTENSION']);
+			}	
+				
+			} else {
+				$errors = $customer->invalidFields();
+				$this->Session->write('loginFormErrors', $errors);
+				$this->Session->write('loginFormData', $customer->data['Customer']);
+				$this->redirect('/customer/password_recovery'  . $config['URL_EXTENSION']);
+			}
+		} else {
+			$this->redirect('/customer/password_recovery'  . $config['URL_EXTENSION']);
+		}
+	}
+
+	public function new_password($verification_code = false)
+	{
+		global $config;
+	  
+	  if ($verification_code) {
+	  
+			$user = $this->Customer->find('first', array('conditions' => array('code' => $verification_code)));
+
+			if(empty($user))
+			{
+				$this->Session->setFlash(__('Customer not found for this verification code.', true), 'bootstrap_alert_error');
+  			$this->redirect('/customer/password_recovery'  . $config['URL_EXTENSION']);
+			}
+
+			if(!empty($user))
+			{
+			  
+  			$customer = new Customer();
+
+			  $customer_data = array();
+			  
+			  $customer_password = $this->RandomString(8);
+			  
+			  $customer_data['Customer']['id'] = $user['Customer']['id'];
+			  $customer_data['Customer']['password'] = Security::hash($customer_password, 'sha1', true);
+
+				$customer->save($customer_data);
+
+				// Retrieve email template
+				$this->EmailTemplate->unbindModel(array('hasMany' => array('EmailTemplateDescription')));
+				$this->EmailTemplate->bindModel(array(
+					'hasOne' => array(
+						'EmailTemplateDescription' => array(
+							'className'  => 'EmailTemplateDescription',
+							'conditions' => 'language_id = ' . $this->Session->read('Customer.language_id')
+						)
+					)
+				));
+
+				// Get email template
+				$email_template = $this->EmailTemplate->findByAlias('password_recovery_new_password');
+
+				// Email Subject
+				$subject = $email_template['EmailTemplateDescription']['subject'];
+				$subject = $config['SITE_NAME'] . ' - ' . $subject;
+
+				$body = $email_template['EmailTemplateDescription']['content'];
+				$body = str_replace('{$email}', $user['Customer']['email'], $body);
+				$body = str_replace('{$password}', $customer_password, $body);
+				$body = str_replace('{$account_page}', FULL_BASE_URL . BASE . '/page/account' . $config['URL_EXTENSION'], $body);
+
+				$this->Email->init();
+				$this->Email->From = $config['NEW_ORDER_FROM_EMAIL'];
+				$this->Email->FromName = __($config['NEW_ORDER_FROM_NAME'],true);
+
+				// Send to customer
+				$this->Email->AddAddress($user['Customer']['email']);
+				// Send to admin
+				//$this->Email->AddCC($config['SEND_EXTRA_EMAIL']);
+				$this->Email->Subject = $subject;
+
+				// Email Body
+				$this->Email->Body = $body;
+
+				// Sending mail
+				$this->Email->send();
+
+			$this->Session->setFlash(__('New password email was send.', true), 'bootstrap_alert_success');
+			$this->redirect('/page/account'  . $config['URL_EXTENSION']);
+			}	
+		}	else {
+			$this->Session->setFlash(__('Login Error.', true), 'bootstrap_alert_error');
+			$this->redirect('/page/account'  . $config['URL_EXTENSION']);
+		}	
+	}
+
+
+	private function RandomString($length) 
+		{
+		$chars = array( 'a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e', 'E', 'f', 'F', 'g', 'G', 'h', 'H', 'i', 'I', 'j', 'J',  'k', 'K', 'l', 'L', 'm', 'M', 'n','N', 'o', 'O', 'p', 'P', 'q', 'Q', 'r', 'R', 's', 'S', 't', 'T',  'u', 'U', 'v','V', 'w', 'W', 'x', 'X', 'y', 'Y', 'z', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0');
+		
+		$max_chars = count($chars) - 1;
+		srand( (double) microtime()*1000000);
+		
+		$rand_str = '';
+		for($i=0;$i<$length;$i++)
+		{
+		$rand_str = ( $i == 0 ) ? $chars[rand(0, $max_chars)] : $rand_str . $chars[rand(0, $max_chars)];
+		}
+		
+		return $rand_str;
+		}
+
 }
