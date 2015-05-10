dc7575f827fbcacddc0fda268f7038242c904b9e
vamcart <vamshop@gmail.com>
2015-05-10 17:50:54 +0300

    Добавлена адаптивность в colorbox

--- app/webroot/js/vamshop.js
+++ app/webroot/js/vamshop.js
@@ -72,3 +72,22 @@ $(window).resize(function(){
   equalheight('.shop-products .thumbnails .thumbnail');
 });
 }
+
+// Make ColorBox responsive
+	jQuery.colorbox.settings.maxWidth  = '95%';
+	jQuery.colorbox.settings.maxHeight = '95%';
+
+	// ColorBox resize function
+	var resizeTimer;
+	function resizeColorBox()
+	{
+		if (resizeTimer) clearTimeout(resizeTimer);
+		resizeTimer = setTimeout(function() {
+				if (jQuery('#cboxOverlay').is(':visible')) {
+						jQuery.colorbox.load(true);
+				}
+		}, 300);
+	}
+
+	// Resize ColorBox when resizing window or changing mobile device orientation
+	jQuery(window).resize(resizeColorBox);
