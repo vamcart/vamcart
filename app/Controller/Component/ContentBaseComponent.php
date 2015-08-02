8d618f041e26fbd8416d51e4d3d9ed7cc8dc3c82
vamcart <vamshop@gmail.com>
2015-08-02 13:11:24 +0300

    Корректировка запроса контента

--- app/Controller/Component/ContentBaseComponent.php
+++ app/Controller/Component/ContentBaseComponent.php
@@ -131,7 +131,7 @@ class ContentBaseComponent extends Object
                 $this->Content->Attribute->ValAttribute->setLanguageDescriptor($this->Session->read('Customer.language_id'));
 
 		$content_conditions = "Content.id = '" . $content_alias . "' OR BINARY Content.alias = '" . $content_alias . "' AND Content.active ='1'";
-		$content = $this->Content->find('first', array('recursive' => 2, 'conditions' => $content_conditions));
+		$content = $this->Content->find('first', array('recursive' => 2, 'order' => 'Content.id DESC', 'conditions' => $content_conditions));
 
 		// Error page
 	    if (!$content) {
