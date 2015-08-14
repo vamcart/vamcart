32dce1a3022698b66ef3085b4804bd7db8940879
vamcart <vamshop@gmail.com>
2015-08-14 13:33:46 +0300

    Добавлена возможность открытия конкреной вкладки с помощью параметра в url адресе

--- app/webroot/js/vamshop.js
+++ app/webroot/js/vamshop.js
@@ -100,3 +100,13 @@ $(document).ready(function(){
 	jQuery(window).resize(resizeColorBox);
 
 });
+
+// Select tab by url
+
+$(document).ready(function(event) {
+    $('ul.nav.nav-tabs a:first').tab('show'); // Select first tab
+    $('ul.nav.nav-tabs a[href="'+ window.location.hash+ '"]').tab('show'); // Select tab by name if provided in location hash
+    $('ul.nav.nav-tabs a[data-toggle="tab"]').on('shown', function (event) {    // Update the location hash to current tab
+        window.location.hash= event.target.hash;
+    })
+});
