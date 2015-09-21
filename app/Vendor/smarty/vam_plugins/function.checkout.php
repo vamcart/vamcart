95e5968e6491c17e90aa8798040a22964b759824
vamcart <vamshop@gmail.com>
2015-09-21 21:19:19 +0300

    В модуле оформления заказа оставлены два обязательных поля - Имя и Телефон

--- app/Vendor/smarty/vam_plugins/function.checkout.php
+++ app/Vendor/smarty/vam_plugins/function.checkout.php
@@ -53,11 +53,11 @@ $(this).parent().addClass("selected");
         required: true,
         minlength: 2      
      },
-      email: {
-        required: true,
-        minlength: 6,
-        email: true      
-     },
+      //email: {
+        //required: true,
+        //minlength: 6,
+        //email: true      
+     //},
       phone: {
         required: true,
         minlength: 10,
@@ -68,10 +68,10 @@ $(this).parent().addClass("selected");
         required: "{lang}Required field{/lang}",
         minlength: "{lang}Required field{/lang}. {lang}Min length{/lang}: 2"
       },
-      email: {
-        required: "{lang}Required field{/lang}",
-        minlength: "{lang}Required field{/lang}. {lang}Min length{/lang}: 6"
-      },
+      //email: {
+        //required: "{lang}Required field{/lang}",
+        //minlength: "{lang}Required field{/lang}. {lang}Min length{/lang}: 6"
+      //},
       phone: {
         required: "{lang}Required field{/lang}",
         minlength: "{lang}Required field{/lang}. {lang}Min length{/lang}: 10"
