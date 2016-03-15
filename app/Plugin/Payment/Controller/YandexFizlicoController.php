b725298629fad3ea24ac403a2ad7205e22745042
vamcart <vamshop@gmail.com>
2016-03-15 09:37:38 +0300

    Добавлен новый модуль оплаты - Яндекс.Касса для физических лиц

---
+++ app/Plugin/Payment/Controller/YandexFizlicoController.php
@@ -0,0 +1,123 @@
+<?php 
+/* -----------------------------------------------------------------------------------------
+   VamShop - http://vamshop.com
+   -----------------------------------------------------------------------------------------
+   Copyright (c) 2014 VamSoft Ltd.
+   License - http://vamshop.com/license.html
+   ---------------------------------------------------------------------------------------*/
+App::uses('PaymentAppController', 'Payment.Controller');
+
+class YandexFizlicoController extends PaymentAppController {
+	public $uses = array('PaymentMethod', 'Order');
+	public $module_name = 'YandexFizlico';
+	public $icon = 'yandex.png';
+
+	public function settings ()
+	{
+		$this->set('data', $this->PaymentMethod->findByAlias($this->module_name));
+	}
+
+	public function install()
+	{
+		$new_module = array();
+		$new_module['PaymentMethod']['active'] = '1';
+		$new_module['PaymentMethod']['default'] = '0';
+		$new_module['PaymentMethod']['name'] = Inflector::humanize($this->module_name);
+		$new_module['PaymentMethod']['icon'] = $this->icon;
+		$new_module['PaymentMethod']['alias'] = $this->module_name;
+
+		$new_module['PaymentMethodValue'][0]['payment_method_id'] = $this->PaymentMethod->id;
+		$new_module['PaymentMethodValue'][0]['key'] = 'wallet';
+		$new_module['PaymentMethodValue'][0]['value'] = '';
+
+		$new_module['PaymentMethodValue'][1]['payment_method_id'] = $this->PaymentMethod->id;
+		$new_module['PaymentMethodValue'][1]['key'] = 'secret_key';
+		$new_module['PaymentMethodValue'][1]['value'] = '';
+
+		$this->PaymentMethod->saveAll($new_module);
+
+		$this->Session->setFlash(__('Module Installed'));
+		$this->redirect('/payment_methods/admin/');
+	}
+
+	public function uninstall()
+	{
+
+		$module_id = $this->PaymentMethod->findByAlias($this->module_name);
+
+		$this->PaymentMethod->delete($module_id['PaymentMethod']['id'], true);
+			
+		$this->Session->setFlash(__('Module Uninstalled'));
+		$this->redirect('/payment_methods/admin/');
+	}
+
+	public function before_process () 
+	{
+			
+		global $config;
+		
+		$order = $this->Order->read(null,$_SESSION['Customer']['order_id']);
+		
+		$payment_method = $this->PaymentMethod->find('first', array('conditions' => array('alias' => $this->module_name)));
+
+		$yandex_settings_wallet = $this->PaymentMethod->PaymentMethodValue->find('first', array('conditions' => array('key' => 'wallet')));
+		$yandex_wallet = $yandex_settings_wallet['PaymentMethodValue']['value'];
+
+		$yandex_settings_key = $this->PaymentMethod->PaymentMethodValue->find('first', array('conditions' => array('key' => 'secret_key')));
+		$yandex_key = $yandex_settings_key['PaymentMethodValue']['value'];
+
+		$success_url = 'http://'.$_SERVER['HTTP_HOST'] .  BASE . '/orders/place_order/';
+		$fail_url = 'http://'.$_SERVER['HTTP_HOST'] .  BASE . '/page/checkout' . $config['URL_EXTENSION'];
+		
+		$content = '
+		
+			<iframe frameborder="0" allowtransparency="true" scrolling="no" src="https://money.yandex.ru/embed/shop.xml?account='.$yandex_wallet.'&quickpay=shop&payment-type-choice=on&mobile-payment-type-choice=on&writer=seller&label='.$_SESSION['Customer']['order_id'].'&targets=Заказ №'.$_SESSION['Customer']['order_id'].'&targets-hint=&default-sum='.number_format($order['Order']['total'], 2).'&button-text=01&hint=&successURL='.$success_url.'" width="450" height="255"></iframe>
+			
+			';
+						
+	// Save the order
+	
+		foreach($_POST AS $key => $value)
+			$order['Order'][$key] = $value;
+		
+		// Get the default order status
+		$default_status = $this->Order->OrderStatus->find('first', array('conditions' => array('default' => '1')));
+		$order['Order']['order_status_id'] = $default_status['OrderStatus']['id'];
+
+		// Save the order
+		$this->Order->save($order);
+
+		return $content;
+	}
+
+	public function after_process()
+	{
+	}
+	
+	
+	public function result()
+	{
+		$this->layout = false;
+      $secret_key_data = $this->PaymentMethod->PaymentMethodValue->find('first', array('conditions' => array('key' => 'secret_key')));
+      $secret_key = $secret_key_data['PaymentMethodValue']['value'];
+		$order = $this->Order->read(null,$_POST['label']);
+		$crc = $_POST['sha1_hash'];
+		$hash = sha1($_POST['notification_type'].'&'.$_POST['operation_id'].'&'.$_POST['amount'].'&'.$_POST['currency'].'&'.$_POST['datetime'].'&'.$_POST['sender'].'&'.$_POST['codepro'].'&'.$secret_key.'&'.$_POST['label']);
+		$merchant_summ = number_format($_POST['withdraw_amount'], 2);
+		$order_summ = number_format($order['Order']['total'], 2);
+
+		if (($crc == $hash) && ($merchant_summ == $order_summ) && $_POST['codepro'] == 'false' && $_POST['unaccepted'] == 'false') {
+		
+		$payment_method = $this->PaymentMethod->find('first', array('conditions' => array('alias' => $this->module_name)));
+		$order_data = $this->Order->find('first', array('conditions' => array('Order.id' => $_POST['label'])));
+		$order_data['Order']['order_status_id'] = $payment_method['PaymentMethod']['order_status_id'];
+		
+		$this->Order->save($order_data);
+		
+		}
+	
+	}
+	
+}
+
+?>
---
+++ app/Plugin/Payment/View/YandexFizlico/settings.ctp
@@ -0,0 +1,21 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   VamShop - http://vamshop.com
+   -----------------------------------------------------------------------------------------
+   Copyright (c) 2014 VamSoft Ltd.
+   License - http://vamshop.com/license.html
+   ---------------------------------------------------------------------------------------*/
+
+echo $this->Form->input('yandexfizlico.wallet', array(
+	'label' => __('Yandex Wallet'),
+	'type' => 'text',
+	'value' => $data['PaymentMethodValue'][0]['value']
+	));
+
+echo $this->Form->input('yandexfizlico.secret_key', array(
+	'label' => __('Yandex Secret Key'),
+	'type' => 'text',
+	'value' => $data['PaymentMethodValue'][1]['value']
+	));
+	
+?>
