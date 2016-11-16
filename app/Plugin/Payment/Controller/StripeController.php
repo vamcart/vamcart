8396910031a384e4dc6f8d4baad9f8fb1b6e48e0
vam <vamshop@gmail.com>
2016-11-16 17:22:17 +0300

    Добавлен новый модуль оплаты - Stripe

---
+++ app/Plugin/Payment/Controller/StripeController.php
@@ -0,0 +1,100 @@
+<?php 
+/* -----------------------------------------------------------------------------------------
+   VamShop - http://vamshop.com
+   -----------------------------------------------------------------------------------------
+   Copyright (c) 2014 VamSoft Ltd.
+   License - http://vamshop.com/license.html
+   ---------------------------------------------------------------------------------------*/
+App::uses('PaymentAppController', 'Payment.Controller');
+
+class StripeController extends PaymentAppController {
+	public $uses = array('PaymentMethod', 'Order');
+	public $module_name = 'Stripe';
+	public $icon = 'stripe.png';
+
+	public function settings ()
+	{
+		$this->set('data', $this->PaymentMethod->findByAlias($this->module_name));
+	}
+
+	public function install()
+	{
+		$new_module = array();
+		$new_module['PaymentMethod']['active'] = '1';
+		$new_module['PaymentMethod']['default'] = '0';
+		$new_module['PaymentMethod']['name'] = Inflector::humanize($this->module_name);
+		$new_module['PaymentMethod']['icon'] = $this->icon;
+		$new_module['PaymentMethod']['alias'] = $this->module_name;
+
+		$new_module['PaymentMethodValue'][0]['payment_method_id'] = $this->PaymentMethod->id;
+		$new_module['PaymentMethodValue'][0]['key'] = 'secret_key';
+		$new_module['PaymentMethodValue'][0]['value'] = '';
+
+		$new_module['PaymentMethodValue'][1]['payment_method_id'] = $this->PaymentMethod->id;
+		$new_module['PaymentMethodValue'][1]['key'] = 'publish_key';
+		$new_module['PaymentMethodValue'][1]['value'] = '';
+
+		$this->PaymentMethod->saveAll($new_module);
+
+		$this->Session->setFlash(__('Module Installed'));
+		$this->redirect('/payment_methods/admin/');
+	}
+
+	public function uninstall()
+	{
+
+		$module_id = $this->PaymentMethod->findByAlias($this->module_name);
+
+		$this->PaymentMethod->delete($module_id['PaymentMethod']['id'], true);
+			
+		$this->Session->setFlash(__('Module Uninstalled'));
+		$this->redirect('/payment_methods/admin/');
+	}
+
+	public function before_process () 
+	{
+		$order = $this->Order->read(null,$_SESSION['Customer']['order_id']);
+		
+		$payment_method = $this->PaymentMethod->find('first', array('conditions' => array('alias' => $this->module_name)));
+
+		$secret_key_query = $this->PaymentMethod->PaymentMethodValue->find('first', array('conditions' => array('key' => 'secret_key')));
+		$secret_key = $secret_key_query['PaymentMethodValue']['value'];
+
+		$publish_key_query = $this->PaymentMethod->PaymentMethodValue->find('first', array('conditions' => array('key' => 'publish_key')));
+		$publish_key = $publish_key_query['PaymentMethodValue']['value'];
+		
+				$content = '	
+		<form action="' . BASE . '/orders/place_order/" method="post">
+		<button class="btn btn-default" type="submit" value="{lang}Pay With Card{/lang}"><i class="fa fa-check"></i> {lang}Pay With Card{/lang}</button>
+		  <script
+		    src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+		    data-key="'.$publish_key.'"
+		    data-amount="' . $order['Order']['total'] . '"
+		    data-name="' . $_SESSION['Customer']['order_id'] . ' ' . $order['Order']['email'] . '"
+		    data-description="'.$_SESSION['Customer']['order_id'].'"
+		    data-image="https://stripe.com/img/documentation/checkout/marketplace.png"
+		    data-locale="auto">
+		  </script>
+		</form>';
+		return $content;
+	}
+	
+	public function after_process()
+	{
+		$payment_method = $this->PaymentMethod->find('first', array('conditions' => array('alias' => $this->module_name)));
+		$order_data = $this->Order->find('first', array('conditions' => array('Order.id' => $_SESSION['Customer']['order_id'])));
+		if ($payment_method['PaymentMethod']['order_status_id'] > 0) {
+		$order_data['Order']['order_status_id'] = $payment_method['PaymentMethod']['order_status_id'];
+		
+		$this->Order->save($order_data);
+		}
+	}
+	
+	
+	public function result()
+	{
+	}
+	
+}
+
+?>
