0b265f6dc7137415a1b31e79587d97feeddd3231
vam <vamshop@gmail.com>
2017-03-24 01:53:13 +0300

    Корректировка кода

--- app/Controller/Component/OrderBaseComponent.php
+++ app/Controller/Component/OrderBaseComponent.php
@@ -95,12 +95,8 @@ class OrderBaseComponent extends Object
 	{
 		$running_total = 0;
 
-		if ($order['OrderProduct']) {
 		foreach($order['OrderProduct'] AS $product) {
-			if ($product['tax'] > 0) {
 			$running_total += ($product['quantity'] * $product['tax']);
-			}
-		}
 		}
 
 		return $running_total;
