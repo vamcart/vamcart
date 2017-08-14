70deaba6c009ed0c9159a5cb3f19aadbe436147b
vam <vamshop@gmail.com>
2017-08-14 22:05:11 +0300

    Корректировка кода добавления подарка

--- app/Plugin/ModuleGift/Controller/GiftController.php
+++ app/Plugin/ModuleGift/Controller/GiftController.php
@@ -7,44 +7,47 @@
    ---------------------------------------------------------------------------------------*/
 class GiftController extends ModuleGiftAppController {
 	public $uses = array('ModuleGift');
+	public $components = array('ContentBase');
 	
 	public function get_gift()
 	{
 		global $order;
 
 		$discount = 0;
-		//$payment_discount = $this->ModuleGift->find('first', array('conditions' => array('payment_method_id' => $order['Order']['payment_method_id'])));
-
-		//if($payment_discount['ModuleGift']['discount'] > 0) {
-
-		//$discount = $discount - (((int)$payment_discount['ModuleGift']['discount']/100)*$order['Order']['total']);	
+		$gift = $this->ModuleGift->find('first', array('conditions' => array('ModuleGift.order_total <=' => $order['Order']['total'], 'ModuleGift.content_id >' => 0), 'order' => array('ModuleGift.order_total' => 'DESC')));
 		
-		$payment_discount_product = array();
-		$payment_discount_product['OrderProduct']['order_id'] = $order['Order']['id'];
-		$payment_discount_product['OrderProduct']['name'] = __('Discount');
-		$payment_discount_product['OrderProduct']['quantity'] = 1;
-		$payment_discount_product['OrderProduct']['price'] = $discount;	
+		if ($gift) {
+		if($order['Order']['total'] >= $gift['ModuleGift']['order_total']) {
+			
+		$content_description = $this->ContentBase->get_content_description($gift['ModuleGift']['content_id']);
 
-		$payment_discount_product['OrderProduct']['content_id'] = 46;
+		$order_product_gift = array();
+		$order_product_gift['OrderProduct']['order_id'] = $order['Order']['id'];
+		$order_product_gift['OrderProduct']['name'] = $content_description['ContentDescription']['name'];
+		$order_product_gift['OrderProduct']['quantity'] = 1;
+		$order_product_gift['OrderProduct']['price'] = 0;	
+
+		$order_product_gift['OrderProduct']['content_id'] = $gift['ModuleGift']['content_id'];
 
 		// Load the OrderProduct model for saving and error checking
 		App::import('Model', 'OrderProduct');
 		$this->OrderProduct = new OrderProduct();
 		
 		// Save the new discount as an order product
-		$this->OrderProduct->save($payment_discount_product);
+		$this->OrderProduct->save($order_product_gift);
 		
 		// Save the new order totals
 		App::import('Model', 'Order');
 		$this->Order = new Order();
 		
 		$order = $this->Order->read(null,$_SESSION['Customer']['order_id']);
-		$order['Order']['total'] = 	$order['Order']['total'] + $discount;
+		$order['Order']['total'] = $order['Order']['total'] + $discount;
 
 		$this->Order->save($order);
 		
-		//}
-		
+		}
+		}
+	
 	}
 	
 }
