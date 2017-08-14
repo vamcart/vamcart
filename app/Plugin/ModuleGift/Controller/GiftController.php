2f3469e63299a1ac2e6960a33eec36788d51dcff
vam <vamshop@gmail.com>
2017-08-14 22:53:35 +0300

    Добавлена суммирование подарков

--- app/Plugin/ModuleGift/Controller/GiftController.php
+++ app/Plugin/ModuleGift/Controller/GiftController.php
@@ -14,12 +14,17 @@ class GiftController extends ModuleGiftAppController {
 		global $order;
 
 		$discount = 0;
-		$gift = $this->ModuleGift->find('first', array('conditions' => array('ModuleGift.order_total <=' => $order['Order']['total'], 'ModuleGift.content_id >' => 0), 'order' => array('ModuleGift.order_total' => 'DESC')));
+		//$gift = $this->ModuleGift->find('first', array('conditions' => array('ModuleGift.order_total <=' => $order['Order']['total'], 'ModuleGift.content_id >' => 0), 'order' => array('ModuleGift.order_total' => 'DESC')));
+		//$gifts = $gift;
 		
-		if ($gift) {
-		if($order['Order']['total'] >= $gift['ModuleGift']['order_total']) {
+		$gift = $this->ModuleGift->find('all', array('conditions' => array('ModuleGift.order_total <=' => $order['Order']['total'], 'ModuleGift.content_id >' => 0), 'order' => array('ModuleGift.order_total' => 'DESC')));
+		
+		foreach ($gift as $gifts) { 
+	
+		if ($gifts) {
+		if($order['Order']['total'] >= $gifts['ModuleGift']['order_total']) {
 			
-		$content_description = $this->ContentBase->get_content_description($gift['ModuleGift']['content_id']);
+		$content_description = $this->ContentBase->get_content_description($gifts['ModuleGift']['content_id']);
 
 		$order_product_gift = array();
 		$order_product_gift['OrderProduct']['order_id'] = $order['Order']['id'];
@@ -27,7 +32,7 @@ class GiftController extends ModuleGiftAppController {
 		$order_product_gift['OrderProduct']['quantity'] = 1;
 		$order_product_gift['OrderProduct']['price'] = 0;	
 
-		$order_product_gift['OrderProduct']['content_id'] = $gift['ModuleGift']['content_id'];
+		$order_product_gift['OrderProduct']['content_id'] = $gifts['ModuleGift']['content_id'];
 
 		// Load the OrderProduct model for saving and error checking
 		App::import('Model', 'OrderProduct');
@@ -46,6 +51,8 @@ class GiftController extends ModuleGiftAppController {
 		$this->Order->save($order);
 		
 		}
+		}
+		
 		}
 	
 	}
