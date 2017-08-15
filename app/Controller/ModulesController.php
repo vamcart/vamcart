7ed7233d7f1601c2e8c61a27ad47457ab1186711
vam <vamshop@gmail.com>
2017-08-15 19:41:38 +0300

    Добавлена очистка кэша на странице модулей в админке

--- app/Controller/ModulesController.php
+++ app/Controller/ModulesController.php
@@ -42,7 +42,15 @@ class ModulesController extends AppController {
 
 		
 		$this->set('modules',$modules);
-				
+
+		// clear Cache::write() items
+		Cache::clear();
+		// clear core cache
+		$cachePaths = array('views', 'persistent', 'models', 'catalog');
+		foreach($cachePaths as $config) {
+				clearCache(null, $config);
+		}	
+		
 	}
 
 	public function admin_add ()
