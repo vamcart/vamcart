97867bd57545493624c6676f242aa7abb759e473
vam <vamshop@gmail.com>
2017-08-16 21:47:46 +0300

    Корректировка кода

--- app/Plugin/ModuleGift/Controller/GiftController.php
+++ app/Plugin/ModuleGift/Controller/GiftController.php
@@ -37,7 +37,7 @@ class GiftController extends ModuleGiftAppController {
 		App::import('Model', 'OrderProduct');
 		$this->OrderProduct = new OrderProduct();
 
-		// Make sure this coupon isn't already in our 'cart'	
+		// Make sure this gift isn't already in our 'cart'	
 		$coupon_count = $this->OrderProduct->find('count', array('conditions' => array('order_id' => $order['Order']['id'], 'name' => $order_product_gift['OrderProduct']['name'])));
 		if($coupon_count > 0)
 		{
