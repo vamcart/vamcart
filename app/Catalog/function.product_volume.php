5ff3cd0804283fb39cf930334746121890a1fdc8
vam <vamshop@gmail.com>
2017-10-29 17:36:15 +0300

    Добавлена новая метка {product_volume} - выводит объём товара

--- app/Catalog/function.product_volume.php
+++ app/Catalog/function.product_volume.php
@@ -6,25 +6,25 @@
    License - http://vamshop.com/license.html
    ---------------------------------------------------------------------------------------*/
 
-function smarty_function_product_weight($params, $template)
+function smarty_function_product_volume($params, $template)
 {
 	global $content;
 
 	if ($content['Content']['content_type_id'] != 2) return;
 
-	$weight = $content['ContentProduct']['weight'];
+	$volume = $content['ContentProduct']['volume'];
 	
-	if ($weight > 0) {
-	echo $weight;
+	if ($volume > 0) {
+	echo $volume;
 	}
 }
 
-function smarty_help_function_product_weight () {
+function smarty_help_function_product_volume () {
 	?>
 	<h3><?php echo __('What does this tag do?') ?></h3>
-	<p><?php echo __('Displays the product weight for the current content.') ?></p>
+	<p><?php echo __('Displays the product volume for the current content.') ?></p>
 	<h3><?php echo __('How do I use it?') ?></h3>
-	<p><?php echo __('Just insert the tag into your template like:') ?> <code>{product_weight}</code></p>
+	<p><?php echo __('Just insert the tag into your template like:') ?> <code>{product_volume}</code></p>
 	<h3><?php echo __('What parameters does it take?') ?></h3>
 	<ul>
 		<li><em>(<?php echo __('None') ?>)</em></li>
@@ -32,6 +32,6 @@ function smarty_help_function_product_weight () {
 	<?php
 }
 
-function smarty_about_function_product_weight () {
+function smarty_about_function_product_volume () {
 }
 ?>
