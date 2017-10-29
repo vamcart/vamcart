d10cf177edfb034b021d9242b1473d947350af7c
vam <vamshop@gmail.com>
2017-10-29 17:35:33 +0300

    Добавлена новая метка {product_height} - выводит высоту товара

--- app/Catalog/function.product_height.php
+++ app/Catalog/function.product_height.php
@@ -6,25 +6,25 @@
    License - http://vamshop.com/license.html
    ---------------------------------------------------------------------------------------*/
 
-function smarty_function_product_weight($params, $template)
+function smarty_function_product_height($params, $template)
 {
 	global $content;
 
 	if ($content['Content']['content_type_id'] != 2) return;
 
-	$weight = $content['ContentProduct']['weight'];
+	$height = $content['ContentProduct']['height'];
 	
-	if ($weight > 0) {
-	echo $weight;
+	if ($height > 0) {
+	echo $height;
 	}
 }
 
-function smarty_help_function_product_weight () {
+function smarty_help_function_product_height () {
 	?>
 	<h3><?php echo __('What does this tag do?') ?></h3>
-	<p><?php echo __('Displays the product weight for the current content.') ?></p>
+	<p><?php echo __('Displays the product height for the current content.') ?></p>
 	<h3><?php echo __('How do I use it?') ?></h3>
-	<p><?php echo __('Just insert the tag into your template like:') ?> <code>{product_weight}</code></p>
+	<p><?php echo __('Just insert the tag into your template like:') ?> <code>{product_height}</code></p>
 	<h3><?php echo __('What parameters does it take?') ?></h3>
 	<ul>
 		<li><em>(<?php echo __('None') ?>)</em></li>
@@ -32,6 +32,6 @@ function smarty_help_function_product_weight () {
 	<?php
 }
 
-function smarty_about_function_product_weight () {
+function smarty_about_function_product_height () {
 }
 ?>
