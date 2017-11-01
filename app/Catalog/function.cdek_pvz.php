90a1ff4e2ac039d3c7b5bf4a4f1a20b834796dab
vam <vamshop@gmail.com>
2017-11-01 13:58:36 +0300

    Корректирока кода метки {cdek_pvz}

--- app/Catalog/function.cdek_pvz.php
+++ app/Catalog/function.cdek_pvz.php
@@ -9,21 +9,44 @@
 function default_template_cdek_pvz()
 {
 $template = '
+<!-- Button trigger modal -->
+<span><u><a href="" data-toggle="modal" data-target="#myModalPvz">{if {cookie name="vamshop-city"}}{cookie name="vamshop-city"}{else}{$city}{/if}</a></span></u>
+
+<!-- Modal -->
+<div class="modal fade" id="myModalPvz" tabindex="-1" role="dialog" aria-labelledby="myModalPvzLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalPvzLabel">Список пунктов выдачи заказов <u>{if {cookie name="vamshop-city"}}{cookie name="vamshop-city"}{else}{$city}{/if}</u></h4>
+      </div>
+      <div class="modal-body">
+
 <table cellpadding="0" cellspacing="0" border="0" class="table table-striped">
 <thead>
 <tr>
-<th colspan="2" class="ship-title">{lang}Shipping{/lang} <span><u>{$city}</span></u></th>
+<th class="ship-title">{lang}Name{/lang}</th>
+<th class="ship-title">{lang}Address Line 1{/lang}</th>
+<th class="ship-title">{lang}Phone{/lang}</th>
 </tr>
 </thead>
 <tbody>
-{foreach from=$ship_methods item=ship_method}
+{foreach from=$cdek_pvz item=pvz}
 <tr>
-<td>{lang}{$ship_method.name}{/lang}</td>
-<td class="text-right">{$ship_method.cost}</td>
+<td>{lang}{$pvz.name}{/lang}</td>
+<td>{lang}{$pvz.address}{/lang}</td>
+<td class="text-left">{$pvz.phone}</td>
 </tr>
 {/foreach}
 </tbody>
-</table>  
+</table>        
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Закрыть</button>
+      </div>
+    </div>
+  </div>
+</div>  
 ';
 		
 return $template;
@@ -35,6 +58,7 @@ function smarty_function_cdek_pvz($params, $template)
 	global $content, $config, $order;
 
    $ip = $_SERVER['REMOTE_ADDR'];
+   //$ip = '178.34.161.115';
 	
    $curl = curl_init();
    curl_setopt($curl, CURLOPT_URL, "https://ru.sxgeo.city/json/".$ip);
@@ -69,68 +93,63 @@ function smarty_function_cdek_pvz($params, $template)
 	App::import('Model', 'ShippingMethod');
 		$ShippingMethod = new ShippingMethod();
 
-	if(!isset($params['price']))
-		$params['price'] = 0;		
-
-	if(!isset($params['weight']))
-		$params['weight'] = 0;		
-
-	if(!isset($params['length']))
-		$params['length'] = 0;		
-
-	if(!isset($params['width']))
-		$params['width'] = 0;		
-
-	if(!isset($params['height']))
-		$params['height'] = 0;		
-
-	if(!isset($params['volume']))
-		$params['volume'] = 0;		
-
 	if(!isset($params['city']))
-		$params['city'] = false;		
+		$params['city'] = $city;		
 
-	if(!isset($params['state']))
-		$params['state'] = false;		
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, "http://api.cdek.ru/city/getListByTerm/json.php?q=".$city);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $data = curl_exec($curl);
+    
+    curl_close($curl);
+    if($data === false) {
+	  if ($data_cdek['debug'] == 1) echo "ID номер для города отправителя посылки не найден.";
+    }
+    
+    $senderCity = json_decode($data, $assoc=true);
+    $senderCityId = $senderCity["geonames"][0]["id"];
+    
+     $cdek_pvz = "https://integration.cdek.ru/pvzlist.php?cityid=".$senderCityId;
 
-	$order['Order']['bill_city'] = $ip_geo["city"]["name_ru"];
+     // create curl resource
+     $ch = curl_init();
 
-	$order['OrderProduct'][1]['name'] = $content['ContentDescription']['name'];	
-	$order['OrderProduct'][1]['quantity'] = 1;	
-	$order['OrderProduct'][1]['weight'] = $content['ContentProduct']['weight'];	
-	$order['OrderProduct'][1]['length'] = $content['ContentProduct']['length'];	
-	$order['OrderProduct'][1]['width'] = $content['ContentProduct']['width'];	
-	$order['OrderProduct'][1]['height'] = $content['ContentProduct']['height'];	
-	$order['OrderProduct'][1]['volume'] = $content['ContentProduct']['volume'];	
+     // set url
+     curl_setopt($ch, CURLOPT_URL, $cdek_pvz);
 
-	$active_ship_methods = $ShippingMethod->find('all', array('conditions' => array('active' => '1'),'order' => array('order')));
+     //return the transfer as a string
+     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-	$keyed_ship_methods = array();
-	foreach($active_ship_methods AS $method)
+     // $output contains the output string
+     $output = curl_exec($ch);
+
+     // close curl resource to free up system resources
+     curl_close($ch);  
+
+     $cdek_contents = $output;
+     $cdek_results = simplexml_load_string($cdek_contents);
+     
+     //echo var_dump($cdek_results);
+        
+	$keyed_cdek_pvz = array();
+	$pvz_id = 0;
+	foreach($cdek_results AS $pvz)
 	{
-		$shipping = Inflector::classify($method['ShippingMethod']['code']);
-		$shipping_controller =  Inflector::classify($method['ShippingMethod']['code']) . 'Controller';
-		App::import('Controller', 'Shipping.'.$shipping);
-		$MethodBase = new $shipping_controller();
-		
-		$ship_method_id = $method['ShippingMethod']['id'];
-		
-		$keyed_ship_methods[$ship_method_id] = array(
-										  'id' => $ship_method_id,
-										  'name' => $method['ShippingMethod']['name'],
-										  'description' => (isset($method['ShippingMethod']['description'])) ? __($method['ShippingMethod']['description']) : false,
-										  'icon' => (isset($method['ShippingMethod']['icon']) && file_exists(IMAGES . 'icons/shipping/' . $method['ShippingMethod']['icon'])) ? $method['ShippingMethod']['icon'] : false,
-										  'cost_plain' => $MethodBase->calculate(),
-										  'cost' => $CurrencyBase->display_price($MethodBase->calculate())
+		$keyed_cdek_pvz[$pvz_id] = array(
+										  'id' => $pvz['Code'],
+										  'name' => $pvz['Name'],
+										  'address' => $pvz['FullAddress'],
+										  'phone' => $pvz['Phone'],
 										  );
-
+										  
+   $pvz_id++;
 	}	
 	
 	$assignments = array(
 		'city' => $city,
 		'state' => $state,
 		'country' => $country,
-		'ship_methods' => $keyed_ship_methods
+		'cdek_pvz' => $keyed_cdek_pvz
 	);
 
 	$display_template = $Smarty->load_template($params, 'cdek_pvz');
@@ -149,12 +168,13 @@ function smarty_function_cdek_pvz($params, $template)
 function smarty_help_function_cdek_pvz() {
 	?>
 	<h3><?php echo __('What does this tag do?') ?></h3>
-	<p><?php echo __('Displays the active shipping methods and calculations for the current product.') ?></p>
+	<p><?php echo __('Displays the cdek shipping offices.') ?></p>
 	<h3><?php echo __('How do I use it?') ?></h3>
 	<p><?php echo __('Just insert the tag into your template like:') ?> <code>{cdek_pvz}</code></p>
 	<h3><?php echo __('What parameters does it take?') ?></h3>
 	<ul>
 		<li><em><?php echo __('(template)') ?></em> - <?php echo __('Useful if you want to override the default content listing template. Setting this will utilize the template that matches this alias.') ?></li>
+		<li><em><?php echo __('(city)') ?></em> - <?php echo __('City name.') ?></li>
 	</ul>
 	<?php
 }
