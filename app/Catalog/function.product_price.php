750d4eb7a8ad81457c15e2b73bace78721a31e06
vam <vamshop@gmail.com>
2017-11-26 12:24:03 +0300

    Добавлен параметр plain в метку product_price, для возможности вывода просто числа, без символа валюты

--- app/Catalog/function.product_price.php
+++ app/Catalog/function.product_price.php
@@ -22,6 +22,8 @@ function smarty_function_product_price($params, $template)
 	if(isset($params['price'])) $price = $params['price'];
 
 	$price = $CurrencyBase->display_price($price);
+
+	if(isset($params['plain']) && $params['plain'] == true) $price = $content[$content_type]['price'];
 	
 	echo $price;
 }
