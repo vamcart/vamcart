8cef90db4ef7f1b8ac51a89d647bd5bfef56d997
vam <vamshop@gmail.com>
2018-02-09 13:47:31 +0300

    Добавлены пробные тесты

---
+++ app/Test/Foo.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of Kristuff\dummyPHP.
+ *
+ * (c) Kristuff <contact@kristuff.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Stuff\dummyPHP;
+
+/**
+ * Interersting class Foo
+ */
+class Foo 
+{
+
+    public static function bar()
+    {
+        return 'bar';
+    }
+      
+}
---
+++ app/Test/FooTest.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once '/../Vendor/autoload.php';
+
+use Stuff\dummyPHP;
+use Stuff\dummyPHP\Foo;
+
+class FooTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testThatBarReturnsBar()
+    {
+        $this->assertEquals('bar', Foo::bar());
+    }
+     
+    public function testCaseSensitiveBar()
+    {
+        $this->assertNotEquals('Bar', Foo::bar());
+    }        
+}
