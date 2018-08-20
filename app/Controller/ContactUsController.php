34261a1ddbd218056afb99cb63af2c61e4a617db
vam <vamshop@gmail.com>
2018-08-20 18:49:02 +0300

    Корректировка кода сохранения писем обратной связи

--- app/Controller/ContactUsController.php
+++ app/Controller/ContactUsController.php
@@ -42,25 +42,25 @@ class ContactUsController extends AppController {
 		App::import('Model', 'Contact');
 		$contact = new Contact();
 		
-		$contact = array();
+		$contact_data = array();
 
-		$contact['name'] = $_POST['name'];
-		$contact['email'] = $_POST['email'];
-		$contact['message'] = $_POST['message'];
+		$contact_data['name'] = $_POST['name'];
+		$contact_data['email'] = $_POST['email'];
+		$contact_data['message'] = $_POST['message'];
 
-		$contact['customer_id'] = (($_SESSION['Customer']['customer_id'] > 0) ? $_SESSION['Customer']['customer_id'] : 0);
-		$contact['answered'] = 0;
+		$contact_data['customer_id'] = (($_SESSION['Customer']['customer_id'] > 0) ? $_SESSION['Customer']['customer_id'] : 0);
+		$contact_data['answered'] = 0;
 
-		$contact['created'] = date("Y-m-d H:i:s");
-		$contact['modified'] = date("Y-m-d H:i:s");
+		$contact_data['created'] = date("Y-m-d H:i:s");
+		$contact_data['modified'] = date("Y-m-d H:i:s");
 
-		$contact['ref'] = $_SERVER['HTTP_REFERER'];
-		$contact['ip'] = $_SERVER['REMOTE_ADDR'];
-		$contact['forwarded_ip'] = $_SERVER['REMOTE_ADDR'];
-		$contact['user_agent'] = $_SERVER['HTTP_USER_AGENT'];
-		$contact['accept_language'] = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
+		$contact_data['ref'] = $_SERVER['HTTP_REFERER'];
+		$contact_data['ip'] = $_SERVER['REMOTE_ADDR'];
+		$contact_data['forwarded_ip'] = $_SERVER['REMOTE_ADDR'];
+		$contact_data['user_agent'] = $_SERVER['HTTP_USER_AGENT'];
+		$contact_data['accept_language'] = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
 
-		$contact->save($contact);
+		$contact->save($contact_data);
 
 		
 		$config = $this->ConfigurationBase->load_configuration();
