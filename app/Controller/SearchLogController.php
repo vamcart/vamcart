1fcda3c2181a8ef0f7cb289717e750def6d07813
vam <vamshop@gmail.com>
2018-08-23 17:43:36 +0300

    Добавлен новый раздел Поисковые запросы в Админке - Инструменты, в котором выводится лог поисковых запросов, т.е. то, что ищут посетители в Вашем магазине через поиск.

---
+++ app/Controller/SearchLogController.php
@@ -0,0 +1,53 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   VamShop - http://vamshop.com
+   -----------------------------------------------------------------------------------------
+   Copyright (c) 2014 VamSoft Ltd.
+   License - http://vamshop.com/license.html
+   ---------------------------------------------------------------------------------------*/
+
+class SearchLogController extends AppController {
+	public $name = 'SearchLog';
+        public $paginate = array('limit' => 20, 'order' => array('SearchLog.id' => 'asc'));
+        
+        public function admin ()
+			{
+            $this->set('current_crumb', __('Search Log', true));
+            $this->set('title_for_layout', __('Search Log Listing', true));
+            
+            $data = $this->paginate('SearchLog');
+            $this->set('data',$data);
+        	}          
+        
+			public function admin_delete ($group_id = null)
+			{
+            $this->SearchLog->delete($group_id);
+            $this->Session->setFlash( __('Record deleted.', true));
+            $this->redirect('/search_log/admin/');
+			}
+			
+			public function admin_modify_selected() 	
+			{
+			$build_flash = "";
+			foreach($this->params['data']['SearchLog']['modify'] AS $value)
+			{
+				// Make sure the id is valid
+				if($value > 0)
+				{
+					$this->SearchLog->id = $value;
+					$currency = $this->SearchLog->read();
+			
+					switch ($this->data['multiaction']) 
+					{
+						case "delete":
+							    $this->SearchLog->delete($value);
+								$build_flash .= __('Record deleted.', true) . '<br />';									
+						break;
+					}
+				}
+			}
+			$this->Session->setFlash($build_flash);
+			$this->redirect('/search_log/admin/');
+			}
+							
+	}
---
+++ app/View/SearchLog/admin.ctp
@@ -0,0 +1,43 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   VamShop - http://vamshop.com
+   -----------------------------------------------------------------------------------------
+   Copyright (c) 2014 VamSoft Ltd.
+   License - http://vamshop.com/license.html
+   ---------------------------------------------------------------------------------------*/
+
+$this->Html->script(array(
+	'admin/selectall.js'
+), array('inline' => false));
+
+echo $this->Admin->ShowPageHeaderStart($current_crumb, 'cus-group');
+
+echo $this->Form->create('SearchLog', array('url' => '/search_log/admin_modify_selected/'));
+
+echo '<table class="contentTable">';
+
+echo $this->Html->tableHeaders(array( __('Search Keywords'),  __('Action'), '<input type="checkbox" onclick="checkAll(this)" />'));
+
+foreach ($data AS $search_keywords)
+{
+	echo $this->Admin->TableCells(
+		  array(
+				$this->Html->link($search_keywords['SearchLog']['keyword'], ''),
+				array($this->Admin->ActionButton('delete','/search_log/admin_delete/' . $search_keywords['SearchLog']['id'],__('Delete')), array('align'=>'center')),
+				array($this->Form->checkbox('modify][', array('value' => $search_keywords['SearchLog']['id'])), array('align'=>'center'))
+		   ));
+		   	
+}
+
+echo '</table>';
+
+echo $this->Admin->ActionBar(array('delete'=>__('Delete')),false);
+?>
+
+<table class="contentPagination">
+	<tr>
+		<td><?php echo $this->Paginator->numbers(array('separator'=>' - ')); ?></td>
+	</tr>
+</table>
+<?php
+echo $this->Admin->ShowPageHeaderEnd();
