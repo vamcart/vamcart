0e4196c88ed04e6845c4d9fc310f6c25ded7a51c
vam <vamshop@gmail.com>
2018-10-09 23:53:48 +0300

    Добавлена по умолчанию новая метка {geo_city} для определения города посетителя

---
+++ app/Catalog/function.geo_city.php
@@ -0,0 +1,34 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   VamShop - http://vamshop.com
+   -----------------------------------------------------------------------------------------
+   Copyright (c) 2014 VamSoft Ltd.
+   License - http://vamshop.com/license.html
+   ---------------------------------------------------------------------------------------*/
+
+function smarty_function_geo_city($params, $template)
+{
+	global $content;
+  App::import('Vendor', 'GeoCity', array('file' => 'GeoCity'.DS.'SxGeo.php'));
+  $SxGeo= new SxGeo(APP.'Vendor/GeoCity/SxGeo.dat');
+  $city = $SxGeo->get($_SERVER['REMOTE_ADDR']);
+
+	return $city['city']['name_ru'];
+}
+
+function smarty_help_function_geo_city() {
+	?>
+	<h3><?php echo __('What does this tag do?') ?></h3>
+	<p><?php echo __('Displays the current user city name.') ?></p>
+	<h3><?php echo __('How do I use it?') ?></h3>
+	<p><?php echo __('Just insert the tag into your template like:') ?> <code>{geo_city}</code></p>
+	<h3><?php echo __('What parameters does it take?') ?></h3>
+	<ul>
+		<li><em>(<?php echo __('None') ?>)</em></li>
+	</ul>
+	<?php
+}
+
+function smarty_about_function_geo_city() {
+}
+?>
