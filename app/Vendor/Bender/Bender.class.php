c180843bcd3363312c75942551037b039b450e40
vam <vamshop@gmail.com>
2018-11-24 20:51:27 +0300

    По умолчанию отключена отложенная загрузка стилей

--- app/Vendor/Bender/Bender.class.php
+++ app/Vendor/Bender/Bender.class.php
@@ -121,8 +121,8 @@ class Bender
             case "css":
                 $this->check_recombine( $output, $_stylesheets );
                 $this->minify( $_stylesheets, "css", $output );
-                //return '<link href="' . $this->get_src( $output ) . '" rel="stylesheet" type="text/css"/>';
-                return '<script id="loadcss">function loadCSS(e,n,o,t){"use strict";var d=window.document.createElement("link"),i=n||window.document.getElementsByTagName("script")[0],r=window.document.styleSheets;return d.rel="stylesheet",d.href=e,d.media="only x",t&&(d.onload=t),i.parentNode.insertBefore(d,i),d.onloadcssdefined=function(e){for(var n,o=0;o<r.length;o++)r[o].href&&r[o].href===d.href&&(n=!0);n?e():setTimeout(function(){d.onloadcssdefined(e)})},d.onloadcssdefined(function(){d.media=o||"all"}),d}loadCSS("' . $this->get_src( $output ) . '", document.getElementById("loadcss"));</script><noscript><link href="' . $this->get_src($output) . '" rel="stylesheet"></noscript>';    
+                return '<link href="' . $this->get_src( $output ) . '" rel="stylesheet" type="text/css"/>';
+                //return '<script id="loadcss">function loadCSS(e,n,o,t){"use strict";var d=window.document.createElement("link"),i=n||window.document.getElementsByTagName("script")[0],r=window.document.styleSheets;return d.rel="stylesheet",d.href=e,d.media="only x",t&&(d.onload=t),i.parentNode.insertBefore(d,i),d.onloadcssdefined=function(e){for(var n,o=0;o<r.length;o++)r[o].href&&r[o].href===d.href&&(n=!0);n?e():setTimeout(function(){d.onloadcssdefined(e)})},d.onloadcssdefined(function(){d.media=o||"all"}),d}loadCSS("' . $this->get_src( $output ) . '", document.getElementById("loadcss"));</script><noscript><link href="' . $this->get_src($output) . '" rel="stylesheet"></noscript>';    
                 break;
             case "js":
                 $this->check_recombine( $output, $_javascripts );
