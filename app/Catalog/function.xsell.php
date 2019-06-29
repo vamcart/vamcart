5c6eba5a8bb590fde49b63091fdf1d0b7b96273d
vam <vamshop@gmail.com>
2019-06-29 23:13:11 +0300

    Корректировка кода

--- app/Catalog/function.xsell.php
+++ app/Catalog/function.xsell.php
@@ -176,8 +176,8 @@ function smarty_function_xsell($params, &$smarty)
 			if ($content['ContentRelations'][$key]['content_type_id'] == 2) $content['ContentRelations'][$key]['width']	= $product[$content_type]['width'];	
 			if ($content['ContentRelations'][$key]['content_type_id'] == 2) $content['ContentRelations'][$key]['height']	= $product[$content_type]['height'];	
 			if ($content['ContentRelations'][$key]['content_type_id'] == 2) $content['ContentRelations'][$key]['volume']	= $product[$content_type]['volume'];	
-			if ($content['ContentRelations'][$key]['content_type_id'] == 2) $content['ContentRelations'][$key]['is_new']	= $product[$content_type]['is_new'];	
-			if ($content['ContentRelations'][$key]['content_type_id'] == 2) $content['ContentRelations'][$key]['is_featured']	= $product[$content_type]['is_featured'];	
+			$content['ContentRelations'][$key]['is_new']	= $product[$content_type]['is_new'];	
+			$content['ContentRelations'][$key]['is_featured']	= $product[$content_type]['is_featured'];	
 			if ($content['ContentRelations'][$key]['content_type_id'] == 2 or $content['ContentRelations'][$key]['content_type_id'] == 7) $content['ContentRelations'][$key]['manufacturer']	= $ContentBase->getManufacturerName($product[$content_type]['manufacturer_id']);	
 			if ($content['ContentRelations'][$key]['content_type_id'] == 2 or $content['ContentRelations'][$key]['content_type_id'] == 7) $content['ContentRelations'][$key]['label_id']	= $product[$content_type]['label_id'];	
 			$content['ContentRelations'][$key]['date_added']	= CakeTime::i18nFormat($content['ContentRelations'][$key]['created']);	
