2e8672d8c220086eb83d47aa0db01c15d16c98bc
VamCart <vamcart@gmail.com>
2025-04-25 13:30:41 +0300

    Корректировка папки /app/Vendor/composer

--- app/Vendor/composer/autoload_real.php
+++ app/Vendor/composer/autoload_real.php
@@ -13,30 +13,23 @@ class ComposerAutoloaderInit13a400b1587fe780160c7a787a83ec5f
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit13a400b1587fe780160c7a787a83ec5f', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit13a400b1587fe780160c7a787a83ec5f', 'loadClassLoader'));
 
-        $map = require __DIR__ . '/autoload_namespaces.php';
-        foreach ($map as $namespace => $path) {
-            $loader->set($namespace, $path);
-        }
-
-        $map = require __DIR__ . '/autoload_psr4.php';
-        foreach ($map as $namespace => $path) {
-            $loader->setPsr4($namespace, $path);
-        }
-
-        $classMap = require __DIR__ . '/autoload_classmap.php';
-        if ($classMap) {
-            $loader->addClassMap($classMap);
-        }
+        require __DIR__ . '/autoload_static.php';
+        call_user_func(\Composer\Autoload\ComposerStaticInit13a400b1587fe780160c7a787a83ec5f::getInitializer($loader));
 
         $loader->register(true);
 
